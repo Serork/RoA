6a796b12c055757e311a2a72a66022af94d85d2d
Serork <mandrygin.2013@mail.ru>
2024-08-30 22:33:33 +0400

    Update BackwoodsBackgroundSurface.cs

--- Content/Backgrounds/BackwoodsBackgroundSurface.cs
+++ Content/Backgrounds/BackwoodsBackgroundSurface.cs
@@ -24,13 +24,13 @@ sealed class BackwoodsBackgroundSurface : ModSurfaceBackgroundStyle {
     public override void ModifyFarFades(float[] fades, float transitionSpeed) {
 		for (int i = 0; i < fades.Length; i++) {
 			if (i == Slot) {
-                fades[i] += BackwoodsBiome.TransitionSpeed;
+                fades[i] += transitionSpeed;
 				if (fades[i] > 1f) {
 					fades[i] = 1f;
 				}
 			}
 			else {
-                fades[i] -= BackwoodsBiome.TransitionSpeed;
+                fades[i] -= transitionSpeed;
 				if (fades[i] < 0f) {
 					fades[i] = 0f;
 				}
