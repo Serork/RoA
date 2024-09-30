2c49b35d0a1ee8952be5d37d107677e227fc3772
Serork <mandrygin.2013@mail.ru>
2024-10-01 03:18:01 +0400

    Update BackwoodsVars.cs

--- Common/BackwoodsSystems/BackwoodsVars.cs
+++ Common/BackwoodsSystems/BackwoodsVars.cs
@@ -16,8 +16,8 @@ using Terraria.ModLoader.IO;
 namespace RoA.Common.BackwoodsSystems;
 
 sealed class BackwoodsVars : ModSystem {
-    private static float _preDownedBoss2Timer;
-    private static bool _backwoodsAwake, _backwoodsAwake2;
+    private static float _preDownedBossTimer;
+    private static bool _backwoodsAwake;
 
     public static int FirstTileYAtCenter { get; internal set; }
     public static int BackwoodsTileForBackground { get; internal set; }
@@ -29,30 +29,35 @@ sealed class BackwoodsVars : ModSystem {
     public override void SaveWorldData(TagCompound tag) {
         tag[nameof(FirstTileYAtCenter)] = FirstTileYAtCenter;
         tag[nameof(BackwoodsTileForBackground)] = BackwoodsTileForBackground;
-        tag[nameof(_preDownedBoss2Timer)] = _preDownedBoss2Timer;
+        tag[nameof(_preDownedBossTimer)] = _preDownedBossTimer;
+        tag[nameof(_backwoodsAwake)] = _backwoodsAwake;
     }
 
     public override void LoadWorldData(TagCompound tag) {
         FirstTileYAtCenter = tag.GetInt(nameof(FirstTileYAtCenter));
         BackwoodsTileForBackground = tag.GetInt(nameof(BackwoodsTileForBackground));
-        _preDownedBoss2Timer = tag.GetFloat(nameof(_preDownedBoss2Timer));
+        _preDownedBossTimer = tag.GetFloat(nameof(_preDownedBossTimer));
+        _backwoodsAwake = tag.GetBool(nameof(_backwoodsAwake));
     }
 
     private static void ResetAllFlags() {
         FirstTileYAtCenter = BackwoodsTileForBackground = 0;
-        _preDownedBoss2Timer = 0f;
+        _preDownedBossTimer = 0f;
+        _backwoodsAwake = false;
     }
 
     public override void NetSend(BinaryWriter writer) {
         writer.Write(FirstTileYAtCenter);
         writer.Write(BackwoodsTileForBackground);
-        writer.Write(_preDownedBoss2Timer);
+        writer.Write(_preDownedBossTimer);
+        writer.Write(_backwoodsAwake);
     }
 
     public override void NetReceive(BinaryReader reader) {
         FirstTileYAtCenter = reader.ReadInt32();
         BackwoodsTileForBackground = reader.ReadInt32();
-        _preDownedBoss2Timer = reader.ReadSingle();
+        _preDownedBossTimer = reader.ReadSingle();
+        _backwoodsAwake = reader.ReadBoolean();
     }
 
     public override void PostUpdateNPCs() {
@@ -64,12 +69,12 @@ sealed class BackwoodsVars : ModSystem {
             return;
         }
 
-        if (_preDownedBoss2Timer == -1f || Main.netMode == NetmodeID.MultiplayerClient) {
+        if (_preDownedBossTimer == -1f || Main.netMode == NetmodeID.MultiplayerClient) {
             return;
         }
-        _preDownedBoss2Timer += TimeSystem.LogicDeltaTime;
-        if (_preDownedBoss2Timer >= 5f) {
-            _preDownedBoss2Timer = -1f;
+        _preDownedBossTimer += TimeSystem.LogicDeltaTime;
+        if (_preDownedBossTimer >= 5f) {
+            _preDownedBossTimer = -1f;
 
             Color color = Color.LightGreen;
             string text1 = Language.GetText("Mods.RoA.World.BackwoodsFree").ToString();
