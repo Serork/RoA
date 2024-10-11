7dbcfadc2b0826a1681b2f7198801f5d36855371
Serork <mandrygin.2013@mail.ru>
2024-10-11 16:40:07 +0400

    Adjustments

--- Content/Tiles/Crafting/Tapper.Drawing.cs
+++ Content/Tiles/Crafting/Tapper.Drawing.cs
@@ -116,7 +116,7 @@ sealed class TapperDrawing : GlobalTile {
                 int offsetX = flag ? 0 : 1;
                 bool flag4 = flag && flag2;
                 Asset<Texture2D>? tapperBracingAsset = ModContent.Request<Texture2D>((TileLoader.GetTile(tapperTileType) as Tapper).BracingTexture);
-                spriteBatch.Draw(tapperBracingAsset.Value, new Vector2((float)((i + offset) * 16 - (int)position.X + offsetX - (flag4 ? 3 : 0)), (float)(j * 16 - (int)position.Y) - 6), new Rectangle(0, flag4 ? num12 : 0, coordinateWidth + (flag4 ? 2 : 0), num12), color, 0f, Vector2.Zero, 1f, offset == 0 ? SpriteEffects.FlipHorizontally : SpriteEffects.None, 0);
+                spriteBatch.Draw(tapperBracingAsset.Value, new Vector2((float)((i + offset) * 16 - (int)position.X + offsetX - (flag4 ? 2 : 0)), (float)(j * 16 - (int)position.Y) - 6), new Rectangle(0, flag4 ? num12 : 0, coordinateWidth + (flag4 ? 2 : 0), num12), color, 0f, Vector2.Zero, 1f, offset == 0 ? SpriteEffects.FlipHorizontally : SpriteEffects.None, 0);
             }
         }
     }
