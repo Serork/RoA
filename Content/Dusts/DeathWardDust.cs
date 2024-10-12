8d8b503a8769c70e1753a32726e4df247aa4ac56
Serork <mandrygin.2013@mail.ru>
2024-10-12 23:35:37 +0400

    Update DeathWardDust.cs

--- Content/Dusts/DeathWardDust.cs
+++ Content/Dusts/DeathWardDust.cs
@@ -6,6 +6,10 @@ using Terraria.ModLoader;
 namespace RoA.Content.Dusts;
 
 public class DeathWardDust : ModDust {
+    public override Color? GetAlpha(Dust dust, Color lightColor) {
+        return new Color(lightColor.R, lightColor.G, lightColor.B, 10);
+    }
+
     public override bool Update(Dust dust) {
         dust.scale -= 0.01f;
         float num55 = dust.scale * 1f;
