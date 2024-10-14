a348a11cb5bb67956441f395e5249384af1b60f2
Serork <mandrygin.2013@mail.ru>
2024-10-14 13:52:27 +0400

    Adjustments

---
+++ Common/InterfaceElements/BeltButton.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+
+using RoA.Common.Players;
+using RoA.Core;
+using RoA.Core.Utility;
+
+using System.Collections.Generic;
+
+using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.UI;
+
+namespace RoA.Common.InterfaceElements;
+
+sealed class BeltButton() : InterfaceElement(RoA.ModName + ": Belt Button", InterfaceScaleType.UI) {
+    private static Asset<Texture2D> _beltButtonTexture, _beltButtonTextureOutline;
+
+    public static bool IsUsed { get; private set; }
+
+    public override void Load(Mod mod) {
+        if (Main.dedServ) {
+            return;
+        }
+
+        string textureName = ResourceManager.GUITextures + "BeltButton";
+        _beltButtonTexture = ModContent.Request<Texture2D>(textureName);
+        _beltButtonTextureOutline = ModContent.Request<Texture2D>(textureName + "_Outline");
+    }
+
+    public override int GetInsertIndex(List<GameInterfaceLayer> layers) {
+        int preferredIndex = layers.FindIndex(layer => layer.Name == "Vanilla: Inventory");
+        return preferredIndex < 0 ? 0 : preferredIndex;
+    }
+
+    protected override bool DrawSelf() {
+        if (Main.playerInventory && Main.EquipPage != 2) {
+            SpriteFrame beltButtonFrame = new SpriteFrame(2, 1);
+            Texture2D texture = (Texture2D)_beltButtonTexture;
+            int num9 = 8 + Main.LocalPlayer.GetAmountOfExtraAccessorySlotsToShow();
+            int inventoryTop = 174;
+            int num10 = 950;
+            if (Main.screenHeight < num10 && num9 >= 10) {
+                inventoryTop -= (int)(56.0 * Main.inventoryScale * (num9 - 9));
+            }
+            float yPos = inventoryTop - 32;
+            int height = _beltButtonTexture.Height() / beltButtonFrame.RowCount;
+            int width = _beltButtonTexture.Width() / beltButtonFrame.ColumnCount;
+            Vector2 origin = new Vector2(width, height);
+            float extraY = Main.screenWidth / 8f;
+            //Vector2 vector2 = new Vector2(Main.screenWidth - 162 - 6f - 12f, (float)yPos + 14f + (!Main.mapFullscreen && Main.mapStyle == 1 && Main.mapEnabled ? extraY : 0f)) + origin / 2f;
+            Vector2 vector2 = new Vector2(Main.screenWidth - 162 - 6f - 12f, WreathSlot.GetCustomLocation().Y);
+            vector2.Y -= texture.Height / 2;
+            vector2.X -= 32f;
+            vector2.X -= Main.netMode == NetmodeID.MultiplayerClient ? 25f : 0f;
+            vector2.Y += 4f;
+            Rectangle sourceRectangle = beltButtonFrame.GetSourceRectangle(texture);
+            float scale = 0.9f;
+            bool flag = Collision.CheckAABBvAABBCollision(vector2 - origin, origin, new Vector2(Main.mouseX, Main.mouseY), Vector2.One) && Main.mouseItem.IsEmpty();
+            sourceRectangle.X += IsUsed ? width : 0;
+            Color color = Color.White;
+            SpriteBatch spriteBatch = Main.spriteBatch;
+            spriteBatch.Draw(texture, vector2, sourceRectangle, color, 0f, origin, scale, SpriteEffects.None, 0f);
+            texture = (Texture2D)_beltButtonTextureOutline;
+            if (flag) {
+                Main.mouseText = true;
+                Main.LocalPlayer.mouseInterface = true;
+                if (Main.mouseLeft && Main.mouseLeftRelease) {
+                    SoundEngine.PlaySound(SoundID.MenuOpen);
+                    IsUsed = !IsUsed;
+                }
+                Main.instance.MouseText(string.Concat(new object[] {
+                            IsUsed ? "Hide Wreath Slot" : "Show Wreath Slot"
+                        }), 0, 0, -1, -1, -1, -1);
+                spriteBatch.Draw(texture, vector2 - Vector2.One * 2f, null, color, 0f, origin, scale, SpriteEffects.None, 0f);
+            }
+        }
+
+        return true;
+    }
+}
--- Common/Players/WreathSlot.cs
+++ Common/Players/WreathSlot.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 
+using RoA.Common.InterfaceElements;
 using RoA.Content.Items.Equipables.Wreaths;
 using RoA.Core;
 
@@ -10,22 +11,18 @@ using Terraria.ModLoader;
 namespace RoA.Common.Players;
 
 sealed class WreathSlot : ModAccessorySlot {
-    public override Vector2? CustomLocation {
-        get {
-            Vector2 getCustomLocation() {
-                int mapH = (Main.mapEnabled && !Main.mapFullscreen && Main.mapStyle == 1) ? 256 : 0;
-                mapH = (Main.mapEnabled && (mapH + 600) > Main.screenHeight) ? Main.screenHeight - 600 : mapH;
-                int offsetX = 47 * 2;
-                int x = (Main.netMode == NetmodeID.MultiplayerClient) ? Main.screenWidth - 135 - offsetX - 47 : Main.screenWidth - 94 - offsetX + 2 - 64;
-                int y = mapH + 174;
-                return new Vector2(x, y);
-            }
-
-            return getCustomLocation();
-        }
+    public static Vector2 GetCustomLocation() {
+        int mapH = (Main.mapEnabled && !Main.mapFullscreen && Main.mapStyle == 1) ? 256 : 0;
+        mapH = (Main.mapEnabled && (mapH + 600) > Main.screenHeight) ? Main.screenHeight - 600 : mapH;
+        int offsetX = 47 * 2;
+        int x = (Main.netMode == NetmodeID.MultiplayerClient) ? Main.screenWidth - 135 - offsetX - 47 : Main.screenWidth - 94 - offsetX + 2 - 64;
+        int y = mapH + 174;
+        return new Vector2(x, y);
     }
 
-    public override bool IsHidden() => Main.EquipPage == 2;
+    public override Vector2? CustomLocation => GetCustomLocation();
+
+    public override bool IsHidden() => (!IsItemValidForSlot(Main.mouseItem) && !BeltButton.IsUsed) || Main.EquipPage == 2;
 
     public override string FunctionalTexture => ResourceManager.GUITextures + "Wreath_SlotBackground";
 
