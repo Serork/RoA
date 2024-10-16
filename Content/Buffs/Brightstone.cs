d67b1ee0d31c1c0c7624ab612d17654e8826664b
Serork <mandrygin.2013@mail.ru>
2024-10-16 13:16:39 +0400

    Update Brightstone.cs

--- Content/Buffs/Brightstone.cs
+++ Content/Buffs/Brightstone.cs
@@ -1,19 +1,17 @@
 using Microsoft.Xna.Framework;
 
-using System;
-
 using Terraria;
 using Terraria.ModLoader;
 
 namespace RoA.Content.Buffs;
 
 public class Brightstone : ModBuff {
-	public override void SetStaticDefaults () {
+	public override void SetStaticDefaults() {
 		// DisplayName.SetDefault("Brightstone Lights");
 		// Description.SetDefault("You emit glowing lumps of light");
 	}
 
-	public override void Update (Player player, ref int buffIndex) => player.GetModPlayer<BrightstonePlayer>().brightstoneEffect = true;
+	public override void Update(Player player, ref int buffIndex) => player.GetModPlayer<BrightstonePlayer>().brightstoneEffect = true;
 }
 
 sealed class BrightstonePlayer : ModPlayer {
@@ -28,9 +26,9 @@ sealed class BrightstonePlayer : ModPlayer {
 
         //Lighting.AddLight(Player.Center, new Color(238, 225, 111).ToVector3());
         Lighting.AddLight(Player.Center, new Color(238, 225, 111).ToVector3() * 0.6f);
-        if (Player.velocity.Length() > 1f && (Player.controlLeft || Player.controlRight || Player.controlJump || Player.velocity.Y > 1f) && !Player.rocketFrame) {
+        if (Player.velocity.Length() >= 1f && (Player.controlLeft || Player.controlRight || Player.controlJump || Player.velocity.Y >= 1f) && !Player.rocketFrame) {
             if (Main.rand.NextBool(6))
-				Projectile.NewProjectile(Player.GetSource_Misc("Brightstone"), Player.Center.X - 2f * Player.direction, Player.Center.Y, 0, 0, ModContent.ProjectileType<Projectiles.Friendly.Brightstone>(), 0, 0, Player.whoAmI);
+				Projectile.NewProjectile(Player.GetSource_Misc("Brightstone"), Player.Center.X, Player.Center.Y, 0, 0, ModContent.ProjectileType<Projectiles.Friendly.Brightstone>(), 0, 0, Player.whoAmI);
 		}
 	}
 }
