23d07406c88e8adf7a20fb35a0cf9fa8803fc0d7
Serork <mandrygin.2013@mail.ru>
2024-11-19 17:28:31 +0400

    Adjustments

--- Content/Projectiles/Friendly/Magic/ShockLightning.cs
+++ Content/Projectiles/Friendly/Magic/ShockLightning.cs
@@ -88,14 +88,14 @@ sealed class ShockLightning : ModProjectile {
     private void UpdateSegments() {
         Player player = Main.player[Projectile.owner];
         Vector2 source = player.itemLocation + (new Vector2(0f, 3f) * player.direction).RotatedBy(Projectile.velocity.ToRotation()) + Utils.SafeNormalize(Projectile.velocity, Vector2.One) * (player.HeldItem.width + player.HeldItem.width / 4);
-        float thickness = 4f;
+        float thickness = 3f;
         Vector2 dest = source + Vector2.Normalize(Projectile.velocity) * _lightningLength;
         Vector2 dif = dest - source;
         Vector2 normal = Vector2.Normalize(new Vector2(dif.Y, -dif.X));
         float length = dif.Length();
         int positionCount = Math.Min(100, (int)(length / 4));
         _results.Clear();
-        List<float> positions = new List<float>(positionCount + 50) {
+        List<float> positions = new(positionCount + 50) {
                 0
             };
         for (int i = 0; i < positionCount; i++) {
@@ -113,7 +113,7 @@ sealed class ShockLightning : ModProjectile {
             displacement -= (displacement - prevDisplacement) * (1 - scale);
             displacement *= envelope;
             Vector2 to = source + position * dif + displacement * normal;
-            if (Main.rand.NextChance(0.25)) {
+            if (tempThickness > 0.25f && Main.rand.NextChance(0.25)) {
                 tempThickness -= tempThickness / 10;
             }
             _results.Add(new Line(Projectile, prevPoint, to, tempThickness));
@@ -129,6 +129,7 @@ sealed class ShockLightning : ModProjectile {
         UpdateSegments();
 
         foreach (var segment in _results) {
+            //Color color = Color.Lerp(Color.White, Lighting.GetColor((int)segment.Position.X / 16, (int)segment.Position.Y / 16), Lighting.Brightness((int)segment.Position.X / 16, (int)segment.Position.Y / 16));
             segment.Draw(Main.spriteBatch, Color.White);
 
             if (!Main.dedServ) {
@@ -144,20 +145,13 @@ sealed class ShockLightning : ModProjectile {
         return Helper.DeathrayHitbox(Projectile.position, lineEnd, targetHitbox, 16f);
     }
 
-    private readonly struct Line {
-        private readonly Projectile _projectile;
-        private readonly Vector2 _a, _b;
-        private readonly float _thickness;
+    private readonly struct Line(Projectile projectile, Vector2 a, Vector2 b, float thickness = 1) {
+        private readonly Projectile _projectile = projectile;
+        private readonly Vector2 _a = a, _b = b;
+        private readonly float _thickness = thickness;
 
         public readonly Vector2 Position => _a + (_b - _a);
         public readonly Player Player => Main.player[_projectile.owner];
-
-        public Line(Projectile projectile, Vector2 a, Vector2 b, float thickness = 1) {
-            _projectile = projectile;
-            _a = a;
-            _b = b;
-            _thickness = thickness;
-        }
 
         public void Draw(SpriteBatch spriteBatch, Color color = default) {
             Player player = Main.player[_projectile.owner];
@@ -166,15 +160,13 @@ sealed class ShockLightning : ModProjectile {
             Vector2 dest = pos - offset;
             offset = pos - _a;
             Vector2 source = pos - offset;
-
             if (Main.rand.NextChance(0.05)) {
                 Dust dust = Dust.NewDustPerfect(source, ModContent.DustType<Electric>(), Utils.SafeNormalize(source.DirectionTo(dest), Vector2.Zero) * 7.5f * Main.rand.NextFloat(0.25f, 1f), 0, Color.White);
                 dust.rotation = Main.rand.NextFloat(MathHelper.TwoPi);
-                dust.scale *= 0.325f;
+                dust.scale *= 0.4f + Main.rand.NextFloatRange(0.15f);
                 dust.fadeIn = dust.scale + 0.1f;
                 dust.noGravity = true;
             }
-
             float rotation = (dest - source).ToRotation();
             float thicknessScale = _thickness / _segmentTexture.Height();
             Vector2 capOrigin = new(_endTexture.Width(), _endTexture.Height() / 2f);
