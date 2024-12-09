3752d80ad8987086252bf0568972e8e8533758f7
Serork <mandrygin.2013@mail.ru>
2024-12-09 07:55:42 +0400

    Adjustments

---
+++ Content/Projectiles/Friendly/TectonicCaneProjectile2.cs
@@ -0,0 +1,65 @@
+﻿using RoA.Content.Dusts;
+using System;
+using Terraria.DataStructures;
+using Terraria.ModLoader;
+using Terraria;
+using Microsoft.Xna.Framework;
+
+namespace RoA.Content.Projectiles.Friendly;
+
+sealed class TectonicCaneProjectile2 : NatureProjectile {
+    public override void SetStaticDefaults() {
+        Main.projFrames[Type] = 2;
+    }
+
+    protected override void SafeSetDefaults() {
+        Projectile.Size = new Vector2(18f, 16f);
+        Projectile.aiStyle = 0;
+        Projectile.friendly = true;
+        Projectile.timeLeft = 300;
+        Projectile.penetrate = 1;
+
+        Projectile.usesLocalNPCImmunity = true;
+        Projectile.localNPCHitCooldown = 20;
+    }
+
+    protected override void SafeOnSpawn(IEntitySource source) {
+        Projectile.frame = Main.rand.NextBool().ToInt();
+    }
+
+    public override void AI() {
+        float value = Projectile.velocity.Length();
+        Projectile.direction = (int)Projectile.ai[0];
+        Projectile.rotation += value * 0.05f * Projectile.direction;
+        Projectile.velocity.Y += 0.1f;
+        Projectile.velocity.Y = Math.Min(10f, Projectile.velocity.Y);
+    }
+
+    public override bool OnTileCollide(Vector2 oldVelocity) {
+        if (Projectile.ai[1] != 1f) {
+            float max = Math.Max(oldVelocity.X, oldVelocity.Y);
+            if (oldVelocity.X == max) {
+                oldVelocity.X = 0f;
+            }
+            else {
+                oldVelocity.Y = 0f;
+            }
+            Projectile.ai[1] = 1f;
+        }
+        else {
+            Projectile.velocity *= 0.97f;
+        }
+
+        return false;
+    }
+
+    public override void OnKill(int timeLeft) {
+        for (int i = 0; i < 6; i++) {
+            Dust dust = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, ModContent.DustType<TectonicDust>(),
+                Scale: Main.rand.NextFloat(0.95f, 1.05f) * 1.2f);
+            dust.velocity *= Main.rand.NextFloat();
+            dust.velocity *= 0.7f;
+            dust.noGravity = true;
+        }
+    }
+}
---
+++ Content/VisualEffects/TectonicDebris.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Xna.Framework;
+using RoA.Common.VisualEffects;
+using System;
+using Terraria.Graphics.Renderers;
+using Terraria;
+
+namespace RoA.Content.VisualEffects;
+
+sealed class TectonicDebris : VisualEffect<TectonicDebris> {
+    protected override void SetDefaults() {
+        MaxTimeLeft = TimeLeft = 120;
+
+        SetFramedTexture(3);
+    }
+
+    public override void Update(ref ParticleRendererSettings settings) {
+        float length = Velocity.Length();
+        Rotation += length * 0.0314f;
+
+        DrawColor = Lighting.GetColor(Position.ToTileCoordinates());
+
+        if (TimeLeft <= 30) {
+            Scale = Utils.GetLerpValue(0, 30, TimeLeft, true);
+        }
+        else {
+            Scale = MathHelper.Lerp(Scale, 1f, 0.015f);
+        }
+
+        if (AI0 == 0f) {
+            Velocity *= 0.95f;
+            if (Velocity.Length() < 0.1f) {
+                AI0 = 1f;
+                Velocity *= 0.5f;
+            }
+        }
+        else {
+            Velocity.X *= 0.5f;
+            Velocity.Y += 0.1f;
+            Velocity.Y = Math.Min(10f, Velocity.Y);
+        }
+
+        Position += Velocity;
+
+        if (Scale <= 0.1f || float.IsNaN(Scale) || --TimeLeft <= 0) {
+            RestInPool();
+        }
+    }
+}
