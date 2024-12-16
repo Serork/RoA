d8e1afac93a65a035e8e83af2219f2ffb1ef896a
Serork <mandrygin.2013@mail.ru>
2024-12-16 22:06:33 +0400

    Adjustments

---
+++ Content/Dusts/NaturesHeartDust.cs
@@ -0,0 +1,21 @@
+using Microsoft.Xna.Framework;
+
+using Newtonsoft.Json.Linq;
+
+using Terraria;
+using Terraria.ModLoader;
+
+namespace RoA.Content.Dusts;
+
+sealed class NaturesHeartDust : ModDust {
+    public override Color? GetAlpha(Dust dust, Color lightColor) => Color.Lerp(lightColor, Color.White, 0.5f);
+
+    public override void OnSpawn(Dust dust) => UpdateType = 184;
+
+    public override bool PreDraw(Dust dust) {
+        Lighting.AddLight(dust.position + Vector2.UnitX * 2f,
+     (Color.Green * 1f).ToVector3() * 0.5f);
+
+        return true;
+    }
+}
--- Content/Items/Materials/NaturesHeart.cs
+++ Content/Items/Materials/NaturesHeart.cs
@@ -3,6 +3,9 @@ using Microsoft.Xna.Framework.Graphics;
 
 using Newtonsoft.Json.Linq;
 
+using RoA.Content.Dusts;
+using RoA.Core.Utility;
+
 using Terraria;
 using Terraria.DataStructures;
 using Terraria.GameContent;
@@ -37,7 +40,8 @@ sealed class NaturesHeart : ModItem {
 
     public override void PostDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, float rotation, float scale, int whoAmI) {
         float value = 0.2f;
-        switch (Main.itemAnimations[Type].GetFrame(TextureAssets.Item[Type].Value, Main.itemFrameCounter[whoAmI]).Y / Item.height) {
+        int frame = Main.itemAnimations[Type].GetFrame(TextureAssets.Item[Type].Value, Main.itemFrameCounter[whoAmI]).Y / Item.height;
+        switch (frame) {
             case 1 or 5:
                 value = 0.4f;
                 break;
@@ -48,7 +52,32 @@ sealed class NaturesHeart : ModItem {
                 value = 0.8f;
                 break;
         }
+        int type = ModContent.DustType<NaturesHeartDust>();
+        if (Main.rand.NextBool(20)) {
+            for (int num740 = 0; num740 < 1; num740++) {
+                if (Main.rand.NextChance(value / 2f)) {
+                    int num741 = Dust.NewDust(new Vector2(Item.position.X, Item.position.Y), Item.width, Item.height, type);
+                    Dust dust2 = Main.dust[num741];
+                    dust2.velocity *= 0.5f;
+                    dust2.scale *= 0.9f;
+                    Main.dust[num741].noGravity = true;
+                }
+            }
+        }
+        if (Main.rand.NextBool(20)) {
+            for (int num742 = 0; num742 < 1; num742++) {
+                if (Main.rand.NextChance(value / 2f)) {
+                    int num743 = Dust.NewDust(new Vector2(Item.position.X, Item.position.Y), Item.width, Item.height, type);
+                    Dust dust2 = Main.dust[num743];
+                    dust2.velocity *= 2.5f;
+                    dust2.velocity *= 0.5f;
+                    dust2 = Main.dust[num743];
+                    dust2.scale *= 0.7f;
+                    Main.dust[num743].noGravity = true;
+                }
+            }
+        }
         Lighting.AddLight(Item.Center + Vector2.UnitX * 2f,
-            (Color.Green * value).ToVector3() * 0.6f);
+            (Color.Green * value).ToVector3() * 0.75f);
     }
 }
