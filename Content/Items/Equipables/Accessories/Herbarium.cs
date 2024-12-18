a1d37253424f0a8f7f0977fd2b7bc9c167e378ea
Serork <mandrygin.2013@mail.ru>
2024-12-18 11:41:58 +0400

    Content

---
+++ Content/Items/Equipables/Accessories/Herbarium.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Xna.Framework;
+
+using RoA.Content.Items.Materials;
+using RoA.Content.Items.Miscellaneous;
+using RoA.Core.Utility;
+
+using Terraria;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace RoA.Content.Items.Equipables.Accessories;
+
+sealed class Herbarium : NatureItem {
+    public override void SetStaticDefaults() {
+        //DisplayName.SetDefault("Herbarium");
+        //Tooltip.SetDefault("Enemies can drop healing herbs when wreath is fully charged");
+        CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+    }
+
+    protected override void SafeSetDefaults() {
+        int width = 28; int height = 32;
+        Item.Size = new Vector2(width, height);
+
+        Item.rare = ItemRarityID.Orange;
+        Item.accessory = true;
+        Item.value = Item.sellPrice(gold: 2);
+    }
+
+    public override void UpdateAccessory(Player player, bool hideVisual) => player.GetModPlayer<HerbariumPlayer>().healingHerb = true;
+
+    //public override void AddRecipes() {
+    //    CreateRecipe()
+    //        .AddIngredient(ItemID.Book)
+    //        .AddIngredient(ItemID.Daybloom)
+    //        .AddIngredient(ItemID.Shiverthorn)
+    //        .AddIngredient(ItemID.Blinkroot)
+    //        .AddIngredient(ItemID.Waterleaf)
+    //        .AddIngredient(ItemID.Deathweed)
+    //        .AddIngredient(ItemID.Moonglow)
+    //        .AddIngredient(ModContent.ItemType<MiracleMint>())
+    //        .AddIngredient(ModContent.ItemType<Bonerose>())
+    //        .AddIngredient(ItemID.Fireblossom)
+    //        .AddTile(TileID.Bookcases)
+    //        .Register();
+    //}
+}
+
+internal class HerbariumPlayer : ModPlayer {
+    public bool healingHerb;
+
+    public override void ResetEffects()
+        => healingHerb = false;
+
+    public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone) {
+        if (healingHerb && target.life <= damageDone && Player.GetSelectedItem().IsADruidicWeapon() && Main.rand.Next(2) == 0) {
+            int getHerb() {
+                int rand = Main.rand.Next(3);
+                return rand switch {
+                    0 => ModContent.ItemType<MagicHerb1>(),
+                    1 => ModContent.ItemType<MagicHerb2>(),
+                    _ => ModContent.ItemType<MagicHerb3>(),
+                };
+            }
+            Item.NewItem(Player.GetSource_OnHit(target), (int)target.position.X, (int)target.position.Y, target.width, target.height, getHerb());
+        }
+    }
+}
---
+++ Content/Items/Miscellaneous/MagicHerb.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Xna.Framework;
+
+using RoA.Common.Druid.Wreath;
+
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace RoA.Content.Items.Miscellaneous;
+
+class MagicHerb3 : MagicHerb1 { }
+class MagicHerb2 : MagicHerb1 { }
+class MagicHerb1 : ModItem {
+	public override void SetStaticDefaults() {
+		//DisplayName.SetDefault("Magic Herb");
+		ItemID.Sets.ItemIconPulse[Item.type] = true;
+	}
+
+	public override Color? GetAlpha(Color lightColor) => Color.White;
+
+	public override void SetDefaults() {
+		int width = 20; int height = width;
+		Item.Size = new Vector2(width, height);
+	}
+
+	public override bool ItemSpace(Player player) => true;
+
+	public override bool CanPickup(Player player) => true;
+
+	public override void PostUpdate() {
+		_ = Main.rand.Next(90, 111) * 0.01f * (Main.essScale * 0.5f);
+		Lighting.AddLight(Item.Center, Color.LightGreen.ToVector3() * 0.5f * Main.essScale);
+	}
+
+	public override bool OnPickup(Player player) {
+		if (player.GetModPlayer<WreathHandler>().IsFull) {
+			player.statLife += 40;
+			if (Main.myPlayer == player.whoAmI) {
+				player.HealEffect(40);
+			}
+		}
+
+		SoundEngine.PlaySound(SoundID.Item60, player.position);
+		return false;
+	}
+}
