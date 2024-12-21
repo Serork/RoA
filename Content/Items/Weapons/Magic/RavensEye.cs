d80785e717120b3756169186df2a2371cd970562
Serork <mandrygin.2013@mail.ru>
2024-12-21 13:34:56 +0400

    Ravens Eye

---
+++ Content/Items/Weapons/Magic/RavensEye.cs
@@ -0,0 +1,66 @@
+using Microsoft.Xna.Framework;
+
+using RoA.Content.Projectiles.Friendly.Magic;
+
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace RoA.Content.Items.Weapons.Magic;
+
+sealed class RavensEye : ModItem {
+    public override void SetStaticDefaults() {
+        // DisplayName.SetDefault("Raven's Eye");
+        // Tooltip.SetDefault("");
+
+        Item.ResearchUnlockCount = 1;
+    }
+
+    public override void SetDefaults() {
+        Item.staff[Item.type] = true;
+
+        int width = 38; int height = 40;
+        Item.Size = new Vector2(width, height);
+
+        Item.useStyle = ItemUseStyleID.Shoot;
+        Item.useTime = Item.useAnimation = 45;
+        Item.autoReuse = false;
+
+        Item.DamageType = DamageClass.Magic;
+        Item.damage = 20;
+        Item.knockBack = 1;
+
+        Item.noMelee = true;
+        Item.channel = true;
+        Item.mana = 10;
+
+        Item.value = Item.sellPrice(silver: 45);
+        Item.rare = ItemRarityID.Green;
+        Item.UseSound = SoundID.Item105;
+
+        Item.shoot = ModContent.ProjectileType<BloodyFeather>();
+        Item.shootSpeed = 12f;
+    }
+
+    public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback) {
+        Vector2 newVelocity = new Vector2(velocity.X, velocity.Y).SafeNormalize(Vector2.Zero);
+        position += newVelocity * 40;
+        position += new Vector2(-newVelocity.Y, newVelocity.X) * (-5f * player.direction);
+    }
+
+    public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback) {
+        Vector2 funnyOffset = Vector2.Normalize(new Vector2(velocity.X, velocity.Y)) * 5f;
+        position += funnyOffset - new Vector2(player.direction == -1 ? 0f : 8f, -2f * player.direction).RotatedBy(funnyOffset.ToRotation());
+        for (int i = 0; i < 5; i++) {
+            if (Main.rand.NextBool()) {
+                int dust = Dust.NewDust(position, 0, 0, 60, 0, 0.5f, 0, default, 2.3f + 0.5f * Main.rand.NextFloat());
+                Main.dust[dust].noGravity = true;
+            }
+        }
+        for (int i = 0; i < 3; i++) {
+            Projectile.NewProjectile(source, new Vector2(player.Center.X + Main.rand.NextFloat(-60, 60), player.Center.Y + Main.rand.NextFloat(-30, 20)), new Vector2(velocity.X, velocity.Y), type, damage, knockback, player.whoAmI);
+        }
+        return false;
+    }
+}
---
+++ Content/Projectiles/Friendly/Magic/BloodyFeather.cs
@@ -0,0 +1,73 @@
+using Microsoft.Xna.Framework;
+
+using System;
+
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace RoA.Content.Projectiles.Friendly.Magic;
+
+sealed class BloodyFeather : ModProjectile {
+    private int _direction;
+    private bool _flag = true;
+
+    //public override void SetStaticDefaults()	
+    //	=> DisplayName.SetDefault("Bloody Feather");
+
+    public override void SetDefaults() {
+        Projectile.CloneDefaults(ProjectileID.WoodenArrowFriendly);
+
+        int width = 10; int height = 20;
+        Projectile.Size = new Vector2(width, height);
+
+        Projectile.aiStyle = -1;
+
+        Projectile.friendly = true;
+        Projectile.DamageType = DamageClass.Magic;
+
+        Projectile.penetrate = 1;
+        Projectile.timeLeft = 120;
+
+        Projectile.extraUpdates = 1;
+        Projectile.alpha = 255;
+    }
+
+    public override void AI() {
+        if (Projectile.alpha >= 0) Projectile.alpha -= 17;
+        if (Projectile.timeLeft == 60) Projectile.tileCollide = true;
+        if (_flag) {
+            Player player = Main.player[Projectile.owner];
+            _direction = -player.direction;
+            _flag = false;
+        }
+        Projectile.rotation = (float)Math.Atan2(Projectile.velocity.Y, Projectile.velocity.X) + 1.57f;
+        Projectile.ai[0]++;
+        if (Projectile.ai[0] % 8 == 0 && Projectile.velocity.Length() > 6f) {
+            int dust = Dust.NewDust(new Vector2(Projectile.position.X, Projectile.position.Y), 1, 1, Main.rand.NextBool(3) ? 60 : 96, Projectile.velocity.X, Projectile.velocity.Y, 0, default, 0.8f);
+            Main.dust[dust].velocity *= 0.75f;
+        }
+        Projectile.velocity.X = (float)(Math.Cos(60) * Projectile.ai[0] / 8) * _direction;
+        Projectile.velocity.X *= 1.2f;
+        Projectile.velocity.Y *= 0.98f;
+    }
+
+    public override bool TileCollideStyle(ref int width, ref int height, ref bool fallThrough, ref Vector2 hitboxCenterFrac) {
+        width = height = 10;
+        if (Projectile.timeLeft > 10) return false;
+        else return true;
+    }
+
+    public override void OnKill(int timeLeft) {
+        for (int i = 0; i < 4; i++) {
+            int dust2 = Dust.NewDust(new Vector2(Projectile.position.X, Projectile.position.Y), 1, 1, Main.rand.NextBool(3) ? 60 : 96, 0, 0, 0, default, Main.rand.NextFloat(1f, 1.2f) * 1.25f);
+            Main.dust[dust2].noGravity = true;
+            Main.dust[dust2].fadeIn = 0.75f;
+
+            if (Main.rand.NextBool()) {
+                int dust = Dust.NewDust(new Vector2(Projectile.position.X, Projectile.position.Y), 1, 1, Main.rand.NextBool(3) ? 60 : 96, Projectile.velocity.X * 0.5f, Projectile.velocity.Y * 0.5f, 0, default, Main.rand.NextFloat(1f, 1.2f));
+                Main.dust[dust].velocity *= 0.75f;
+            }
+        }
+    }
+}
