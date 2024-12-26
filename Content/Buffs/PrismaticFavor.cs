01dd358b3d56716c9d31ed853c40e8594d553b9f
Serork <mandrygin.2013@mail.ru>
2024-12-26 14:52:38 +0400

    Prismatic potion

---
+++ Content/Buffs/PrismaticFavor.cs
@@ -0,0 +1,52 @@
+using Terraria;
+using Terraria.ModLoader;
+namespace RoA.Content.Buffs;
+
+sealed class PrismaticFavor : ModBuff {
+	public override void SetStaticDefaults() {
+		// DisplayName.SetDefault("Bloodlust");
+		// Description.SetDefault("You restore some health on deadly hits");
+	}
+
+	public override void Update(Player player, ref int buffIndex) => player.GetModPlayer<PrismaticFavorPlayer>().IsEffectActive = true;
+
+    internal class PrismaticFavorPlayer : ModPlayer {
+        private float _totalDamage;
+
+        public bool IsEffectActive;
+
+        public override void ResetEffects() => IsEffectActive = false;
+        public override void PostUpdate() {
+            if (!IsEffectActive) {
+                return;
+            }
+
+            float totalDamage = 0f;
+            bool isValid(DamageClass damageClass) {
+                return damageClass is not null && !(damageClass == DamageClass.Default ||
+                    damageClass == DamageClass.Generic ||
+                    damageClass == DamageClass.MeleeNoSpeed ||
+                    damageClass == DamageClass.SummonMeleeSpeed ||
+                    damageClass == DamageClass.MagicSummonHybrid);
+            }
+            for (int i = 0; i < DamageClassLoader.DamageClassCount; i++) {
+                DamageClass damageClass = DamageClassLoader.GetDamageClass(i);
+                if (!isValid(damageClass)) {
+                    continue;
+                }
+                float value = Player.GetTotalDamage(damageClass).ApplyTo(1f);
+                totalDamage += value - 1f;
+            }
+            for (int i = 0; i < DamageClassLoader.DamageClassCount; i++) {
+                DamageClass damageClass = DamageClassLoader.GetDamageClass(i);
+                if (!isValid(damageClass)) {
+                    continue;
+                }
+                float addDamage = totalDamage;
+                float value = Player.GetTotalDamage(damageClass).ApplyTo(1f);
+                addDamage -= value - 1f;
+                Player.GetDamage(damageClass) += addDamage;
+            }
+        }
+    }
+}
---
+++ Content/Items/Potions/PrismaticPotion.cs
@@ -0,0 +1,49 @@
+using Microsoft.Xna.Framework;
+
+using RoA.Content.Buffs;
+
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace RoA.Content.Items.Potions;
+
+sealed class PrismaticPotion : ModItem {
+	public override void SetStaticDefaults() {
+        Item.ResearchUnlockCount = 20;
+        ItemID.Sets.DrinkParticleColors[Type] = new Color[4] {
+			new Color(169, 226, 232),
+			new Color(218, 62, 184),
+			new Color(195, 17, 79),
+            new Color(88, 111, 156)
+        };
+	}
+
+	public override void SetDefaults() {
+		int width = 20; int height = 30;
+		Item.Size = new Vector2(width, height);
+
+        Item.maxStack = 9999;
+        Item.rare = ItemRarityID.Green;
+
+		Item.useTime = Item.useAnimation = 15;
+		Item.useStyle = ItemUseStyleID.DrinkLiquid;
+
+		Item.UseSound = SoundID.Item3;
+		Item.consumable = true;
+
+		Item.buffType = ModContent.BuffType<PrismaticFavor>();
+		Item.buffTime = 3600 * 4;
+	}
+
+	//public override void AddRecipes() {
+	//	CreateRecipe()
+	//		.AddIngredient(ItemID.BottledWater)
+	//		.AddIngredient<Bonerose>()
+	//		.AddIngredient(ItemID.Daybloom)
+	//		.AddIngredient(ItemID.Deathweed)
+	//		.AddIngredient(ItemID.Worm)
+	//		.AddTile(TileID.Bottles)
+	//		.Register();
+	//}
+}
