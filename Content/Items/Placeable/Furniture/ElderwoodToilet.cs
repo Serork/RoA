0fb5e1a8d2926d2f71b8c7086fd57ce96f77c814
Serork <mandrygin.2013@mail.ru>
2025-01-02 21:25:58 +0400

    Update ElderwoodToilet.cs

--- Content/Items/Placeable/Furniture/ElderwoodToilet.cs
+++ Content/Items/Placeable/Furniture/ElderwoodToilet.cs
@@ -7,7 +7,7 @@ namespace RoA.Content.Items.Placeable.Furniture;
 
 sealed class ElderwoodToilet : ModItem {
 	public override void SetDefaults() {
-		int width = 32; int height = 18;
+		int width = 16; int height = 30;
 		Item.Size = new Vector2(width, height);
 
         Item.DefaultToPlaceableTile(ModContent.TileType<Tiles.Furniture.ElderwoodToilet>(), 0);
