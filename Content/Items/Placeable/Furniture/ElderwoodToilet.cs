7e2ce363a022bd2be8284ddac3c59ef90615ec4e
Serork <mandrygin.2013@mail.ru>
2025-01-02 20:46:38 +0400

    NEW TOILET (ALERT!!!)

---
+++ Content/Items/Placeable/Furniture/ElderwoodToilet.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+
+using Terraria;
+using Terraria.ModLoader;
+
+namespace RoA.Content.Items.Placeable.Furniture;
+
+sealed class ElderwoodToilet : ModItem {
+	public override void SetDefaults() {
+		int width = 32; int height = 18;
+		Item.Size = new Vector2(width, height);
+
+        Item.DefaultToPlaceableTile(ModContent.TileType<Tiles.Furniture.ElderwoodToilet>(), 0);
+        Item.maxStack = Item.CommonMaxStack;
+        Item.value = 150;
+    }
+
+	//public override void AddRecipes() {
+	//	CreateRecipe()
+	//		.AddIngredient<Elderwood>(10)
+	//		.Register();
+	//}
+}
