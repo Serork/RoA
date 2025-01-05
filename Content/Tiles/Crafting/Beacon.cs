a2de49deda6920cc506b38af86450899d05e0938
Serork <mandrygin.2013@mail.ru>
2025-01-06 02:57:42 +0400

    fix fix fix

--- Content/Tiles/Crafting/Beacon.cs
+++ Content/Tiles/Crafting/Beacon.cs
@@ -1,110 +1,101 @@
 using Microsoft.Xna.Framework;
 
-using RoA.Content.Items.Placeable.Crafting;
-using RoA.Content.UI;
 using RoA.Core.Utility;
 
 using System;
-using System.Collections.Generic;
 using System.Linq;
 
 using Terraria;
 using Terraria.Audio;
 using Terraria.DataStructures;
-using Terraria.GameContent.Drawing;
 using Terraria.GameContent.ObjectInteractions;
 using Terraria.ID;
 using Terraria.ModLoader;
-using Terraria.ModLoader.IO;
 using Terraria.ObjectData;
-
-using static RoA.Content.Tiles.Crafting.BeaconTE;
-using static Terraria.GameContent.Animations.Actions.NPCs;
-using static Terraria.GameContent.Animations.Actions.Sprites;
 
 namespace RoA.Content.Tiles.Crafting;
 
-sealed class BeaconTE : ModTileEntity {
-    public static readonly short[] Gems = [ItemID.Amethyst, ItemID.Topaz, ItemID.Sapphire, ItemID.Emerald, ItemID.Ruby, ItemID.Diamond, ItemID.Amber];
+//sealed class BeaconTE : ModTileEntity {
+//    public static readonly short[] Gems = [ItemID.Amethyst, ItemID.Topaz, ItemID.Sapphire, ItemID.Emerald, ItemID.Ruby, ItemID.Diamond, ItemID.Amber];
 
-    public enum BeaconVariant : byte {
-        None,
-        Amethyst,
-        Topaz,
-        Sapphire,
-        Emerald,
-        Ruby,
-        Diamond,
-        Amber,
-        Length
-    }
+//    public enum BeaconVariant : byte {
+//        None,
+//        Amethyst,
+//        Topaz,
+//        Sapphire,
+//        Emerald,
+//        Ruby,
+//        Diamond,
+//        Amber,
+//        Length
+//    }
 
-    private BeaconVariant _variant = BeaconVariant.None;
+//    private BeaconVariant _variant = BeaconVariant.None;
 
-    public bool HasGemInIt => GetItemID(num3: 0) != -1;
+//    public bool HasGemInIt => GetItemID(num3: 0) != -1;
 
-    public void RemoveGem() => _variant = BeaconVariant.None;
+//    public void RemoveGem() => _variant = BeaconVariant.None;
 
-    public void DropGem(Player player, int x, int y) {
-        if (!HasGemInIt) {
-            return;
-        }
+//    public void DropGem(Player player, int x, int y) {
+//        if (!HasGemInIt) {
+//            return;
+//        }
 
-        int num = Item.NewItem(player.GetSource_Misc("frombeacon"), x, y, 18, 18, GetItemID());
-        if (Main.netMode == NetmodeID.MultiplayerClient) {
-            NetMessage.SendData(MessageID.SyncItem, -1, -1, null, num);
-        }
-    }
+//        int num = Item.NewItem(player.GetSource_Misc("frombeacon"), x, y, 18, 18, GetItemID());
+//        if (Main.netMode == NetmodeID.MultiplayerClient) {
+//            NetMessage.SendData(MessageID.SyncItem, -1, -1, null, num);
+//        }
+//    }
 
-    public void InsertGem(short gemType) {
-        for (int i = 0; i < Gems.Length; i++) {
-            if (Gems[i] == gemType) {
-                _variant = (BeaconVariant)(i + 1);
-            }
-        }
-    }
+//    public void InsertGem(short gemType) {
+//        for (int i = 0; i < Gems.Length; i++) {
+//            if (Gems[i] == gemType) {
+//                _variant = (BeaconVariant)(i + 1);
+//            }
+//        }
+//    }
 
-    public byte GetVariant(bool forDrawing = true, int num = 0) {
-        bool flag = forDrawing && _variant == BeaconVariant.None;
-        return Math.Max((byte)((byte)(flag ? BeaconVariant.Length : _variant) + (flag ? -1 : 0) + num), (byte)0);
-    }
+//    public byte GetVariant(bool forDrawing = true, int num = 0) {
+//        bool flag = forDrawing && _variant == BeaconVariant.None;
+//        return Math.Max((byte)((byte)(flag ? BeaconVariant.Length : _variant) + (flag ? -1 : 0) + num), (byte)0);
+//    }
 
-    public int GetItemID(bool forDrawing = true, int num2 = -1, int num3 = -1) {
-        int num = num2 != -1 ? num2 : GetVariant(forDrawing, num3);
-        if (_variant != BeaconVariant.None && num == 7) {
-            num = 6;
-        }
-        return num switch {
-            >= 0 and <= 6 => Gems[num],
-            _ => -1,
-        };
-    }
+//    public int GetItemID(bool forDrawing = true, int num2 = -1, int num3 = -1) {
+//        int num = num2 != -1 ? num2 : GetVariant(forDrawing, num3);
+//        if (_variant != BeaconVariant.None && num == 7) {
+//            num = 6;
+//        }
+//        return num switch {
+//            >= 0 and <= 6 => Gems[num],
+//            _ => -1,
+//        };
+//    }
 
-    public override void SaveData(TagCompound tag) {
-        tag[nameof(BeaconTE)] = (byte)_variant;
-    }
+//    public override void SaveData(TagCompound tag) {
+//        tag[nameof(BeaconTE)] = (byte)_variant;
+//    }
 
-    public override void LoadData(TagCompound tag) {
-        _variant = (BeaconVariant)tag.Get<byte>(nameof(BeaconTE));
-    }
+//    public override void LoadData(TagCompound tag) {
+//        _variant = (BeaconVariant)tag.Get<byte>(nameof(BeaconTE));
+//    }
 
-    public override int Hook_AfterPlacement(int i, int j, int type, int style, int direction, int alternate) {
-        if (Main.netMode == NetmodeID.MultiplayerClient) {
-            NetMessage.SendTileSquare(Main.myPlayer, i, j);
-            NetMessage.SendData(MessageID.TileEntityPlacement, -1, -1, null, i, j, Type);
+//    public override int Hook_AfterPlacement(int i, int j, int type, int style, int direction, int alternate) {
+//        if (Main.netMode == NetmodeID.MultiplayerClient) {
+//            NetMessage.SendTileSquare(Main.myPlayer, i, j);
+//            NetMessage.SendData(MessageID.TileEntityPlacement, -1, -1, null, i, j, Type);
 
-            return -1;
-        }
+//            return -1;
+//        }
 
-        return Place(i, j);
-    }
+//        return Place(i, j);
+//    }
 
-    public override void OnKill() => DropGem(Main.LocalPlayer, (int)(Position.X * 16f), (int)((Position.Y - 2) * 16f + 8f));
+//    public override void OnKill() => DropGem(Main.LocalPlayer, (int)(Position.X * 16f), (int)((Position.Y - 2) * 16f + 8f));
 
-    public override void OnNetPlace() => NetMessage.SendData(MessageID.TileEntitySharing, -1, -1, null, ID, Position.X, Position.Y, 0f, 0, 0, 0);
+//    public override void OnNetPlace() => NetMessage.SendData(MessageID.TileEntitySharing, -1, -1, null, ID, Position.X, Position.Y, 0f, 0, 0, 0);
 
-    public override bool IsTileValidForEntity(int i, int j) => WorldGenHelper.GetTileSafely(i, j).ActiveTile(ModContent.TileType<Beacon>());
-}
+//    public override bool IsTileValidForEntity(int i, int j) => WorldGenHelper.GetTileSafely(i, j).ActiveTile(ModContent.TileType<Beacon>());
+//}
 
 sealed class Beacon : ModTile {
     private static int _variantToShow;
@@ -137,63 +128,63 @@ sealed class Beacon : ModTile {
 
     //public override void KillTile(int i, int j, ref bool fail, ref bool effectOnly, ref bool noItem) => ModContent.GetInstance<BeaconTE>().Kill(i, j);
 
-    private static BeaconTE GetTE(int i, int j) {
-        BeaconTE foundTE = null;
-        int j2 = 0;
-        TileObjectData tileObjectData = TileObjectData.GetTileData(WorldGenHelper.GetTileSafely(i, j));
-        if (tileObjectData == null) {
-            return null;
-        }
-        while (j2 < tileObjectData.CoordinateHeights.Length) {
-            BeaconTE desiredTE = TileHelper.GetTE<BeaconTE>(i, j + j2);
-            if (desiredTE is null) {
-                j2++;
-                continue;
-            }
+    //private static BeaconTE GetTE(int i, int j) {
+    //    BeaconTE foundTE = null;
+    //    int j2 = 0;
+    //    TileObjectData tileObjectData = TileObjectData.GetTileData(WorldGenHelper.GetTileSafely(i, j));
+    //    if (tileObjectData == null) {
+    //        return null;
+    //    }
+    //    while (j2 < tileObjectData.CoordinateHeights.Length) {
+    //        BeaconTE desiredTE = TileHelper.GetTE<BeaconTE>(i, j + j2);
+    //        if (desiredTE is null) {
+    //            j2++;
+    //            continue;
+    //        }
 
-            foundTE = desiredTE;
-            break;
-        }
+    //        foundTE = desiredTE;
+    //        break;
+    //    }
 
-        return foundTE;
-    }
+    //    return foundTE;
+    //}
 
-    private static bool IsTileValidToBeHovered(int i, int j) {
-        if (WorldGenHelper.GetTileSafely(i, j + 1).TileType != ModContent.TileType<Beacon>()) {
+    private bool IsTileValidToBeHovered(int i, int j) {
+        if (WorldGenHelper.GetTileSafely(i, j + 1).TileType != Type) {
             return false;
         }
 
         return true;
     }
 
-    public static int VariantToShow => _variantToShow;
+    //public static int VariantToShow => _variantToShow;
 
-    public static void UpdateVariants() {
-        double time = 50.0;
-        if (Main.timeForVisualEffects % time == 0.0) {
-            _variantToShow++;
-            if (_variantToShow >= Enum.GetNames(typeof(BeaconTE.BeaconVariant)).Length - 2) {
-                _variantToShow = 0;
-            }
-        }
-    }
+    //public static void UpdateVariants() {
+    //    double time = 50.0;
+    //    if (Main.timeForVisualEffects % time == 0.0) {
+    //        _variantToShow++;
+    //        if (_variantToShow >= Enum.GetNames(typeof(BeaconTE.BeaconVariant)).Length - 2) {
+    //            _variantToShow = 0;
+    //        }
+    //    }
+    //}
 
-    public static int GetGemItemID(int i, int j) {
-        BeaconTE te = GetTE(i, j);
-        if (te is null) {
-            return -1;
-        }
+    //public static int GetGemItemID(int i, int j) {
+    //    BeaconTE te = GetTE(i, j);
+    //    if (te is null) {
+    //        return -1;
+    //    }
 
-        bool flag2 = te.HasGemInIt;
-        bool flag = !flag2 && ModContent.GetInstance<BeaconInterface>().Active;
-        if (flag) {
-            UpdateVariants();
-            return te.GetItemID(false, VariantToShow);
-        }
-        else {
-            return flag2 ? te.GetItemID() : ModContent.ItemType<Items.Placeable.Crafting.Beacon>();
-        }
-    }
+    //    bool flag2 = te.HasGemInIt;
+    //    bool flag = !flag2 && ModContent.GetInstance<BeaconInterface>().Active;
+    //    if (flag) {
+    //        UpdateVariants();
+    //        return te.GetItemID(false, VariantToShow);
+    //    }
+    //    else {
+    //        return flag2 ? te.GetItemID() : ModContent.ItemType<Items.Placeable.Crafting.Beacon>();
+    //    }
+    //}
 
     //public override void SetDrawPositions(int i, int j, ref int width, ref int offsetY, ref int height, ref short tileFrameX, ref short tileFrameY) {
     //    BeaconTE te = GetTE(i, j);
--- Content/UI/BeaconInterface.cs
+++ Content/UI/BeaconInterface.cs
@@ -1,173 +1,154 @@
-﻿using Microsoft.CodeAnalysis.Text;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
+﻿namespace RoA.Content.UI;
 
-using RoA.Common.UI;
-using RoA.Content.Tiles.Crafting;
-using RoA.Core;
-using RoA.Core.Utility;
+//sealed class BeaconInterface : UILayer {
+//    private static List<(BeaconTE, Vector2)> _activeBeaconUIs = [];
 
-using System.Collections.Generic;
-using System.Linq;
+//    public BeaconInterface() : base("BeaconInterface", "Vanilla: Inventory", InterfaceScaleType.Game) { }
 
-using Terraria;
-using Terraria.Audio;
-using Terraria.DataStructures;
-using Terraria.ID;
-using Terraria.ModLoader;
-using Terraria.UI;
+//    public override void OnUnload() {
+//        _activeBeaconUIs.Clear();
+//        _activeBeaconUIs = null;
+//    }
 
-namespace RoA.Content.UI;
+//    public static bool HasOpened(BeaconTE beaconTE) => _activeBeaconUIs.Any(x => x.Item1 == beaconTE);
 
-sealed class BeaconInterface : UILayer {
-    private static List<(BeaconTE, Vector2)> _activeBeaconUIs = [];
+//    public static void ToggleUI(int i, int j, BeaconTE beaconTE) {
+//        BeaconInterface beaconInterface = ModContent.GetInstance<BeaconInterface>();
+//        if (!_activeBeaconUIs.Contains((beaconTE, new Point(i, j).ToWorldCoordinates()))) {
+//            beaconInterface.ActivateSelectively(i, j, beaconTE);
+//        }
+//        else {
+//            bool flag = Main.InSmartCursorHighlightArea(i, j, out bool actuallySelected);
+//            if ((flag && (!actuallySelected || !beaconTE.HasGemInIt)) || !flag) {
+//                beaconInterface.DeactivateSelectively(i, j, beaconTE);
+//            }
+//        }
+//    }
 
-    public BeaconInterface() : base("BeaconInterface", "Vanilla: Inventory", InterfaceScaleType.Game) { }
+//    private void ActivateSelectively(int i, int j, BeaconTE beaconTE) {
+//        if (!Active) {
+//            Activate();
+//        }
 
-    public override void OnUnload() {
-        _activeBeaconUIs.Clear();
-        _activeBeaconUIs = null;
-    }
+//        _activeBeaconUIs.Add((beaconTE, new Point(i, j).ToWorldCoordinates()));
 
-    public static bool HasOpened(BeaconTE beaconTE) => _activeBeaconUIs.Any(x => x.Item1 == beaconTE);
+//        SoundEngine.PlaySound(SoundID.MenuOpen);
+//    }
 
-    public static void ToggleUI(int i, int j, BeaconTE beaconTE) {
-        BeaconInterface beaconInterface = ModContent.GetInstance<BeaconInterface>();
-        if (!_activeBeaconUIs.Contains((beaconTE, new Point(i, j).ToWorldCoordinates()))) {
-            beaconInterface.ActivateSelectively(i, j, beaconTE);
-        }
-        else {
-            bool flag = Main.InSmartCursorHighlightArea(i, j, out bool actuallySelected);
-            if ((flag && (!actuallySelected || !beaconTE.HasGemInIt)) || !flag) {
-                beaconInterface.DeactivateSelectively(i, j, beaconTE);
-            }
-        }
-    }
+//    private void DeactivateSelectively(int i, int j, BeaconTE beaconTE) {
+//        if (!Active) {
+//            return;
+//        }
 
-    private void ActivateSelectively(int i, int j, BeaconTE beaconTE) {
-        if (!Active) {
-            Activate();
-        }
+//        _activeBeaconUIs.Remove((beaconTE, new Point(i, j).ToWorldCoordinates()));
 
-        _activeBeaconUIs.Add((beaconTE, new Point(i, j).ToWorldCoordinates()));
+//        SoundEngine.PlaySound(SoundID.MenuClose);
+//    }
 
-        SoundEngine.PlaySound(SoundID.MenuOpen);
-    }
+//    public override bool OnUIUpdate(GameTime gameTime) {
+//        if (Active) {
+//            if (_activeBeaconUIs.Count < 1) {
+//                Deactivate();
+//                return false;
+//            }
+//            else {
+//                for (int i = 0; i < _activeBeaconUIs.Count; i++) {
+//                    (BeaconTE, Vector2) activeBeacon = _activeBeaconUIs[i];
+//                    BeaconTE beaconTE = activeBeacon.Item1;
+//                    Vector2 beaconPosition = activeBeacon.Item2;
+//                    Point beaconTilePosition = beaconPosition.ToTileCoordinates();
+//                    int x = beaconTilePosition.X;
+//                    int y = beaconTilePosition.Y;
+//                    float distance = 85f;
+//                    bool flag = !WorldGenHelper.ActiveTile(x, y, ModContent.TileType<Beacon>()) || Vector2.Distance(Main.LocalPlayer.Center, beaconPosition) > distance;
+//                    if (flag) {
+//                        DeactivateSelectively(x, y, beaconTE);
+//                    }
+//                }
+//            }
+//        }
 
-    private void DeactivateSelectively(int i, int j, BeaconTE beaconTE) {
-        if (!Active) {
-            return;
-        }
+//        return true;
+//    }
 
-        _activeBeaconUIs.Remove((beaconTE, new Point(i, j).ToWorldCoordinates()));
+//    protected override bool DrawSelf() {
+//        SpriteBatch spriteBatch = Main.spriteBatch;
+//        foreach ((BeaconTE, Vector2) activeBeacon in _activeBeaconUIs) {
+//            Texture2D texture = ModContent.Request<Texture2D>(ResourceManager.GUITextures + "Beacon_Icons").Value;
+//            SpriteFrame frame = new(8, 1);
+//            BeaconTE beaconTE = activeBeacon.Item1;
+//            Vector2 beaconPosition = activeBeacon.Item2;
+//            byte variant = beaconTE.GetVariant();
+//            int gemType = beaconTE.GetItemID(num3: 0);
+//            bool hasGemInIt = beaconTE.HasGemInIt;
+//            frame = frame.With(hasGemInIt ? (byte)(variant - 1) : variant, 0);
+//            Rectangle sourceRectangle = frame.GetSourceRectangle(texture);
+//            Vector2 origin = sourceRectangle.Size() / 2f;
+//            Vector2 position = beaconPosition + Vector2.UnitX - origin - Vector2.UnitY * sourceRectangle.Height;
+//            Color color = Color.White;
+//            Vector2 drawPosition = position - Main.screenPosition;
+//            spriteBatch.Draw(texture, drawPosition, frame.GetSourceRectangle(texture), color);
+//            texture = ModContent.Request<Texture2D>(ResourceManager.GUITextures + "Beacon_Icons_Gems").Value;
+//            Player player = Main.LocalPlayer;
+//            if (!hasGemInIt) {
+//                if (player.noThrow != 2) {
+//                    Beacon.UpdateVariants();
+//                }
+//                frame = frame.With((byte)Beacon.VariantToShow, 0);
+//                spriteBatch.Draw(texture, drawPosition, frame.GetSourceRectangle(texture), color.MultiplyRGBA(Color.Black with { A = 100 }));
+//            }
+//            gemType = beaconTE.GetItemID();
+//            Vector2 mousePosition = Main.MouseWorld;
+//            bool flag;
+//            Vector2 adjustedBeaconPosition = beaconPosition;
+//            Point tilePosition = beaconPosition.ToTileCoordinates();
+//            adjustedBeaconPosition.Y -= 8f;
+//            adjustedBeaconPosition.X -= 8f;
+//            flag = mousePosition.Between(adjustedBeaconPosition, adjustedBeaconPosition + Vector2.One * 26f);
+//            bool flag4 = Main.InSmartCursorHighlightArea(tilePosition.X, tilePosition.Y, out bool actuallySelected);
+//            bool flag5 = mousePosition.Between(position, position + sourceRectangle.Size());
+//            if (flag5 ||
+//                flag || 
+//                actuallySelected) {
+//                Item item = player.GetSelectedItem();
+//                if (!Main.mouseItem.IsEmpty()) {
+//                    item = Main.mouseItem;
+//                    player.mouseInterface = true;
+//                }
+//                if (flag5 && hasGemInIt) {
+//                    if (Main.mouseRight && Main.mouseRightRelease) {
+//                        SoundEngine.PlaySound(SoundID.MenuTick, adjustedBeaconPosition);
+//                        beaconTE.DropGem(player, (int)adjustedBeaconPosition.X, (int)adjustedBeaconPosition.Y);
+//                        beaconTE.RemoveGem();
+//                    }
+//                }
+//                short gemTypeToInsert = (short)item.type;
+//                if (!BeaconTE.Gems.Contains(gemTypeToInsert)) {
+//                }
+//                else {
+//                    if (Main.mouseLeft && Main.mouseLeftRelease && !player.mouseInterface) {
+//                        bool flag2 = gemType != item.type;
+//                        if (flag2 || !hasGemInIt) {
+//                            if (hasGemInIt) {
+//                                SoundEngine.PlaySound(SoundID.MenuTick, adjustedBeaconPosition);
+//                                beaconTE.DropGem(player, (int)adjustedBeaconPosition.X, (int)adjustedBeaconPosition.Y);
+//                            }
+//                            if (--item.stack < 0) {
+//                                item.TurnToAir();
+//                            }
+//                            SoundEngine.PlaySound(SoundID.MenuTick, adjustedBeaconPosition);
+//                            beaconTE.InsertGem(gemTypeToInsert);
+//                        }
+//                        else if (!flag2) {
+//                            SoundEngine.PlaySound(SoundID.MenuTick, adjustedBeaconPosition);
+//                            beaconTE.DropGem(player, (int)adjustedBeaconPosition.X, (int)adjustedBeaconPosition.Y);
+//                            beaconTE.RemoveGem();
+//                        }
+//                    }
+//                }
+//            }
+//        }
 
-        SoundEngine.PlaySound(SoundID.MenuClose);
-    }
-
-    public override bool OnUIUpdate(GameTime gameTime) {
-        if (Active) {
-            if (_activeBeaconUIs.Count < 1) {
-                Deactivate();
-                return false;
-            }
-            else {
-                for (int i = 0; i < _activeBeaconUIs.Count; i++) {
-                    (BeaconTE, Vector2) activeBeacon = _activeBeaconUIs[i];
-                    BeaconTE beaconTE = activeBeacon.Item1;
-                    Vector2 beaconPosition = activeBeacon.Item2;
-                    Point beaconTilePosition = beaconPosition.ToTileCoordinates();
-                    int x = beaconTilePosition.X;
-                    int y = beaconTilePosition.Y;
-                    float distance = 85f;
-                    bool flag = !WorldGenHelper.ActiveTile(x, y, ModContent.TileType<Beacon>()) || Vector2.Distance(Main.LocalPlayer.Center, beaconPosition) > distance;
-                    if (flag) {
-                        DeactivateSelectively(x, y, beaconTE);
-                    }
-                }
-            }
-        }
-
-        return true;
-    }
-
-    protected override bool DrawSelf() {
-        SpriteBatch spriteBatch = Main.spriteBatch;
-        foreach ((BeaconTE, Vector2) activeBeacon in _activeBeaconUIs) {
-            Texture2D texture = ModContent.Request<Texture2D>(ResourceManager.GUITextures + "Beacon_Icons").Value;
-            SpriteFrame frame = new(8, 1);
-            BeaconTE beaconTE = activeBeacon.Item1;
-            Vector2 beaconPosition = activeBeacon.Item2;
-            byte variant = beaconTE.GetVariant();
-            int gemType = beaconTE.GetItemID(num3: 0);
-            bool hasGemInIt = beaconTE.HasGemInIt;
-            frame = frame.With(hasGemInIt ? (byte)(variant - 1) : variant, 0);
-            Rectangle sourceRectangle = frame.GetSourceRectangle(texture);
-            Vector2 origin = sourceRectangle.Size() / 2f;
-            Vector2 position = beaconPosition + Vector2.UnitX - origin - Vector2.UnitY * sourceRectangle.Height;
-            Color color = Color.White;
-            Vector2 drawPosition = position - Main.screenPosition;
-            spriteBatch.Draw(texture, drawPosition, frame.GetSourceRectangle(texture), color);
-            texture = ModContent.Request<Texture2D>(ResourceManager.GUITextures + "Beacon_Icons_Gems").Value;
-            Player player = Main.LocalPlayer;
-            if (!hasGemInIt) {
-                if (player.noThrow != 2) {
-                    Beacon.UpdateVariants();
-                }
-                frame = frame.With((byte)Beacon.VariantToShow, 0);
-                spriteBatch.Draw(texture, drawPosition, frame.GetSourceRectangle(texture), color.MultiplyRGBA(Color.Black with { A = 100 }));
-            }
-            gemType = beaconTE.GetItemID();
-            Vector2 mousePosition = Main.MouseWorld;
-            bool flag;
-            Vector2 adjustedBeaconPosition = beaconPosition;
-            Point tilePosition = beaconPosition.ToTileCoordinates();
-            adjustedBeaconPosition.Y -= 8f;
-            adjustedBeaconPosition.X -= 8f;
-            flag = mousePosition.Between(adjustedBeaconPosition, adjustedBeaconPosition + Vector2.One * 26f);
-            bool flag4 = Main.InSmartCursorHighlightArea(tilePosition.X, tilePosition.Y, out bool actuallySelected);
-            bool flag5 = mousePosition.Between(position, position + sourceRectangle.Size());
-            if (flag5 ||
-                flag || 
-                actuallySelected) {
-                Item item = player.GetSelectedItem();
-                if (!Main.mouseItem.IsEmpty()) {
-                    item = Main.mouseItem;
-                    player.mouseInterface = true;
-                }
-                if (flag5 && hasGemInIt) {
-                    if (Main.mouseRight && Main.mouseRightRelease) {
-                        SoundEngine.PlaySound(SoundID.MenuTick, adjustedBeaconPosition);
-                        beaconTE.DropGem(player, (int)adjustedBeaconPosition.X, (int)adjustedBeaconPosition.Y);
-                        beaconTE.RemoveGem();
-                    }
-                }
-                short gemTypeToInsert = (short)item.type;
-                if (!BeaconTE.Gems.Contains(gemTypeToInsert)) {
-                }
-                else {
-                    if (Main.mouseLeft && Main.mouseLeftRelease && !player.mouseInterface) {
-                        bool flag2 = gemType != item.type;
-                        if (flag2 || !hasGemInIt) {
-                            if (hasGemInIt) {
-                                SoundEngine.PlaySound(SoundID.MenuTick, adjustedBeaconPosition);
-                                beaconTE.DropGem(player, (int)adjustedBeaconPosition.X, (int)adjustedBeaconPosition.Y);
-                            }
-                            if (--item.stack < 0) {
-                                item.TurnToAir();
-                            }
-                            SoundEngine.PlaySound(SoundID.MenuTick, adjustedBeaconPosition);
-                            beaconTE.InsertGem(gemTypeToInsert);
-                        }
-                        else if (!flag2) {
-                            SoundEngine.PlaySound(SoundID.MenuTick, adjustedBeaconPosition);
-                            beaconTE.DropGem(player, (int)adjustedBeaconPosition.X, (int)adjustedBeaconPosition.Y);
-                            beaconTE.RemoveGem();
-                        }
-                    }
-                }
-            }
-        }
-
-        return base.DrawSelf();
-    }
-}
+//        return base.DrawSelf();
+//    }
+//}
