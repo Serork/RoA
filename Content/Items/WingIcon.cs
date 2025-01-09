aebc00eb8dfa0e0766a1676bf61ae9b5590c33a3
Serork <mandrygin.2013@mail.ru>
2025-01-10 01:09:46 +0400

    Update WingIcon.cs

--- Content/Items/WingIcon.cs
+++ Content/Items/WingIcon.cs
@@ -6,5 +6,6 @@ namespace RoA.Content.Items;
 sealed class WingIcon : ModItem {
     public override void SetStaticDefaults() {
         ItemID.Sets.ItemsThatShouldNotBeInInventory[Type] = true;
+        ItemID.Sets.Deprecated[Type] = true;
     }
 }
