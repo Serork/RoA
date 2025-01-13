be4211a7d9a82fbd6b18819516e51cfbb8fd1035
Serork <mandrygin.2013@mail.ru>
2025-01-13 17:07:31 +0400

    Update LothorPoison.cs

--- Content/Dusts/LothorPoison.cs
+++ Content/Dusts/LothorPoison.cs
@@ -7,7 +7,7 @@ using Terraria.ModLoader;
 namespace RoA.Content.Dusts;
 
 sealed class LothorPoison : ModDust {
-    public override Color? GetAlpha(Dust dust, Color lightColor) => lightColor;
+    public override Color? GetAlpha(Dust dust, Color lightColor) => Color.White.MultiplyRGB(lightColor);
 
     public override void OnSpawn(Dust dust) => UpdateType = DustID.PoisonStaff;
 }
