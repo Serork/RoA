3899dc53eb83e7aead3ff3701e3f1cff1838c4d6
Serork <mandrygin.2013@mail.ru>
2025-01-15 20:35:28 +0400

    Galipot test

--- Content/Projectiles/Friendly/Druidic/GalipotStream.cs
+++ Content/Projectiles/Friendly/Druidic/GalipotStream.cs
@@ -37,6 +37,8 @@ sealed class GalipotStream : NatureProjectile {
         Projectile.timeLeft = 500;
         Projectile.penetrate = 2;
 
+        Projectile.extraUpdates = 1;
+
         Projectile.frame = Main.rand.Next(3);
     }
 
@@ -76,6 +78,45 @@ sealed class GalipotStream : NatureProjectile {
 
     public override void AI() {
         bool flag2 = IsActive && Projectile.velocity.Length() > 0.5f;
+        //Projectile.hide = flag2;
+        if (flag2) {
+            for (int num132 = 0; num132 < 3; num132++) {
+                float num133 = Projectile.velocity.X / 3f * (float)num132;
+                float num134 = Projectile.velocity.Y / 3f * (float)num132;
+                int num135 = 6;
+                int num136 = Dust.NewDust(new Vector2(Projectile.position.X + (float)num135, Projectile.position.Y + (float)num135), Projectile.width - num135 * 2, Projectile.height - num135 * 2,  ModContent.DustType<Galipot>(), 0f, 0f, 0, default(Color), 1.1f);
+                Main.dust[num136].noGravity = true;
+                Main.dust[num136].noLight = true;
+                Dust dust2 = Main.dust[num136];
+                dust2.velocity *= 0.3f;
+                dust2 = Main.dust[num136];
+                dust2.velocity += Projectile.velocity * 0.5f;
+                Main.dust[num136].position.X -= num133;
+                Main.dust[num136].position.Y -= num134;
+            }
+
+            if (Main.rand.Next(8) == 0) {
+                int num137 = 6;
+                int num138 = Dust.NewDust(new Vector2(Projectile.position.X + (float)num137, Projectile.position.Y + (float)num137), Projectile.width - num137 * 2, Projectile.height - num137 * 2, ModContent.DustType<Galipot>(), 0f, 0f, 0, default(Color), 0.85f);
+                Dust dust2 = Main.dust[num138];
+                dust2.noLight = true;
+                dust2.velocity *= 0.5f;
+                dust2 = Main.dust[num138];
+                dust2.velocity += Projectile.velocity * 0.5f;
+            }
+        }
+        else {
+            int dust = Dust.NewDust(Projectile.position - Vector2.UnitY * Projectile.height / 4, Projectile.width, Projectile.height, ModContent.DustType<Galipot>(), 0f, 0f, 0, default, 1f);
+            Main.dust[dust].scale += Main.rand.Next(40) * 0.01f;
+            Main.dust[dust].noGravity = true;
+            Dust dust2 = Main.dust[dust];
+            dust2.position.X -= 2f;
+            Dust dust3 = Main.dust[dust];
+            dust3.position.Y += 2f;
+            Dust dust4 = Main.dust[dust];
+            dust4.velocity.Y -= 2f;
+        }
+
         //Projectile.hide = flag2;
         //Projectile.rotation = Helper.VelocityAngle(Projectile.velocity) + MathHelper.Pi;
 
