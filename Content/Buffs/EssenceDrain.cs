7a3cde740c95be6b084ae1d3667f99f98906d5a8
peege-on <52748183+peege-on@users.noreply.github.com>
2025-01-17 23:45:15 +0000

    fix

--- Content/Buffs/EssenceDrain.cs
+++ Content/Buffs/EssenceDrain.cs
@@ -1,1 +1,129 @@
-using Microsoft.Xna.Framework;using Microsoft.Xna.Framework.Graphics;using RoA.Core;using System;using Terraria;using Terraria.DataStructures;using Terraria.ID;using Terraria.ModLoader;namespace RoA.Content.Buffs;sealed class EssenceDrain : ModBuff {    public override void SetStaticDefaults() {        // DisplayName.SetDefault("Essence Drain");        // Description.SetDefault("Fast losing life");        Main.buffNoSave[Type] = true;        Main.debuff[Type] = true;        Main.pvpBuff[Type] = true;    }    public override void Update(Player player, ref int buffIndex)        => player.GetModPlayer<EssenceDrainPlayer>().essenceDrain = true;    public override void Update(NPC npc, ref int buffIndex)        => npc.GetGlobalNPC<EssenceDrainNPC>().essenceDrain = true;}internal class EssenceDrainPlayer : ModPlayer {    public bool essenceDrain;    public override void ResetEffects()        => essenceDrain = false;    public override void UpdateBadLifeRegen() {        if (essenceDrain) {            if (Player.lifeRegen > 0) Player.lifeRegen = 0;            Player.lifeRegenTime = 0;            Player.lifeRegen -= 80;        }    }    public override void DrawEffects(PlayerDrawSet drawInfo, ref float r, ref float g, ref float b, ref float a, ref bool fullBright) {        if (essenceDrain) {            if (Main.rand.NextBool(4) && drawInfo.shadow == 0.0) {                int dust = Dust.NewDust(drawInfo.Position - new Vector2(2f, 2f), Player.width + 4, Player.height + 4, DustID.Shadowflame, Player.velocity.X * 0.4f, Player.velocity.Y * 0.4f, 100, Color.DarkViolet, 1.2f);                Main.dust[dust].noGravity = true;                Main.dust[dust].velocity *= 1.8f;                Main.dust[dust].velocity.Y -= 0.5f;            }            Lighting.AddLight(Player.Center, 0.5f, 0f, 0.5f);        }    }}internal class EssenceDrainNPC : GlobalNPC {    public float fadeMult = 0.1f;    public Color color;    public override bool InstancePerEntity => true;    public bool essenceDrain;    public int Source;    private float _essenceDrainTimer = 0;    public override void ResetEffects(NPC npc)        => essenceDrain = false;    public override void UpdateLifeRegen(NPC npc, ref int damage) {        int bonusDamage = 8;        if (essenceDrain) {            if (npc.lifeRegen > 0) npc.lifeRegen = 0;            npc.lifeRegen -= 80;            if (damage < bonusDamage) damage = bonusDamage;        }    }    public override void DrawEffects(NPC npc, ref Color drawColor) {        if (essenceDrain) {            Vector2 vector = Vector2.UnitX * 0f;            vector += -Vector2.UnitY.RotatedBy(Main.rand.NextFloat(MathHelper.TwoPi), default) * new Vector2(npc.width, npc.height) * 0.05f;            vector = vector.RotatedBy(npc.velocity.ToRotation(), default);            if (Main.rand.NextBool(3)) {                int dust = Dust.NewDust(npc.Center, 0, 0, DustID.Shadowflame, 0f, 0f, 0, color, 1.4f);                Main.dust[dust].noGravity = true;                Main.dust[dust].position = npc.Center + vector;                Main.dust[dust].velocity = vector;            }            if (_essenceDrainTimer % 10 == 0) {                float dustCountMax = 6;                int dustCount = 0;                while (dustCount < dustCountMax) {                    if (Main.rand.NextBool(1)) {                        vector = Vector2.UnitX * 0f;                        vector += -Vector2.UnitY.RotatedBy(dustCount * (7f / dustCountMax), default) * new Vector2(npc.width, npc.height) * 0.5f;                        vector = vector.RotatedBy(npc.velocity.ToRotation(), default);                        int dust2 = Dust.NewDust(npc.Center, 0, 0, DustID.Shadowflame, 0f, 0f, 0, color, 1.4f);                        Main.dust[dust2].noGravity = true;                        Main.dust[dust2].position = npc.Center + vector;                        Main.dust[dust2].velocity = npc.velocity * 0f + vector.SafeNormalize(Vector2.UnitY) * 0.8f * Main.rand.NextFloat(-1f, 1f);                        Main.dust[dust2].velocity.Y -= 1f;                    }                    int dustCountMax2 = dustCount;                    dustCount = dustCountMax2 + 1;                }            }            _essenceDrainTimer++;            Lighting.AddLight(npc.position, 0.5f, 0f, 0.5f);        }    }    public override bool PreDraw(NPC npc, SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor) {        if (essenceDrain) {            if (fadeMult < 0.8f) fadeMult += 0.1f;            Vector2 offset = new Vector2(-MathHelper.Lerp(-3.5f, 5f, (float)Math.Sin(_essenceDrainTimer * 0.05f)));            Texture2D texture = (Texture2D)ModContent.Request<Texture2D>(ResourceManager.Textures + "BaneRune");            color = drawColor.MultiplyRGB(Color.DarkViolet) * fadeMult;            color = new Color(color.R, color.G, color.B * MathHelper.Lerp(1f, 2f, (float)Math.Sin(_essenceDrainTimer * 0.25f)), MathHelper.Lerp(0.75f, 1f, (float)Math.Sin(_essenceDrainTimer * 0.25f)));            Vector2 origin = new Vector2(texture.Width * 0.5f, texture.Height * 0.5f);            spriteBatch.Draw(texture, npc.Top + Vector2.UnitY * offset.Y * 0.5f - Vector2.UnitY * texture.Height * 0.6f - Main.screenPosition, new Rectangle?(),                 color, 0f, origin, fadeMult / 0.8f, SpriteEffects.None, 0);        }        else fadeMult = 0f;        return true;    }}
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+using RoA.Core;
+
+using System;
+
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace RoA.Content.Buffs;
+
+sealed class EssenceDrain : ModBuff {
+    public override void SetStaticDefaults() {
+        // DisplayName.SetDefault("Essence Drain");
+        // Description.SetDefault("Fast losing life");
+
+        Main.buffNoSave[Type] = true;
+        Main.debuff[Type] = true;
+        Main.pvpBuff[Type] = true;
+    }
+
+    public override void Update(Player player, ref int buffIndex)
+        => player.GetModPlayer<EssenceDrainPlayer>().essenceDrain = true;
+
+    public override void Update(NPC npc, ref int buffIndex)
+        => npc.GetGlobalNPC<EssenceDrainNPC>().essenceDrain = true;
+}
+
+internal class EssenceDrainPlayer : ModPlayer {
+    public bool essenceDrain;
+    public override void ResetEffects()
+        => essenceDrain = false;
+
+
+    public override void UpdateBadLifeRegen() {
+        if (essenceDrain) {
+            if (Player.lifeRegen > 0) Player.lifeRegen = 0;
+            Player.lifeRegenTime = 0;
+            Player.lifeRegen -= 80;
+        }
+    }
+
+    public override void DrawEffects(PlayerDrawSet drawInfo, ref float r, ref float g, ref float b, ref float a, ref bool fullBright) {
+        if (essenceDrain) {
+            if (Main.rand.NextBool(4) && drawInfo.shadow == 0.0) {
+                int dust = Dust.NewDust(drawInfo.Position - new Vector2(2f, 2f), Player.width + 4, Player.height + 4, DustID.Shadowflame, Player.velocity.X * 0.4f, Player.velocity.Y * 0.4f, 100, Color.DarkViolet, 1.2f);
+                Main.dust[dust].noGravity = true;
+                Main.dust[dust].velocity *= 1.8f;
+                Main.dust[dust].velocity.Y -= 0.5f;
+            }
+            Lighting.AddLight(Player.Center, 0.5f, 0f, 0.5f);
+        }
+    }
+}
+
+internal class EssenceDrainNPC : GlobalNPC {
+    public float fadeMult = 0.1f;
+    public Color color;
+    public override bool InstancePerEntity => true;
+
+    public bool essenceDrain;
+    public int Source;
+
+    private float _essenceDrainTimer = 0;
+
+    public override void ResetEffects(NPC npc)
+        => essenceDrain = false;
+
+
+    public override void UpdateLifeRegen(NPC npc, ref int damage) {
+        int bonusDamage = 8;
+        if (essenceDrain) {
+            if (npc.lifeRegen > 0) npc.lifeRegen = 0;
+            npc.lifeRegen -= 80;
+            if (damage < bonusDamage) damage = bonusDamage;
+        }
+    }
+
+    public override void DrawEffects(NPC npc, ref Color drawColor) {
+        if (essenceDrain) {
+            Vector2 vector = Vector2.UnitX * 0f;
+            vector += -Vector2.UnitY.RotatedBy(Main.rand.NextFloat(MathHelper.TwoPi), default) * new Vector2(npc.width, npc.height) * 0.05f;
+            vector = vector.RotatedBy(npc.velocity.ToRotation(), default);
+            if (Main.rand.NextBool(3)) {
+                int dust = Dust.NewDust(npc.Center, 0, 0, DustID.Shadowflame, 0f, 0f, 0, color, 1.4f);
+                Main.dust[dust].noGravity = true;
+                Main.dust[dust].position = npc.Center + vector;
+                Main.dust[dust].velocity = vector;
+            }
+            if (_essenceDrainTimer % 10 == 0) {
+                float dustCountMax = 6;
+                int dustCount = 0;
+                while (dustCount < dustCountMax) {
+                    if (Main.rand.NextBool(1)) {
+                        vector = Vector2.UnitX * 0f;
+                        vector += -Vector2.UnitY.RotatedBy(dustCount * (7f / dustCountMax), default) * new Vector2(npc.width, npc.height) * 0.5f;
+                        vector = vector.RotatedBy(npc.velocity.ToRotation(), default);
+                        int dust2 = Dust.NewDust(npc.Center, 0, 0, DustID.Shadowflame, 0f, 0f, 0, color, 1.4f);
+                        Main.dust[dust2].noGravity = true;
+                        Main.dust[dust2].position = npc.Center + vector;
+                        Main.dust[dust2].velocity = npc.velocity * 0f + vector.SafeNormalize(Vector2.UnitY) * 0.8f * Main.rand.NextFloat(-1f, 1f);
+                        Main.dust[dust2].velocity.Y -= 1f;
+                    }
+                    int dustCountMax2 = dustCount;
+                    dustCount = dustCountMax2 + 1;
+                }
+            }
+            _essenceDrainTimer++;
+            Lighting.AddLight(npc.position, 0.5f, 0f, 0.5f);
+        }
+    }
+    public override bool PreDraw(NPC npc, SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor) {
+        if (essenceDrain) {
+            if (fadeMult < 0.8f) fadeMult += 0.1f;
+            Vector2 offset = new Vector2(-MathHelper.Lerp(-3.5f, 5f, (float)Math.Sin(_essenceDrainTimer * 0.05f)));
+            Texture2D texture = (Texture2D)ModContent.Request<Texture2D>(ResourceManager.Textures + "BaneRune");
+            color = drawColor.MultiplyRGB(Color.DarkViolet) * fadeMult;
+            color = new Color(color.R, color.G, color.B * MathHelper.Lerp(1f, 2f, (float)Math.Sin(_essenceDrainTimer * 0.25f)), MathHelper.Lerp(0.75f, 1f, (float)Math.Sin(_essenceDrainTimer * 0.25f)));
+            Vector2 origin = new Vector2(texture.Width * 0.5f, texture.Height * 0.5f);
+            spriteBatch.Draw(texture, npc.Top + Vector2.UnitY * offset.Y * 0.5f - Vector2.UnitY * texture.Height * 0.6f - Main.screenPosition, new Rectangle?(), 
+                color, 0f, origin, fadeMult / 0.8f, SpriteEffects.None, 0);
+        }
+        else fadeMult = 0f;
+        return true;
+    }
+}
--- Content/Projectiles/Friendly/Magic/BaneSpell.cs
+++ Content/Projectiles/Friendly/Magic/BaneSpell.cs
@@ -1,1 +1,106 @@
-using Microsoft.Xna.Framework;using RoA.Content.Buffs;using RoA.Core;using RoA.Core.Utility;using Terraria;using Terraria.Audio;using Terraria.ID;using Terraria.ModLoader;namespace RoA.Content.Projectiles.Friendly.Magic;sealed class BaneSpell : ModProjectile {    public override string Texture => ResourceManager.EmptyTexture;    public override void SetStaticDefaults() {        ProjectileID.Sets.TrailCacheLength[Projectile.type] = 40;        ProjectileID.Sets.TrailingMode[Projectile.type] = 2;    }    public override void SetDefaults() {        int width = 4; int height = width;        Projectile.Size = new Vector2(width, height);        Projectile.aiStyle = 1;        AIType = ProjectileID.Bullet;        Projectile.friendly = true;        Projectile.tileCollide = true;        Projectile.DamageType = DamageClass.Magic;        Projectile.penetrate = 1;        Projectile.timeLeft = 180;        Projectile.extraUpdates = 1;        Projectile.alpha = 200;    }    public override bool TileCollideStyle(ref int width, ref int height, ref bool fallThrough, ref Vector2 hitboxCenterFrac) {        width = height = 4;        return true;    }    public override void AI() {        if (Projectile.timeLeft == 180) {            float radius = 30f;            int dustCount = 0;            while (dustCount < radius) {                Vector2 vector = Vector2.UnitX * 0f;                vector += -Vector2.UnitY.RotatedBy(dustCount * (7f / radius), default) * new Vector2(3.2f, 3f);                vector = vector.RotatedBy(Projectile.velocity.ToRotation(), default);                int dust = Dust.NewDust(Projectile.Center, 0, 0, DustID.Shadowflame, 0f, 0f, 0, Color.DarkViolet);                Main.dust[dust].alpha = Projectile.alpha;                Main.dust[dust].noGravity = true;                Main.dust[dust].position = Projectile.Center + vector - Projectile.velocity.SafeNormalize(Vector2.Zero) * 4f;                Main.dust[dust].velocity = vector.SafeNormalize(Vector2.UnitY) * 1f * Main.rand.NextFloat(0.8f, 1f);                Main.dust[dust].velocity += Projectile.velocity.SafeNormalize(Vector2.Zero) * 1.5f;                int dustCountMax = dustCount;                dustCount = dustCountMax + 1;            }        }        if (Projectile.timeLeft <= 178) {            Projectile.alpha -= 2;            if (Main.netMode != NetmodeID.Server) {                for (var i = 0; i < 3; i++) {                    var x = Projectile.Center.X - Projectile.velocity.X / 3f * i;                    var y = Projectile.Center.Y - Projectile.velocity.Y / 3f * i;                    var dust2 = Dust.NewDust(new Vector2(x, y), 1, 1, DustID.Shadowflame, 0f, 0f, 0, Color.DarkViolet, 1.5f);                    Main.dust[dust2].alpha = Projectile.alpha;                    Main.dust[dust2].position.X = x;                    Main.dust[dust2].position.Y = y;                    Main.dust[dust2].velocity *= 0f;                    Main.dust[dust2].noGravity = true;                }            }        }    }    public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone) {        NPC flag = null;        foreach (NPC npc in Main.ActiveNPCs) {            if (npc.HasBuff<EssenceDrain>() && npc.GetGlobalNPC<EssenceDrainNPC>().Source == Projectile.owner) {                npc.ClearBuff(ModContent.BuffType<EssenceDrain>());                break;            }        }        target.AddBuff(ModContent.BuffType<EssenceDrain>(), 180);        target.GetGlobalNPC<EssenceDrainNPC>().Source = Projectile.owner;        SoundEngine.PlaySound(SoundID.NPCDeath55, Projectile.Center);    }    public override void OnHitPlayer(Player target, Player.HurtInfo info) {        target.AddBuff(ModContent.BuffType<EssenceDrain>(), 180);        SoundEngine.PlaySound(SoundID.NPCDeath55, Projectile.Center);    }    public override void OnKill(int timeLeft) {        for (int k = 0; k < 15; ++k) {            int dust = Dust.NewDust(new Vector2(Projectile.position.X, Projectile.position.Y + 2f), Projectile.width + 5, Projectile.height + 5,                DustID.Shadowflame, Projectile.velocity.X * 0.2f, Projectile.velocity.Y * 0.2f, 100, Color.DarkViolet, 1.4f);            Main.dust[dust].noGravity = true;            Main.dust[dust].alpha = 0;        }    }}
+using Microsoft.Xna.Framework;
+
+using RoA.Content.Buffs;
+using RoA.Core;
+using RoA.Core.Utility;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace RoA.Content.Projectiles.Friendly.Magic;
+
+sealed class BaneSpell : ModProjectile {
+    public override string Texture => ResourceManager.EmptyTexture;
+
+    public override void SetStaticDefaults() {
+        ProjectileID.Sets.TrailCacheLength[Projectile.type] = 40;
+        ProjectileID.Sets.TrailingMode[Projectile.type] = 2;
+    }
+
+    public override void SetDefaults() {
+        int width = 4; int height = width;
+        Projectile.Size = new Vector2(width, height);
+
+        Projectile.aiStyle = 1;
+        AIType = ProjectileID.Bullet;
+
+        Projectile.friendly = true;
+        Projectile.tileCollide = true;
+        Projectile.DamageType = DamageClass.Magic;
+
+        Projectile.penetrate = 1;
+        Projectile.timeLeft = 180;
+
+        Projectile.extraUpdates = 1;
+        Projectile.alpha = 200;
+    }
+
+    public override bool TileCollideStyle(ref int width, ref int height, ref bool fallThrough, ref Vector2 hitboxCenterFrac) {
+        width = height = 4;
+        return true;
+    }
+
+    public override void AI() {
+        if (Projectile.timeLeft == 180) {
+            float radius = 30f;
+            int dustCount = 0;
+            while (dustCount < radius) {
+                Vector2 vector = Vector2.UnitX * 0f;
+                vector += -Vector2.UnitY.RotatedBy(dustCount * (7f / radius), default) * new Vector2(3.2f, 3f);
+                vector = vector.RotatedBy(Projectile.velocity.ToRotation(), default);
+                int dust = Dust.NewDust(Projectile.Center, 0, 0, DustID.Shadowflame, 0f, 0f, 0, Color.DarkViolet);
+                Main.dust[dust].alpha = Projectile.alpha;
+                Main.dust[dust].noGravity = true;
+                Main.dust[dust].position = Projectile.Center + vector - Projectile.velocity.SafeNormalize(Vector2.Zero) * 4f;
+                Main.dust[dust].velocity = vector.SafeNormalize(Vector2.UnitY) * 1f * Main.rand.NextFloat(0.8f, 1f);
+                Main.dust[dust].velocity += Projectile.velocity.SafeNormalize(Vector2.Zero) * 1.5f;
+                int dustCountMax = dustCount;
+                dustCount = dustCountMax + 1;
+            }
+        }
+        if (Projectile.timeLeft <= 178) {
+            Projectile.alpha -= 2;
+            if (Main.netMode != NetmodeID.Server) {
+                for (var i = 0; i < 3; i++) {
+                    var x = Projectile.Center.X - Projectile.velocity.X / 3f * i;
+                    var y = Projectile.Center.Y - Projectile.velocity.Y / 3f * i;
+                    var dust2 = Dust.NewDust(new Vector2(x, y), 1, 1, DustID.Shadowflame, 0f, 0f, 0, Color.DarkViolet, 1.5f);
+                    Main.dust[dust2].alpha = Projectile.alpha;
+                    Main.dust[dust2].position.X = x;
+                    Main.dust[dust2].position.Y = y;
+                    Main.dust[dust2].velocity *= 0f;
+                    Main.dust[dust2].noGravity = true;
+                }
+            }
+        }
+    }
+
+    public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone) {
+        NPC flag = null;
+        foreach (NPC npc in Main.ActiveNPCs) {
+            if (npc.HasBuff<EssenceDrain>() && npc.GetGlobalNPC<EssenceDrainNPC>().Source == Projectile.owner) {
+                npc.ClearBuff(ModContent.BuffType<EssenceDrain>());
+                break;
+            }
+        }
+        target.AddBuff(ModContent.BuffType<EssenceDrain>(), 180);
+        target.GetGlobalNPC<EssenceDrainNPC>().Source = Projectile.owner;
+
+        SoundEngine.PlaySound(SoundID.NPCDeath55, Projectile.Center);
+    }
+
+    public override void OnHitPlayer(Player target, Player.HurtInfo info) {
+        target.AddBuff(ModContent.BuffType<EssenceDrain>(), 180);
+        SoundEngine.PlaySound(SoundID.NPCDeath55, Projectile.Center);
+    }
+
+    public override void OnKill(int timeLeft) {
+        for (int k = 0; k < 15; ++k) {
+            int dust = Dust.NewDust(new Vector2(Projectile.position.X, Projectile.position.Y + 2f), Projectile.width + 5, Projectile.height + 5,
+                DustID.Shadowflame, Projectile.velocity.X * 0.2f, Projectile.velocity.Y * 0.2f, 100, Color.DarkViolet, 1.4f);
+            Main.dust[dust].noGravity = true;
+            Main.dust[dust].alpha = 0;
+        }
+    }
+}
