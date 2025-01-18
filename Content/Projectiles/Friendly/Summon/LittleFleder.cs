765a7119834a79e5490bdb8e1e5d97cf640b39e6
Serork <mandrygin.2013@mail.ru>
2025-01-18 19:09:04 +0400

    Update LittleFleder.cs

--- Content/Projectiles/Friendly/Summon/LittleFleder.cs
+++ Content/Projectiles/Friendly/Summon/LittleFleder.cs
@@ -383,7 +383,7 @@ sealed class LittleFleder : ModProjectile {
             distanceBetweenTargetAndMe = v.Length();
             v = v.SafeNormalize(Vector2.Zero);
 
-            bool flag5 = Math.Abs(Projectile.Center.Y - targetCenter.Y) < 10f;
+            bool flag5 = Math.Abs(Projectile.Center.Y - targetCenter.Y) < 100f;
             if (flag5 || targetCenter.Y > Projectile.Center.Y) {
                 if ((targetCenter - Projectile.Center).Length() < 100f) {
                     Projectile.velocity.Y -= 0.075f;
