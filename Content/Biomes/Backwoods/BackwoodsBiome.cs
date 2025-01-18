b1bb7c54e032a95dd1ffed67fbbfd936ec7315b1
Serork <mandrygin.2013@mail.ru>
2025-01-18 23:21:43 +0400

    Update BackwoodsBiome.cs

--- Content/Biomes/Backwoods/BackwoodsBiome.cs
+++ Content/Biomes/Backwoods/BackwoodsBiome.cs
@@ -62,7 +62,7 @@ sealed partial class BackwoodsBiome : ModBiome {
 
     public override ModSurfaceBackgroundStyle SurfaceBackgroundStyle => ModContent.Find<ModSurfaceBackgroundStyle>(RoA.ModName +  "/BackwoodsBackgroundSurface");
 
-    public override int Music => BackwoodsFogHandler.IsFogActive ? MusicLoader.GetMusicSlot(ResourceManager.Music + "Fog") : Main.IsItDay() ? MusicLoader.GetMusicSlot(ResourceManager.Music + "ThicketDay") : MusicLoader.GetMusicSlot(ResourceManager.Music + "ThicketNight");
+    public override int Music => BackwoodsFogHandler.IsFogActive ? MusicLoader.GetMusicSlot(ResourceManager.Music + "Fog") : /*Main.IsItDay() ? MusicLoader.GetMusicSlot(ResourceManager.Music + "ThicketDay") :*/ MusicLoader.GetMusicSlot(ResourceManager.Music + "ThicketNight");
 
     public override string MapBackground => ResourceManager.BackwoodsTextures + "DruidBiomeMapBG";
 
