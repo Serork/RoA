4226e8dbd2b598c0bf47e3576765b8a2e0bcde2c
Serork <mandrygin.2013@mail.ru>
2025-01-26 22:39:00 +0400

    Recipe rework

---
+++ Common/Items/CustomRecipes.cs
@@ -0,0 +1,25 @@
+﻿using RoA.Content.Items.Materials;
+using RoA.Core.Utility;
+
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace RoA.Common.Items;
+
+sealed class CustomRecipes : ModSystem {
+    public override void PostAddRecipes() {
+        // potions overhaul
+        RecipeHelper.NewRecipe(ItemID.EndurancePotion, new int[] { ModContent.ItemType<Bonerose>() }, new int[] { 1 }, false, new int[] { ItemID.Blinkroot });
+        RecipeHelper.NewRecipe(ItemID.FeatherfallPotion, new int[] { ModContent.ItemType<Cloudberry>() }, new int[] { 1 }, false, new int[] { ItemID.Daybloom, ItemID.Blinkroot });
+        RecipeHelper.NewRecipe(ItemID.GravitationPotion, new int[] { ModContent.ItemType<Bonerose>(), ModContent.ItemType<MiracleMint>() }, new int[] { 1, 1 }, false, new int[] { ItemID.Deathweed, ItemID.Blinkroot });
+        RecipeHelper.NewRecipe(ItemID.LifeforcePotion, new int[] { ItemID.Daybloom, ModContent.ItemType<Bonerose>() }, new int[] { 1, 1 }, false, new int[] { ItemID.Moonglow, ItemID.Waterleaf });
+        RecipeHelper.NewRecipe(ItemID.MagicPowerPotion, new int[] { ItemID.Fireblossom, ModContent.ItemType<MiracleMint>() }, new int[] { 1, 1 }, false, new int[] { ItemID.Moonglow, ItemID.Deathweed });
+        RecipeHelper.NewRecipe(ItemID.ManaRegenerationPotion, new int[] { ModContent.ItemType<MiracleMint>() }, new int[] { 1 }, false, new int[] { ItemID.Daybloom });
+        RecipeHelper.NewRecipe(ItemID.ThornsPotion, new int[] { ModContent.ItemType<Bonerose>() }, new int[] { 1 }, false, new int[] { ItemID.WormTooth });
+        RecipeHelper.NewRecipe(ItemID.WormholePotion, new int[] { ModContent.ItemType<MiracleMint>() }, new int[] { 1 }, false, new int[] { ItemID.Blinkroot });
+        
+        // items
+        RecipeHelper.NewRecipe(ItemID.WarAxeoftheNight, new int[] { ItemID.ShadowScale }, new int[] { 5 }, false);
+        RecipeHelper.NewRecipe(ItemID.BloodLustCluster, new int[] { ItemID.TissueSample }, new int[] { 5 }, false);
+    }
+}
---
+++ Core/Utility/RecipeHelper.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using Terraria;
+
+namespace RoA.Core.Utility;
+
+public static class RecipeHelper {
+    public static Item RecipeItem(Recipe recipe, int itemID) {
+        for (int i = 0; i < recipe.requiredItem.Count; i++) {
+            Item item = recipe.requiredItem[i];
+            if (item.type == itemID)
+                return item;
+        }
+        return null;
+    }
+
+    public static void NewRecipe(int itemID, int[] newItemsIDs, int[] newItemsStacks, bool removeIngrediens = false, int[] removeItemsIDs = null, bool removeTile = false, int[] removeTilesIDs = null, int[] newTilesIDs = null) {
+        IEnumerable<Recipe> itemRecipes = Main.recipe.Where(r => itemID.Equals(r.createItem.type));
+        foreach (Recipe recipe in itemRecipes) {
+            if (removeIngrediens)
+                for (int i = 0; i < recipe.requiredItem.Count; i++)
+                    recipe.RemoveIngredient(recipe.requiredItem[i]);
+            if (removeTile)
+                for (int i = 0; i < recipe.requiredTile.Count; i++)
+                    recipe.RemoveTile(recipe.requiredTile[i]);
+            if (removeItemsIDs != null)
+                for (int i = 0; i < removeItemsIDs.Length; i++)
+                    recipe.RemoveIngredient(removeItemsIDs[i]);
+            if (removeTilesIDs != null)
+                for (int i = 0; i < removeTilesIDs.Length; i++)
+                    recipe.RemoveTile(removeTilesIDs[i]);
+            for (int i = 0; i < newItemsIDs.Length; i++)
+                recipe.AddIngredient(newItemsIDs[i], newItemsStacks[i]);
+            if (newTilesIDs != null)
+                for (int i = 0; i < newTilesIDs.Length; i++)
+                    recipe.AddTile(newItemsIDs[i]);
+        }
+    }
+}
