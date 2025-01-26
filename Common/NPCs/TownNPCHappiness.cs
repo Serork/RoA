5ae25da5b82600d30d53c5183680c9a6f0b939fb
Serork <mandrygin.2013@mail.ru>
2025-01-26 22:35:21 +0400

    Create TownNPCHappiness.cs

---
+++ Common/NPCs/TownNPCHappiness.cs
@@ -0,0 +1,14 @@
+﻿using RoA.Content.Biomes.Backwoods;
+
+using Terraria.GameContent.Personalities;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace RoA.Common.NPCs;
+
+sealed class TownNPCHappiness : GlobalNPC {
+    public override void SetStaticDefaults() {
+        NPCHappiness.Get(NPCID.Dryad).SetBiomeAffection<BackwoodsBiome>(AffectionLevel.Hate);
+        NPCHappiness.Get(NPCID.WitchDoctor).SetBiomeAffection<BackwoodsBiome>(AffectionLevel.Hate);
+    }
+}
