9cb4ae9dc8cc43a7e9892ebd11f16077eca3bc2a
Serork <mandrygin.2013@mail.ru>
2025-01-26 22:47:42 +0400

    Update BackwoodsPylonTile.cs

--- Content/Tiles/Miscellaneous/BackwoodsPylonTile.cs
+++ Content/Tiles/Miscellaneous/BackwoodsPylonTile.cs
@@ -7,6 +7,8 @@ using RoA.Common.CustomConditions;
 using RoA.Content.Biomes.Backwoods;
 using RoA.Content.Items.Placeable;
 
+using System;
+
 using Terraria;
 using Terraria.DataStructures;
 using Terraria.GameContent;
@@ -111,16 +113,96 @@ sealed class BackwoodsPylonTile : ModPylon {
         // We want to draw the pylon crystal the exact same way vanilla does, so we can use this built in method in ModPylon for default crystal drawing:
         // For the sake of example, lets make our pylon create a bit more dust by decreasing the dustConsequent value down to 1. If you want your dust spawning to be identical to vanilla, set dustConsequent to 4.
         // We also multiply the pylonShadowColor in order to decrease its opacity, so it actually looks like a "shadow"
-        DefaultDrawPylonCrystal(spriteBatch, 
+        NewDefaultDrawPylonCrystal(spriteBatch, 
             i, 
             j, 
             crystalTexture, 
             crystalHighlightTexture, 
-            new Vector2(0f, -12f), 
-            Color.White * 0.1f,
+            new Vector2(0f, -12f),
+            new Color(255, 255, 255, 0) * 0.1f,
             new Color(79, 172, 211), 
-            10,
+            7,
             CrystalVerticalFrameCount);
+    }
+
+    private void NewDefaultDrawPylonCrystal(SpriteBatch spriteBatch, int i, int j, Asset<Texture2D> crystalTexture, Asset<Texture2D> crystalHighlightTexture, Vector2 crystalOffset, Color pylonShadowColor, Color dustColor, int dustChanceDenominator, int crystalVerticalFrameCount) {
+        // Gets offscreen vector for different lighting modes
+        Vector2 offscreenVector = new Vector2(Main.offScreenRange);
+        if (Main.drawToScreen) {
+            offscreenVector = Vector2.Zero;
+        }
+
+        // Double check that the tile exists
+        Point point = new Point(i, j);
+        Tile tile = Main.tile[point.X, point.Y];
+        if (tile == null || !tile.HasTile) {
+            return;
+        }
+
+        TileObjectData tileData = TileObjectData.GetTileData(tile);
+
+        // Calculate frame based on vanilla counters in order to line up the animation
+        int frameY = Main.tileFrameCounter[TileID.TeleportationPylon] / crystalVerticalFrameCount;
+
+        // Frame our modded crystal sheet accordingly for proper drawing
+        Rectangle crystalFrame = crystalTexture.Frame(1, crystalVerticalFrameCount, 0, frameY);
+        Rectangle smartCursorGlowFrame = crystalHighlightTexture.Frame(1, crystalVerticalFrameCount, 0, frameY);
+        // I have no idea what is happening here; but it fixes the frame bleed issue. All I know is that the vertical sinusoidal motion has something to with it.
+        // If anyone else has a clue as to why, please do tell. - MutantWafflez
+        crystalFrame.Height -= 1;
+        smartCursorGlowFrame.Height -= 1;
+
+        // Calculate positional variables for actually drawing the crystal
+        Vector2 origin = crystalFrame.Size() / 2f;
+        Vector2 tileOrigin = new Vector2(tileData.CoordinateFullWidth / 2f, tileData.CoordinateFullHeight / 2f);
+        Vector2 crystalPosition = point.ToWorldCoordinates(tileOrigin.X - 2f, tileOrigin.Y) + crystalOffset;
+
+        // Calculate additional drawing positions with a sine wave movement
+        float sinusoidalOffset = (float)Math.Sin(Main.GlobalTimeWrappedHourly * (Math.PI * 2) / 5);
+        Vector2 drawingPosition = crystalPosition + offscreenVector + new Vector2(0f, sinusoidalOffset * 4f);
+
+        // Do dust drawing
+        if (!Main.gamePaused && Main.instance.IsActive && (!Lighting.UpdateEveryFrame || Main.rand.NextBool(4)) && Main.rand.NextBool(dustChanceDenominator)) {
+            Rectangle dustBox = Utils.CenteredRectangle(crystalPosition, crystalFrame.Size());
+            int numForDust = Dust.NewDust(dustBox.TopLeft(), dustBox.Width, dustBox.Height, DustID.TintableDustLighted, 0f, 0f, 254, dustColor, 0.5f);
+            Dust obj = Main.dust[numForDust];
+            obj.velocity *= 0.1f;
+            Main.dust[numForDust].velocity.Y -= 0.2f;
+        }
+
+        // Get color value and draw the the crystal
+        Color color = Lighting.GetColor(point.X, point.Y);
+        color = Color.Lerp(color, Color.White, 0.8f);
+        spriteBatch.Draw(crystalTexture.Value, drawingPosition - Main.screenPosition, crystalFrame, color * 0.7f, 0f, origin, 1f, SpriteEffects.None, 0f);
+
+        // Draw the shadow effect for the crystal
+        float scale = (float)Math.Sin(Main.GlobalTimeWrappedHourly * ((float)Math.PI * 2f) / 1f) * 0.2f + 0.8f;
+        Color shadowColor = pylonShadowColor * scale;
+        for (float shadowPos = 0f; shadowPos < 1f; shadowPos += 1f / 6f) {
+            spriteBatch.Draw(crystalTexture.Value, drawingPosition - Main.screenPosition + ((float)Math.PI * 2f * shadowPos).ToRotationVector2() * (6f + sinusoidalOffset * 2f), crystalFrame, shadowColor, 0f, origin, 1f, SpriteEffects.None, 0f);
+        }
+
+        // Interpret smart cursor outline color & draw it
+        int selectionLevel = 0;
+        if (Main.InSmartCursorHighlightArea(point.X, point.Y, out bool actuallySelected)) {
+            selectionLevel = 1;
+            if (actuallySelected) {
+                selectionLevel = 2;
+            }
+        }
+
+        if (selectionLevel == 0) {
+            return;
+        }
+
+        int averageBrightness = (color.R + color.G + color.B) / 3;
+
+        if (averageBrightness <= 10) {
+            return;
+        }
+
+        Color selectionGlowColor = Colors.GetSelectionGlowColor(selectionLevel == 2, averageBrightness);
+        spriteBatch.Draw(crystalHighlightTexture.Value, drawingPosition - Main.screenPosition, smartCursorGlowFrame, selectionGlowColor, 0f, origin, 1f, SpriteEffects.None, 0f);
     }
 
     public override void DrawMapIcon(ref MapOverlayDrawContext context, ref string mouseOverText, TeleportPylonInfo pylonInfo, bool isNearPylon, Color drawColor, float deselectedScale, float selectedScale) {
