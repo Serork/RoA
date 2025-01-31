5f9ec8758264a6c180951b5e2fd894fd215a1f47
Serork <mandrygin.2013@mail.ru>
2025-01-31 13:39:48 +0400

    Update ShockLightning.cs

--- Content/Projectiles/Friendly/Magic/ShockLightning.cs
+++ Content/Projectiles/Friendly/Magic/ShockLightning.cs
@@ -248,7 +248,7 @@ sealed class ShockLightning : ModProjectile {
             spriteBatch.Draw(_endTexture2.Value, source - Main.screenPosition, null, color, rotation, middleOrigin, middleScale * 0.01f, SpriteEffects.None, 0f);
             spriteBatch.BeginBlendState(BlendState.Additive);
             spriteBatch.Draw(_segmentTexture.Value, source - Main.screenPosition, null, color * 0.75f, rotation, middleOrigin, middleScale * 0.95f, SpriteEffects.None, 0f);
-            spriteBatch.Draw(ModContent.Request<Texture2D>(ResourceManager.Textures + "Light").Value, source - Main.screenPosition, null, color.MultiplyRGB(new Color(99, 200, 204)) * 0.75f, rotation, middleOrigin, new Vector2(middleScale.X, middleScale.Y * 2f) * 0.02f, SpriteEffects.None, 0f);
+            spriteBatch.Draw(ModContent.Request<Texture2D>(ResourceManager.Textures + "Light").Value, source - Main.screenPosition, null, color.MultiplyRGB(new Color(99, 200, 204)) * 1f, rotation, middleOrigin, new Vector2(middleScale.X, middleScale.Y * 2f) * 0.02f, SpriteEffects.None, 0f);
             spriteBatch.EndBlendState();
             spriteBatch.Draw(_segmentTexture.Value, source - Main.screenPosition, null, color, rotation, middleOrigin, middleScale * 0.85f, SpriteEffects.None, 0f);
             spriteBatch.Draw(_endTexture.Value, source - Main.screenPosition, null, color, rotation, capOrigin, thicknessScale, SpriteEffects.None, 0f);
