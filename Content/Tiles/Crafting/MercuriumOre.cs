72322358dc94bbf7800381c84d6cb4f7e8d234fc
Serork <mandrygin.2013@mail.ru>
2025-01-31 14:42:04 +0400

    Update MercuriumOre.cs

--- Content/Tiles/Crafting/MercuriumOre.cs
+++ Content/Tiles/Crafting/MercuriumOre.cs
@@ -84,6 +84,8 @@ sealed class MercuriumOre : ModTile {
         //        break;
         //    }
         //}
+        //for (int i2 = x - tilesAway; i2 < x + tilesAway + 1; i2++) {
+        //    for (int j2 = y - tilesAway; j2 < y + tilesAway + 1; j2++) {
         for (int i2 = x - tilesAway; i2 < x + tilesAway + 1; i2++) {
             for (int j2 = y - tilesAway; j2 < y + tilesAway + 1; j2++) {
                 if (WorldGen.InWorld(i2, j)) {
