f9f5360088a59fc30852a5dac024f27e8c65f890
Serork <mandrygin.2013@mail.ru>
2025-02-01 22:31:41 +0400

    Create ExcludeForestPylonFromBuyingSystem.cs

---
+++ Common/ExcludeForestPylonFromBuyingSystem.cs
@@ -0,0 +1,25 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace RoA.Common;
+
+sealed class ExcludeForestPylonFromBuyingSystem : GlobalNPC {
+    public override void ModifyActiveShop(NPC npc, string shopName, Item[] items) {
+        int i2 = 0;
+        for (int i = 0; i < items.Length; i++) {
+            Item item = items[i];
+            if (item != null && item.type == ItemID.TeleportationPylonPurity) {
+                i2 = i;
+                break;
+            }
+        }
+        if (i2 != 0) {
+            for (int i = i2; i < items.Length; i++) {
+                if (i + 1 < items.Length - 1) {
+                    items[i] = items[i + 1];
+                }
+            }
+        }
+    }
+}
