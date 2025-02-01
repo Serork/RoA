f07259b76fee1f6c6b22e0c0da30ae1abcc447bd
Serork <mandrygin.2013@mail.ru>
2025-02-01 22:32:10 +0400

    Update ExcludeForestPylonFromBuyingSystem.cs

--- Common/ExcludeForestPylonFromBuyingSystem.cs
+++ Common/ExcludeForestPylonFromBuyingSystem.cs
@@ -1,4 +1,6 @@
-﻿using Terraria;
+﻿using RoA.Content.Biomes.Backwoods;
+
+using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -6,18 +8,20 @@ namespace RoA.Common;
 
 sealed class ExcludeForestPylonFromBuyingSystem : GlobalNPC {
     public override void ModifyActiveShop(NPC npc, string shopName, Item[] items) {
-        int i2 = 0;
-        for (int i = 0; i < items.Length; i++) {
-            Item item = items[i];
-            if (item != null && item.type == ItemID.TeleportationPylonPurity) {
-                i2 = i;
-                break;
+        if (Main.LocalPlayer.InModBiome<BackwoodsBiome>()) {
+            int i2 = 0;
+            for (int i = 0; i < items.Length; i++) {
+                Item item = items[i];
+                if (item != null && item.type == ItemID.TeleportationPylonPurity) {
+                    i2 = i;
+                    break;
+                }
             }
-        }
-        if (i2 != 0) {
-            for (int i = i2; i < items.Length; i++) {
-                if (i + 1 < items.Length - 1) {
-                    items[i] = items[i + 1];
+            if (i2 != 0) {
+                for (int i = i2; i < items.Length; i++) {
+                    if (i + 1 < items.Length - 1) {
+                        items[i] = items[i + 1];
+                    }
                 }
             }
         }
