917541afe6367976d501d256756b503e3bd06dfd
Serork <mandrygin.2013@mail.ru>
2025-02-09 03:01:08 +0400

    Update TreeDust.cs

--- Content/Dusts/TreeDust.cs
+++ Content/Dusts/TreeDust.cs
@@ -1,5 +1,7 @@
 using Microsoft.Xna.Framework;
 
+using RoA.Common.WorldEvents;
+
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -7,7 +9,8 @@ using Terraria.ModLoader;
 namespace RoA.Content.Dusts;
 
 sealed class TreeDust : ModDust {
-    public override Color? GetAlpha(Dust dust, Color lightColor) => Color.Lerp(Color.White, lightColor, 0.8f);
+    public override Color? GetAlpha(Dust dust, Color lightColor) 
+        => Color.Lerp(Color.Lerp(lightColor, Color.White, BackwoodsFogHandler.Opacity > 0f ? MathHelper.Clamp(BackwoodsFogHandler.Opacity * 1.5f, 0f, 1f) : 0f), lightColor, 0.8f);
 
     public override void OnSpawn(Dust dust) => UpdateType = DustID.Demonite;
 }
