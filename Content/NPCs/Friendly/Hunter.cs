afbfaa6e4f5f75e68dce9cb248a0330558e281e6
Serork <mandrygin.2013@mail.ru>
2025-02-10 18:18:48 +0400

    Hunter quotes

--- Content/NPCs/Friendly/Hunter.cs
+++ Content/NPCs/Friendly/Hunter.cs
@@ -1,6 +1,11 @@
-﻿using Terraria;
+﻿using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using System;
+
+using Terraria;
 using Terraria.GameContent;
 using Terraria.ID;
+using Terraria.Localization;
 using Terraria.ModLoader;
 using Terraria.Utilities;
 
@@ -16,17 +21,10 @@ sealed class Hunter : ModNPC {
         NPCID.Sets.AttackFrameCount[Type] = 4;
         NPCID.Sets.DangerDetectRange[Type] = 700; 
         NPCID.Sets.PrettySafe[Type] = 300;
-        //NPCID.Sets.AttackType[Type] = 1; 
         NPCID.Sets.AttackTime[Type] = 0;
         NPCID.Sets.AttackAverageChance[Type] = 0;
-        //NPCID.Sets.HatOffsetY[Type] = 4;
-        //NPCID.Sets.ShimmerTownTransform[NPC.type] = true;
-
-        NPCID.Sets.ActsLikeTownNPC[Type] = true;
 
         NPCID.Sets.NoTownNPCHappiness[Type] = true;
-
-        NPCID.Sets.SpawnsWithCustomName[Type] = true;
 
         NPCID.Sets.NPCBestiaryDrawModifiers drawModifiers = new NPCID.Sets.NPCBestiaryDrawModifiers() {
             Velocity = 1f,
@@ -34,11 +32,6 @@ sealed class Hunter : ModNPC {
         };
 
         NPCID.Sets.NPCBestiaryDrawOffset.Add(Type, drawModifiers);
-
-        //NPCProfile = new Profiles.StackedNPCProfile(
-        //    new Profiles.DefaultNPCProfile(Texture, -1),
-        //    new Profiles.DefaultNPCProfile(Texture + "_Shimmer", -1)
-        //);
 
         NPCProfile = new Profiles.StackedNPCProfile(
             new Profiles.DefaultNPCProfile(Texture, -1)
@@ -48,7 +41,7 @@ sealed class Hunter : ModNPC {
     public override bool UsesPartyHat() => false;
 
     public override void SetDefaults() {
-        NPC.friendly = true; // NPC Will not attack player
+        NPC.friendly = true;
         NPC.width = 18;
         NPC.height = 40;
         NPC.aiStyle = 7;
@@ -58,29 +51,84 @@ sealed class Hunter : ModNPC {
         NPC.HitSound = SoundID.NPCHit1;
         NPC.DeathSound = SoundID.NPCDeath1;
         NPC.knockBackResist = 0.5f;
-
-        AnimationType = NPCID.Guide;
     }
 
-    public override bool CanChat() => true;
+    public override void FindFrame(int frameHeight) {
+        int num236 = 10;
+        if (TownNPCProfiles.Instance.GetProfile(NPC, out var profile)) {
+            Asset<Texture2D> textureNPCShouldUse = profile.GetTextureNPCShouldUse(NPC);
+            int num = 0;
+            if (textureNPCShouldUse.IsLoaded) {
+                num = textureNPCShouldUse.Height() / Main.npcFrameCount[Type];
+                NPC.frame.Width = textureNPCShouldUse.Width();
+                NPC.frame.Height = num;
+            }
 
-    //public override List<string> SetNPCNameList() {
-    //    return new List<string> {
-    //            "Blocky Bones",
-    //            "Someone's Ribcage",
-    //            "Underground Blockster",
-    //            "Darkness"
-    //        };
-    //}
+            if (NPC.velocity.Y == 0f) {
+                if (NPC.direction == 1)
+                    NPC.spriteDirection = 1;
+
+                if (NPC.direction == -1)
+                    NPC.spriteDirection = -1;
+
+                int num237 = Main.npcFrameCount[Type] - NPCID.Sets.AttackFrameCount[Type];
+                if (NPC.ai[0] == 23f) {
+
+                }
+                else if (NPC.ai[0] >= 20f && NPC.ai[0] <= 22f) {
+
+                }
+                else if (NPC.ai[0] == 2f) {
+
+                }
+                else if (NPC.velocity.X == 0f) {
+                    NPC.frame.Y = 0;
+                    NPC.frameCounter = 0.0;
+                }
+                else {
+                    int num287 = 6;
+
+                    NPC.frameCounter += Math.Abs(NPC.velocity.X) * 2f;
+                    NPC.frameCounter += 1.0;
+
+                    int num288 = num * 2;
+
+                    if (NPC.frame.Y < num288)
+                        NPC.frame.Y = num288;
+
+                    if (NPC.frameCounter > (double)num287) {
+                        NPC.frame.Y += num;
+                        NPC.frameCounter = 0.0;
+                    }
+
+                    if (NPC.frame.Y / num >= Main.npcFrameCount[Type] - num236)
+                        NPC.frame.Y = num288;
+                }
+
+                return;
+            }
+           
+            NPC.frameCounter = 0.0;
+            NPC.frame.Y = num;
+        }
+    }
+
+    public override bool NeedSaving() => true;
+
+    public override bool CheckActive() => false;
+
+    public override bool CanChat() => true;
 
     public override ITownNPCProfile TownNPCProfile() {
         return NPCProfile;
     }
 
     public override string GetChat() {
-        WeightedRandom<string> chat = new WeightedRandom<string>();
-
-        chat.Add("1");
+        WeightedRandom<string> chat = new();
+        string key = $"Mods.RoA.NPC.Quotes.{nameof(Hunter)}.Quote";
+        for (int i = 1; i < 4; i++) {
+            chat.Add(Language.GetTextValue(key + i.ToString()));
+        }
         return chat; 
     }
 }
