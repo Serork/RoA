e130a51720fe426042892dfd707e4b34c1e47541
Serork <mandrygin.2013@mail.ru>
2025-02-10 23:06:51 +0400

    Update FireLighter.cs

--- Content/Items/Weapons/Magic/FireLighter.cs
+++ Content/Items/Weapons/Magic/FireLighter.cs
@@ -57,21 +57,20 @@ sealed class FireLighter : ModItem {
             Main.dust[dust].noGravity = true;
             Main.dust[dust].noLight = false;
         }
-        // пиздец....
         float speedX, speedY;
         speedX = velocity.X; speedY = velocity.Y;
-        float slomatKoleni = 0.25f;
-        float datLescha = (float)Math.Sqrt(speedX * speedX + speedY * speedY);
-        double mox = Math.Atan2(speedX, speedY);
-        double pososi = mox + 0.4f * slomatKoleni;
-        double unichtozhitHrebet = mox + 0f * slomatKoleni;
-        double tsoy = mox - 0.4f * slomatKoleni;
-        float davalka = Main.rand.NextFloat() * 0.2f + 0.95f;
-        speedX = datLescha * davalka * (float)Math.Sin(pososi);
-        speedY = datLescha * davalka * (float)Math.Cos(pososi);
-        Projectile.NewProjectile(source, position.X, position.Y, datLescha * davalka * (float)Math.Sin(pososi), datLescha * davalka * (float)Math.Cos(pososi), type, damage, knockback, player.whoAmI, ai2: player.direction);
-        Projectile.NewProjectile(source, position.X, position.Y, datLescha * davalka * (float)Math.Sin(tsoy), datLescha * davalka * (float)Math.Cos(tsoy), type, damage, knockback, player.whoAmI, ai2: player.direction);
-        Projectile.NewProjectile(source, position.X, position.Y, datLescha * davalka * (float)Math.Sin(unichtozhitHrebet), datLescha * davalka * (float)Math.Cos(unichtozhitHrebet), type, damage, knockback, player.whoAmI, ai2: player.direction);
+        float num0 = 0.25f;
+        float num1 = (float)Math.Sqrt(speedX * speedX + speedY * speedY);
+        double num2 = Math.Atan2(speedX, speedY);
+        double num3 = num2 + 0.4f * num0;
+        double num4 = num2 + 0f * num0;
+        double num5 = num2 - 0.4f * num0;
+        float num6 = Main.rand.NextFloat() * 0.2f + 0.95f;
+        speedX = num1 * num6 * (float)Math.Sin(num3);
+        speedY = num1 * num6 * (float)Math.Cos(num3);
+        Projectile.NewProjectile(source, position.X, position.Y, num1 * num6 * (float)Math.Sin(num3), num1 * num6 * (float)Math.Cos(num3), type, damage, knockback, player.whoAmI, ai2: player.direction);
+        Projectile.NewProjectile(source, position.X, position.Y, num1 * num6 * (float)Math.Sin(num5), num1 * num6 * (float)Math.Cos(num5), type, damage, knockback, player.whoAmI, ai2: player.direction);
+        Projectile.NewProjectile(source, position.X, position.Y, num1 * num6 * (float)Math.Sin(num4), num1 * num6 * (float)Math.Cos(num4), type, damage, knockback, player.whoAmI, ai2: player.direction);
         return false;
     }
 }
