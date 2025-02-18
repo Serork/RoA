cb47fc95cb45229da9ca7fc8aead249c71d7cc55
Serork <mandrygin.2013@mail.ru>
2025-02-19 00:30:32 +0400

    Adjustments

---
+++ Content/Items/Placeable/Furniture/BackwoodsStoneChest.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json.Linq;
+
+using RoA.Content.Items.Placeable.Crafting;
+using RoA.Core;
+
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace RoA.Content.Items.Placeable.Furniture;
+
+sealed class BackwoodsStoneChest : ModItem {
+	public override void SetDefaults() {
+		Item.SetSize(32, 30);
+
+		Item.SetDefaultToUsable(ItemUseStyleID.Swing, 10, 15, useTurn: true, autoReuse: true);
+
+        Item.SetDefaultToStackable(Terraria.Item.CommonMaxStack);
+
+        Item.value = 500;
+
+        Item.createTile = ModContent.TileType<Tiles.Furniture.BackwoodsStoneChest>();
+	}
+
+	public override void AddRecipes() {
+		CreateRecipe()
+			.AddIngredient<GrimstoneBrick>(8)
+			.AddRecipeGroup(RecipeGroupID.IronBar, 2)
+			.AddTile(TileID.WorkBenches)
+			.Register();
+	}
+}
