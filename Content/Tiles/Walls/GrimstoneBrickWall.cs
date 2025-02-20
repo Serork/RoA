6795bdd88e8923a1865e7665db43404fbae9b133
Serork <mandrygin.2013@mail.ru>
2025-02-20 15:06:46 +0400

    Update GrimstoneBrickWall.cs

--- Content/Tiles/Walls/GrimstoneBrickWall.cs
+++ Content/Tiles/Walls/GrimstoneBrickWall.cs
@@ -10,7 +10,7 @@ sealed class GrimstoneBrickWall : ModWall {
         Main.wallHouse[Type] = true;
 
         DustType = (ushort)ModContent.DustType<Dusts.Backwoods.Stone>();
-		AddMapEntry(new Color(34, 37, 46));
+		AddMapEntry(new Color(54, 55, 64));
 	}
 
 	public override void NumDust(int i, int j, bool fail, ref int num) => num = fail ? 1 : 3;
