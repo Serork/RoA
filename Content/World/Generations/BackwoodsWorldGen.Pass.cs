0ac7cff48ad2566d1d609700bdf4fbe5e4fb91d6
Serork <mandrygin.2013@mail.ru>
2025-02-23 18:04:02 +0400

    Update BackwoodsWorldGen.Pass.cs

--- Content/World/Generations/BackwoodsWorldGen.Pass.cs
+++ Content/World/Generations/BackwoodsWorldGen.Pass.cs
@@ -4,6 +4,7 @@ using ReLogic.Utilities;
 
 using RoA.Common.BackwoodsSystems;
 using RoA.Common.Sets;
+using RoA.Common.Tiles;
 using RoA.Common.WorldEvents;
 using RoA.Content.Items.Consumables;
 using RoA.Content.Items.Equipables.Accessories;
@@ -2353,11 +2354,29 @@ sealed class BackwoodsBiomePass(string name, double loadWeight) : GenPass(name, 
                 if (tile.ActiveTile(_grassTileType) && WorldGen.SolidTile(x, y) && !aboveTile.HasTile) {
                     tile = WorldGenHelper.GetTileSafely(x, y - 1);
                     if (_random.NextBool(6) && (tile.WallType == _grassWallType || tile.WallType == _flowerGrassWallType || tile.WallType == _leavesWallType)) {
-                        tile.HasTile = true;
-                        tile.TileFrameY = 0;
-                        tile.TileType = _mintTileType;
-                        tile.TileFrameX = (short)(18 * 2);
-                        ModContent.GetInstance<MiracleMintTE>().Place(x, y - 1);
+                        int num3 = 15;
+                        int num4 = 2;
+                        int num5 = 0;
+                        num3 = (int)((double)num3 * ((double)Main.maxTilesX / 4200.0));
+                        int num6 = Utils.Clamp(x - num3, 4, Main.maxTilesX - 4);
+                        int num7 = Utils.Clamp(x + num3, 4, Main.maxTilesX - 4);
+                        int num8 = Utils.Clamp(y - num3, 4, Main.maxTilesY - 4);
+                        int num9 = Utils.Clamp(y + num3, 4, Main.maxTilesY - 4);
+                        for (int i2 = num6; i2 <= num7; i2++) {
+                            for (int j2 = num8; j2 <= num9; j2++) {
+                                int checkTileType = Main.tile[i2, j2].TileType;
+                                if (checkTileType >= TileID.Count && TileLoader.GetTile(checkTileType) is PlantBase) {
+                                    num5++;
+                                }
+                            }
+                        }
+                        if (num5 < num4) {
+                            tile.HasTile = true;
+                            tile.TileFrameY = 0;
+                            tile.TileType = _mintTileType;
+                            tile.TileFrameX = (short)(18 * 2);
+                            ModContent.GetInstance<MiracleMintTE>().Place(x, y - 1);
+                        }
                     }
                     else {
                         tile.HasTile = true;
