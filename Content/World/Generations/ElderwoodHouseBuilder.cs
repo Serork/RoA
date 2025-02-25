df01cd7b259a0a3689f8ebfe7314fa8dd3ee969d
Serork <mandrygin.2013@mail.ru>
2025-02-25 14:36:55 +0400

    Update ElderwoodHouseBuilder.cs

--- Content/World/Generations/ElderwoodHouseBuilder.cs
+++ Content/World/Generations/ElderwoodHouseBuilder.cs
@@ -726,7 +726,7 @@ public class HouseBuilderCustom {
                 context.SharpenerCount++;
         }
 
-        bool flag = !_placedTanningRack && _random.NextBool(5);
+        bool flag = _random.NextBool(5);
         if (_placedTanningRack && !flag) {
             return;
         }
