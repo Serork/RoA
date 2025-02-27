3b09c4a5d90534e3f7cd98d481494a416bf218f2
Serork <mandrygin.2013@mail.ru>
2025-02-27 23:29:35 +0400

    Update BaseWreathItem.cs

--- Content/Items/Equipables/Wreaths/BaseWreathItem.cs
+++ Content/Items/Equipables/Wreaths/BaseWreathItem.cs
@@ -1,9 +1,26 @@
 ﻿using Terraria;
+using Terraria.GameContent.Creative;
 using Terraria.ID;
+using Terraria.UI;
+
+using static Terraria.GameContent.Creative.ItemFilters.AAccessories;
 
 namespace RoA.Content.Items.Equipables.Wreaths;
 
 abstract class BaseWreathItem : NatureItem {
+    public override void Load() {
+        On_ItemFilters.AAccessories.IsAnAccessoryOfType += AAccessories_IsAnAccessoryOfType;
+    }
+
+    private bool AAccessories_IsAnAccessoryOfType(On_ItemFilters.AAccessories.orig_IsAnAccessoryOfType orig, ItemFilters.AAccessories self, Item entry, ItemFilters.AAccessories.AccessoriesCategory categoryType) {
+        bool flag = entry.ModItem != null && entry.ModItem is BaseWreathItem;
+        if (flag && categoryType == AccessoriesCategory.Misc) {
+            return true;
+        }
+
+        return orig(self, entry, categoryType);
+    }
+
     public override bool CanEquipAccessory(Player player, int slot, bool modded) => modded;
 
     public sealed override void SetStaticDefaults() {
