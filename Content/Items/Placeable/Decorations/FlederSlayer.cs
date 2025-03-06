929843e47b07206e11d45d95af431d949b4e3b01
Nihil, The Demiurge <arslan.tim.2002@gmail.com>
2025-03-06 14:51:35 +0500

    item sprites

--- Content/Items/Placeable/Decorations/FlederSlayer.cs
+++ Content/Items/Placeable/Decorations/FlederSlayer.cs
@@ -6,7 +6,9 @@ namespace RoA.Content.Items.Placeable.Decorations;
 
 sealed class FlederSlayerDecoration : ModItem {
     public override void SetDefaults() {
-        Item.CloneDefaults(ItemID.BloodMoonMonolith);
+        Item.SetSize(20, 38);
+		
+		Item.CloneDefaults(ItemID.BloodMoonMonolith);
         Item.createTile = ModContent.TileType<Tiles.Decorations.FlederSlayerDecoration>();
         Item.placeStyle = 0;
     }
