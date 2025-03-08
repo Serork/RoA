1e2d06d1179e0e74a00ebf5b9dd0cb3f7cfd0bec
Serork <mandrygin.2013@mail.ru>
2025-03-08 15:24:39 +0400

    dawdawd

--- Content/NPCs/Enemies/Bosses/Lothor/Summon/DruidSoul.Behaviour.cs
+++ Content/NPCs/Enemies/Bosses/Lothor/Summon/DruidSoul.Behaviour.cs
@@ -109,9 +109,12 @@ sealed partial class DruidSoul : RoANPC {
         target = NPC.target;
         Player player = Main.player[target];
         if (LothorSummoningHandler.PreArrivedLothorBoss.Item1 || player.dead || !player.InModBiome<BackwoodsBiome>() || NPC.CountNPCS(Type) > 1 || !NPC.downedBoss2) {
-            NPC.KillNPC();
+            NPC.Opacity -= 0.05f;
+            if (NPC.Opacity <= 0f) {
+                NPC.KillNPC();
 
-            SoundEngine.PlaySound(SoundID.NPCDeath6 with { Pitch = 0.2f, Volume = 0.5f }, NPC.Center);
+                SoundEngine.PlaySound(SoundID.NPCDeath6 with { Pitch = 0.2f, Volume = 0.5f }, NPC.Center);
+            }
         }
     }
 
