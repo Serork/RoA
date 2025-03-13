6ac9fbc0932427b51f1c3dd5109b35d249fa8d66
Serork <mandrygin.2013@mail.ru>
2025-03-13 15:58:53 +0400

    Update MercuriumBulletProjectile.cs

--- Content/Projectiles/Friendly/Ranged/Ammo/MercuriumBulletProjectile.cs
+++ Content/Projectiles/Friendly/Ranged/Ammo/MercuriumBulletProjectile.cs
@@ -28,7 +28,7 @@ sealed class MercuriumBulletProjectile : ModProjectile {
         Projectile.light = 0.5f; // How much light emit around the projectile
         Projectile.ignoreWater = true; // Does the projectile's speed be influenced by water?
         Projectile.tileCollide = true; // Can the projectile collide with tiles?
-        Projectile.extraUpdates = 1; // Set to above 0 if you want the projectile to update multiple time in a frame
+        Projectile.extraUpdates = 0; // Set to above 0 if you want the projectile to update multiple time in a frame
 
         AIType = ProjectileID.Bullet; // Act exactly like default Bullet
     }
