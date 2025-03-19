fbe7437a9351fc6ad8273a2a85419dfe6133cdff
Serork <mandrygin.2013@mail.ru>
2025-03-19 23:57:01 +0400

    Adjustments

--- Common/CameraSystem/TrailerCameraTest.cs
+++ Common/CameraSystem/TrailerCameraTest.cs
@@ -9,46 +9,46 @@ using Terraria;
 using Terraria.ModLoader;
 
 namespace RoA.Common.CameraSystem;
-sealed class TrailerCameraTest : ModPlayer {
-    private bool _shifted = true;
+//sealed class TrailerCameraTest : ModPlayer {
+//    private bool _shifted = true;
 
-    private static float EaseFunctionValue(float value1, float value2, float amount) {
-        float result = MathHelper.Clamp(amount, 0f, 1f);
-        //result = MathHelper.Hermite(value1, 0f, value2, 0f, result);
+//    private static float EaseFunctionValue(float value1, float value2, float amount) {
+//        float result = MathHelper.Clamp(amount, 0f, 1f);
+//        //result = MathHelper.Hermite(value1, 0f, value2, 0f, result);
 
-        result = MathHelper.Lerp(value1, value2, Ease.SineIn(Ease.SineOut(amount)));
+//        result = MathHelper.Lerp(value1, value2, Ease.SineIn(Ease.SineOut(amount)));
 
-        return result;
-    }
+//        return result;
+//    }
 
-    private static Vector2 EaseFunction(Vector2 value1, Vector2 value2, float amount) {
-        return new Vector2(
-            EaseFunctionValue(value1.X, value2.X, amount),
-            EaseFunctionValue(value1.Y, value2.Y, amount)
-        );
-    }
+//    private static Vector2 EaseFunction(Vector2 value1, Vector2 value2, float amount) {
+//        return new Vector2(
+//            EaseFunctionValue(value1.X, value2.X, amount),
+//            EaseFunctionValue(value1.Y, value2.Y, amount)
+//        );
+//    }
 
-    private class LightHackGlobalWall : GlobalWall {
-        public override void ModifyLight(int i, int j, int type, ref float r, ref float g, ref float b) {
-            //if (CameraSystem.AsymetricalPanModifier.Progress > 0) {
-            //    float value = 1f - CameraSystem.AsymetricalPanModifier.Progress;
-            //    r = MathHelper.Clamp(r + value, 0, 1);
-            //    g = MathHelper.Clamp(g + value, 0, 1);
-            //    b = MathHelper.Clamp(b + value, 0, 1);
-            //}
-        }
-    }
+//    private class LightHackGlobalWall : GlobalWall {
+//        public override void ModifyLight(int i, int j, int type, ref float r, ref float g, ref float b) {
+//            //if (CameraSystem.AsymetricalPanModifier.Progress > 0) {
+//            //    float value = 1f - CameraSystem.AsymetricalPanModifier.Progress;
+//            //    r = MathHelper.Clamp(r + value, 0, 1);
+//            //    g = MathHelper.Clamp(g + value, 0, 1);
+//            //    b = MathHelper.Clamp(b + value, 0, 1);
+//            //}
+//        }
+//    }
 
-    public override void PostUpdate() {
-        if (Helper.JustPressed(Keys.F5)) {
-            _shifted = false;
-        }
+//    public override void PostUpdate() {
+//        if (Helper.JustPressed(Keys.F5)) {
+//            _shifted = false;
+//        }
 
-        if (!_shifted) {
-            Point altarPosition = new Point(BackwoodsVars.BackwoodsStartX + 9, BackwoodsVars.FirstTileYAtCenter - 25);
-            Point backwoodsBottomPosition = new Point(BackwoodsVars.BackwoodsStartX + 9, BackwoodsVars.FirstTileYAtCenter + BackwoodsVars.BackwoodsSizeY * 2 - BackwoodsVars.BackwoodsSizeY / 2);
-            CameraSystem.AsymetricalPan(BackwoodsVars.BackwoodsSizeY * 10, 240, BackwoodsVars.BackwoodsSizeY, backwoodsBottomPosition.ToWorldCoordinates(), altarPosition.ToWorldCoordinates(), EaseFunction);
-            _shifted = true;
-        }
-    }
-}
+//        if (!_shifted) {
+//            Point altarPosition = new Point(BackwoodsVars.BackwoodsStartX + 9, BackwoodsVars.FirstTileYAtCenter - 25);
+//            Point backwoodsBottomPosition = new Point(BackwoodsVars.BackwoodsStartX + 9, BackwoodsVars.FirstTileYAtCenter + BackwoodsVars.BackwoodsSizeY * 2 - BackwoodsVars.BackwoodsSizeY / 2);
+//            CameraSystem.AsymetricalPan(BackwoodsVars.BackwoodsSizeY * 10, 240, BackwoodsVars.BackwoodsSizeY, backwoodsBottomPosition.ToWorldCoordinates(), altarPosition.ToWorldCoordinates(), EaseFunction);
+//            _shifted = true;
+//        }
+//    }
+//}
