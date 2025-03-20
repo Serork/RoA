d0630d0b834826e070e144600f0ac884185e560b
Serork <mandrygin.2013@mail.ru>
2025-03-20 06:52:56 +0400

    Update BackwoodsWorldGen.Pass.cs

--- Content/World/Generations/BackwoodsWorldGen.Pass.cs
+++ Content/World/Generations/BackwoodsWorldGen.Pass.cs
@@ -2222,7 +2222,7 @@ sealed class BackwoodsBiomePass(string name, double loadWeight) : GenPass(name, 
             Point killPos = killTiles[_random.Next(killTiles.Count)];
             Tile tile = WorldGenHelper.GetTileSafely(killPos.X, killPos.Y);
             if ((tile.ActiveWall(placeholderWallType) || tile.ActiveWall(_elderwoodWallType)) && !tile.HasTile) {
-                if (_random.NextBool(22)) {
+                if (_random.NextBool(25)) {
                     WorldGen.PlaceTile(killPos.X, killPos.Y, TileID.Torches, style: 1);
                     if (TileID.Sets.Torch[Main.tile[killPos.X, killPos.Y].TileType]) {
                         placedTorch = true;
