d988e4c23e09b64dcdbe46a86a4d2128f09ee52e
Serork <mandrygin.2013@mail.ru>
2025-03-23 15:37:40 +0400

    Fixes

--- Content/Tiles/Miscellaneous/LothorMusicBox.cs
+++ Content/Tiles/Miscellaneous/LothorMusicBox.cs
@@ -2,6 +2,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 using Terraria;
+using Terraria.DataStructures;
 using Terraria.GameContent;
 using Terraria.GameContent.Drawing;
 using Terraria.ID;
@@ -53,6 +54,27 @@ sealed class LothorMusicBox : MusicBox {
                 Rectangle rect = new(tile.TileFrameX, tile.TileFrameY, 16, height);
                 Main.spriteBatch.Draw(sourceRectangle: rect, texture: highlightTexture, position: drawPosition, color: highlightColor, rotation: 0f, origin: Vector2.Zero, scale: 1f,
                     effects: SpriteEffects.None, layerDepth: 0f);
+            }
+        }
+
+        if (!(Main.gamePaused || !Main.instance.IsActive || Lighting.UpdateEveryFrame && !Main.rand.NextBool(4))) {
+            if (tile.TileFrameX == 36 && (int)Main.timeForVisualEffects % 7 == 0 && Main.rand.NextBool(3)) {
+                int goreType = Main.rand.Next(570, 573);
+                Vector2 position = new Vector2(i * 16 + 8 + GoreOffsetX, j * 16 - 10);
+                Vector2 velocity = new Vector2(Main.WindForVisuals * 2f, -0.5f);
+                velocity.X *= 1f + Main.rand.NextFloat(-0.5f, 0.5f);
+                velocity.Y *= 1f + Main.rand.NextFloat(-0.5f, 0.5f);
+                if (goreType == 572) {
+                    position.X -= 8f;
+                }
+
+                if (goreType == 571) {
+                    position.X -= 4f;
+                }
+
+                if (!Main.dedServ) {
+                    Gore.NewGore(new EntitySource_TileUpdate(i, j), position, velocity, goreType, 0.8f);
+                }
             }
         }
 
