0a8afa6d0a631401900dbc1d50f0a2c4cfe9af7b
Serork <mandrygin.2013@mail.ru>
2025-03-24 23:36:17 +0400

    Update KeywordSystem.cs

--- Common/Text/KeywordSystem.cs
+++ Common/Text/KeywordSystem.cs
@@ -7,17 +7,61 @@ using RoA.Core;
 using RoA.Core.Utility;
 
 using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
+using System.Text.RegularExpressions;
 
 using Terraria;
 using Terraria.GameContent;
+using Terraria.Localization;
 using Terraria.ModLoader;
 using Terraria.UI.Chat;
+
+using KeywordInfo = (byte, string);
 
 namespace RoA.Common.Text;
 
 sealed class KeywordSystem : ILoadable {
     internal static float _keywordColorOpacity = 1f, _keywordColorOpacity2;
+
+    private sealed class KeywordSystemForVanillaTooltips : GlobalItem {
+        private readonly List<KeywordInfo> _keywords_EN = [(0, "mana"), (1, "life"), (1, "health")];
+        private readonly List<KeywordInfo> _keywords_RU = [
+            (0, "мана"),  (0, "маны"),  (0, "мане"), (0, "ману"), (0, "маной"), (0, "маною"), (0, "мане"),
+            (1, "жизнь"), (1, "жизни"), (1, "жизнь"), (1, "жизнью"),
+            (1, "жизни"), (1, "жизней"), (1, "жизням"), (1, "жизнями"), (1, "жизнях"),
+            (1, "здоровье"), (1, "здоровья"), (1, "здоровью"), (1, "здоровьем"),
+        ];
+
+        public override bool InstancePerEntity => true;
+
+        public override void ModifyTooltips(Item item, List<TooltipLine> tooltips) {
+            List<KeywordInfo> checkArray = _keywords_EN;
+            if (GameCulture.FromCultureName(GameCulture.CultureName.Russian).IsActive) {
+                checkArray = _keywords_RU;
+            }
+            foreach (TooltipLine tooltip in tooltips) {
+                if (tooltip.Name == "ItemName") {
+                    continue;
+                }
+                char[] checks = ['l', 'm'];
+                for (int i = 0; i < checkArray.Count; i++) { 
+                    char tag = checkArray[i].Item1 == 0 ? checks[1] : checks[0];
+                    string keyword = checkArray[i].Item2;
+                    tooltip.Text = Regex.Replace(tooltip.Text, $@"\b{keyword}\b", $"[kw/{tag}:" + keyword + "]");
+                    keyword = checkArray[i].Item2.ToUpper();
+                    tooltip.Text = Regex.Replace(tooltip.Text, $@"\b{keyword}\b", $"[kw/{tag}:" + keyword + "]");
+                }
+                foreach (char check in checks) {
+                    string pattern = $"[kw/{check}:[kw/{check}:";
+                    tooltip.Text = tooltip.Text.Replace(pattern, $"[kw/{check}:");
+                    pattern = "]]";
+                    tooltip.Text = tooltip.Text.Replace(pattern, "]");
+                }
+            }
+        }
+    }
 
     public class KeywordTagHandler : ITagHandler {
         TextSnippet ITagHandler.Parse(string text, Color baseColor, string options) {
@@ -28,10 +72,17 @@ sealed class KeywordSystem : ILoadable {
                         continue;
                     }
                     switch (array[i][0]) {
-                        // nature highlight
                         case 'n':
                             float opacity = Ease.CubeOut(Helper.EaseInOut3(_keywordColorOpacity));
                             baseColor = Color.Lerp(Main.MouseTextColorReal, WreathHandler.GetCurrentColor(Main.LocalPlayer), opacity);
+                            break;
+                        case 'm':
+                            opacity = Ease.CubeOut(Helper.EaseInOut3(_keywordColorOpacity));
+                            baseColor = Color.Lerp(Main.MouseTextColorReal, new Color(134, 199, 252), opacity);
+                            break;
+                        case 'l':
+                            opacity = Ease.CubeOut(Helper.EaseInOut3(_keywordColorOpacity));
+                            baseColor = Color.Lerp(Main.MouseTextColorReal, new Color(252, 145, 134), opacity);
                             break;
                     }
                 }
@@ -74,7 +125,7 @@ sealed class KeywordSystem : ILoadable {
                 if (flag4) {
                     _keywordColorOpacity = 1f;
                 }
-                else if ((!Main.HoverItem.IsEmpty() && Main.HoverItem.IsDruidic()) || flag3 || flag || FancyWreathDrawing.IsHoveringUI || WreathDrawing.JustDrawn) {
+                else if ((!Main.HoverItem.IsEmpty()/* && Main.HoverItem.IsDruidic()*/) || flag3 || flag || FancyWreathDrawing.IsHoveringUI || WreathDrawing.JustDrawn) {
                     if (_keywordColorOpacity > 0f) {
                         _keywordColorOpacity -= TimeSystem.LogicDeltaTime * 0.5f;
                     }
