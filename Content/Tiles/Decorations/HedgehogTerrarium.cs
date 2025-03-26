1eee34785fa718085ca42b257bc4931a72395375
Serork <mandrygin.2013@mail.ru>
2025-03-26 19:33:40 +0400

    Update HedgehogTerrarium.cs

--- Content/Tiles/Decorations/HedgehogTerrarium.cs
+++ Content/Tiles/Decorations/HedgehogTerrarium.cs
@@ -1,5 +1,7 @@
 using Microsoft.Xna.Framework;
 
+using RoA.Content.Items.Placeable;
+
 using Terraria;
 using Terraria.GameContent.Drawing;
 using Terraria.ID;
@@ -36,12 +38,90 @@ sealed class HedgehogTerrarium : ModTile {
         Main.critterCage = true; // Vanilla doesn't run the animation code for critters unless this is checked
     }
 
+    public static int GetCageFrame(int x, int y, int tileFrameX, int tileFrameY) {
+        int num = x - tileFrameX / 18;
+        int num2 = y - tileFrameY / 18;
+        return num / 6 * (num2 / 3) % cageFrames;
+    }
+
+    public static int cageFrames = 43;
+    public static int[] hedgehogCageFrame = new int[cageFrames];
+    public static int[] hedgehogCageFrameCounter = new int[cageFrames];
+
+    public override void Load() {
+        On_Main.AnimateTiles_CritterCages += On_Main_AnimateTiles_CritterCages;
+    }
+
+    private void On_Main_AnimateTiles_CritterCages(On_Main.orig_AnimateTiles_CritterCages orig) {
+        orig();
+
+        if (!Main.critterCage)
+            return;
+
+        var rand = Main.rand;
+
+        for (int i = 0; i < cageFrames; i++) {
+            if (hedgehogCageFrame[i] == 0) {
+                hedgehogCageFrameCounter[i]++;
+                if (hedgehogCageFrameCounter[i] <= rand.Next(30, 900))
+                    continue;
+
+                if (rand.Next(10) == 0) {
+                    hedgehogCageFrame[i] = 2;
+                }
+
+                hedgehogCageFrameCounter[i] = 0;
+            }
+            else if (hedgehogCageFrame[i] >= 1 && hedgehogCageFrame[i] <= 10) {
+                hedgehogCageFrameCounter[i]++;
+                if (hedgehogCageFrameCounter[i] >= 5) {
+                    hedgehogCageFrameCounter[i] = 0;
+                    hedgehogCageFrame[i]++;
+                    if (hedgehogCageFrame[i] >= 10) {
+                        hedgehogCageFrame[i] = 11;
+                    }
+                }
+            }
+            else if (hedgehogCageFrame[i] == 11) {
+                hedgehogCageFrameCounter[i]++;
+                if (hedgehogCageFrameCounter[i] <= rand.Next(30, 900))
+                    continue;
+
+                if (rand.Next(3) != 0) {
+                    if (rand.Next(7) == 0)
+                        hedgehogCageFrame[i] = 13;
+                    else
+                        hedgehogCageFrame[i] = 12;
+                }
+
+                hedgehogCageFrameCounter[i] = 0;
+            }
+            else if (hedgehogCageFrame[i] == 12) {
+                hedgehogCageFrameCounter[i]++;
+                if (hedgehogCageFrameCounter[i] >= 10) {
+                    hedgehogCageFrameCounter[i] = 0;
+                    hedgehogCageFrame[i] = 11;
+                }
+            }
+            else if (hedgehogCageFrame[i] >= 13) {
+                hedgehogCageFrameCounter[i]++;
+                if (hedgehogCageFrameCounter[i] >= 5) {
+                    hedgehogCageFrameCounter[i] = 0;
+                    hedgehogCageFrame[i]++;
+                }
+
+                if (hedgehogCageFrame[i] > 41)
+                    hedgehogCageFrame[i] = 0;
+            }
+        }
+    }
+
     public override void AnimateIndividualTile(int type, int i, int j, ref int frameXOffset, ref int frameYOffset) {
         Tile tile = Main.tile[i, j];
         // The GetSmallAnimalCageFrame method utilizes some math to stagger each individual tile. First the top left tile is found, then those coordinates are passed into some math to stagger an index into Main.snail2CageFrame
         // Main.frogCageFrame is used since we want the same animation, but if we wanted a different frame count or a different animation timing, we could write our own by adapting vanilla code and placing the code in AnimateTile
-        int tileCageFrameIndex = TileDrawing.GetSmallAnimalCageFrame(i, j, tile.TileFrameX, tile.TileFrameY);
-        frameYOffset = Main.bunnyCageFrame[tileCageFrameIndex] * AnimationFrameHeight;
+        int tileCageFrameIndex = GetCageFrame(i, j, tile.TileFrameX, tile.TileFrameY);
+        frameYOffset = hedgehogCageFrame[tileCageFrameIndex] * AnimationFrameHeight;
     }
 
     public override void NumDust(int i, int j, bool fail, ref int num) {
