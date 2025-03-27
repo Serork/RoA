9ffd09b7fc5cb517381f15ca68339d9aaf11dd44
Serork <mandrygin.2013@mail.ru>
2025-03-27 16:45:00 +0400

    Update Snatcher.cs

--- Content/Projectiles/Friendly/Druidic/Snatcher.cs
+++ Content/Projectiles/Friendly/Druidic/Snatcher.cs
@@ -303,7 +303,8 @@ sealed class Snatcher : NatureProjectile {
                 }
             }
         }
-        if (Projectile.owner == Main.myPlayer && player.itemAnimation > player.itemAnimationMax - player.itemAnimationMax / 2 && !flag && !IsAttacking && !IsAttacking2) {
+        if (Projectile.owner == Main.myPlayer && 
+            Main.mouseLeft && !flag && !IsAttacking && !IsAttacking2) {
             Projectile.ai[2] = 5f;
 
             Vector2 mousePos = Helper.GetLimitedPosition(player.Center, _mousePos, 200f, DIST * 0.75f);
