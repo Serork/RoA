568dcb0d6cb0c9f8858934133ce23740bf3fdcb2
Serork <mandrygin.2013@mail.ru>
2025-03-28 19:42:17 +0400

    Update BackwoodsBigTree.cs

--- Content/Tiles/Ambient/LargeTrees/BackwoodsBigTree.cs
+++ Content/Tiles/Ambient/LargeTrees/BackwoodsBigTree.cs
@@ -236,7 +236,7 @@ sealed class BackwoodsBigTree : ModTile, ITileHaveExtraDraws, IRequireMinAxePowe
         PlaceTrunk(pointToStartPlacingTrunk, height, placeRand, gen);
 
         for (int checkY = j - (int)(height * 2f); checkY < j; checkY++) {
-            for (int checkX = i - 2; checkX < i + 3; checkX++) {
+            for (int checkX = i - 2; checkX < i + 4; checkX++) {
                 Tile tile2 = WorldGenHelper.GetTileSafely(checkX, checkY);
                 if (tile2.TileType == TileID.Trees || tile2.TileType == ModContent.TileType<TreeBranch>()) {
                     bool flag3 = true;
