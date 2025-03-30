5fa1528df8b2a513b7707aa4ea6bf433bc618e1e
Serork <mandrygin.2013@mail.ru>
2025-03-30 19:18:58 +0400

    Update Bloodlust.cs

--- Content/Buffs/Bloodlust.cs
+++ Content/Buffs/Bloodlust.cs
@@ -21,7 +21,7 @@ sealed class BloodlustPlayer : ModPlayer {
 
     public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone) {
         if (bloodlustEffect & target.life <= 0) {
-            BloodlustHeal(Math.Max(1, (int)(Player.statLifeMax2 * Main.rand.NextFloat(0.005f, 0.01f))));
+            BloodlustHeal(Math.Max(1, (int)(Player.statLifeMax2 * Main.rand.NextFloat(0.005f, 0.011f))));
         }
     }
 
