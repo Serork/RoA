235b430f10524ae678f2de14192430ebd7a8ffd1
Serork <mandrygin.2013@mail.ru>
2025-04-05 13:21:44 +0400

    Update JungleWreath2.cs

--- Content/Items/Equipables/Wreaths/JungleWreath2.cs
+++ Content/Items/Equipables/Wreaths/JungleWreath2.cs
@@ -27,7 +27,43 @@ sealed class JungleWreath2 : BaseWreathItem {
         
 		if (handler.IsFull1) {
             if (player.thorns < 1f) player.thorns += 0.5f;
-            player.GetModPlayer<JungleWreathPlayer>().poisonedSkin = true;
+            player.GetModPlayer<JungleWreathPlayer>().poisonedSkin2 = true;
+        }
+    }
+}
+
+sealed class JungleWreathPlayer : ModPlayer {
+    public bool poisonedSkin2;
+
+    public override void ResetEffects() => poisonedSkin2 = false;
+
+    public override void OnHitByNPC(NPC npc, Player.HurtInfo hurtInfo) {
+        if (!poisonedSkin2) {
+            return;
+        }
+
+        npc.AddBuff(BuffID.Poisoned, 150, false);
+
+        float num2 = Player.thorns;
+        Rectangle rectangle = new Rectangle((int)Player.position.X, (int)Player.position.Y, Player.width, Player.height);
+        Rectangle npcRect = new Rectangle((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height);
+        bool num = Player.CanParryAgainst(rectangle, npcRect, npc.velocity);
+        float knockback = 10f;
+        if (Player.turtleThorns)
+            num2 = 2f;
+
+        if (num) {
+            num2 = 2f;
+            knockback = 5f;
+        }
+        if (Player.whoAmI == Main.myPlayer && num2 > 0f && !npc.dontTakeDamage) {
+            int damage = 15;
+            if (Main.masterMode)
+                damage = 45;
+            else if (Main.expertMode)
+                damage = 30;
+
+            Player.ApplyDamageToNPC(npc, damage, knockback, -hurtInfo.HitDirection, crit: false);
         }
     }
 }
