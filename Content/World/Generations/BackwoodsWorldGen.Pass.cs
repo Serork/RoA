94d66ffa9800de6e3da3cc1842838eead271d09b
Serork <mandrygin.2013@mail.ru>
2025-04-14 14:41:17 +0400

    Update BackwoodsWorldGen.Pass.cs

--- Content/World/Generations/BackwoodsWorldGen.Pass.cs
+++ Content/World/Generations/BackwoodsWorldGen.Pass.cs
@@ -1236,7 +1236,7 @@ sealed class BackwoodsBiomePass(string name, double loadWeight) : GenPass(name, 
                 Tile tile = WorldGenHelper.GetTileSafely(i, j);
                 if (tile.ActiveTile(treeBranch) && !WorldGenHelper.GetTileSafely(i - 1, j).ActiveTile(TileID.Trees) && !WorldGenHelper.GetTileSafely(i + 1, j).ActiveTile(TileID.Trees)) {
                     WorldGen.KillTile(i, j);
-                    if (tile.WallType != _leavesWallType) {
+                    if (WorldGenHelper.GetTileSafely(i, j - 2).WallType != _leavesWallType) {
                         WallBush2(i, j - 2, false);
                     }
                     //if (_random.NextChance(0.35)) {
