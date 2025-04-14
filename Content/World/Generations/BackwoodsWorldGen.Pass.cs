4769e157ed55c7eba54ec9877a9fb33134cc4ee4
Serork <mandrygin.2013@mail.ru>
2025-04-14 14:48:30 +0400

    Update BackwoodsWorldGen.Pass.cs

--- Content/World/Generations/BackwoodsWorldGen.Pass.cs
+++ Content/World/Generations/BackwoodsWorldGen.Pass.cs
@@ -4737,7 +4737,7 @@ sealed class BackwoodsBiomePass(string name, double loadWeight) : GenPass(name, 
         //    }
         //}
         for (int i = left - 10; i <= right + 10; i++) {
-            if (i > CenterX - 10 && i < CenterX + 19) {
+            if (i > CenterX - 10 && i < CenterX + 22) {
                 continue;
             }
             for (int j = WorldGenHelper.SafeFloatingIslandY; j < BackwoodsVars.FirstTileYAtCenter + 20; j++) {
