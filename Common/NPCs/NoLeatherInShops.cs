b93e8fb7e7834f9f1b9c0be5311f655e9dd56d6a
Serork <mandrygin.2013@mail.ru>
2025-04-18 00:43:36 +0400

    Fixes

---
+++ Common/NPCs/NoLeatherInShops.cs
@@ -0,0 +1,16 @@
+﻿using RoA.Content.Items.Miscellaneous;
+
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace RoA.Common.NPCs;
+
+sealed class NoLeatherInShops : GlobalNPC {
+    public override void ModifyShop(NPCShop shop) {
+        foreach (NPCShop.Entry entry in shop.ActiveEntries) {
+            if (entry.Item.type == ItemID.Leather) {
+                entry.Item.SetDefaults(ModContent.ItemType<AnimalLeather>());
+            }
+        }
+    }
+}
