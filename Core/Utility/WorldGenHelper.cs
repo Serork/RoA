054642877ffe2b51aab239535c1d47b517ca1f47
Serork <mandrygin.2013@mail.ru>
2025-04-19 10:57:34 +0400

    Update WorldGenHelper.cs

--- Core/Utility/WorldGenHelper.cs
+++ Core/Utility/WorldGenHelper.cs
@@ -1033,8 +1033,6 @@ static class WorldGenHelper {
                 NetMessage.SendData(34, -1, -1, null, 2, x, y, styleY);
         }
 
-        Main.NewText(flag2);
-
         if (flag2) {
             short num2 = (short)(54 * styleY);
             short num3 = (short)(54 * styleX);
