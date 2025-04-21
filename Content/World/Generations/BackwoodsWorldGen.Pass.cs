a1d9d4741fc2e865305f0ddad02e6621962693d8
Serork <mandrygin.2013@mail.ru>
2025-04-21 13:08:29 +0400

    Update BackwoodsWorldGen.Pass.cs

--- Content/World/Generations/BackwoodsWorldGen.Pass.cs
+++ Content/World/Generations/BackwoodsWorldGen.Pass.cs
@@ -2671,7 +2671,7 @@ sealed class BackwoodsBiomePass(string name, double loadWeight) : GenPass(name, 
                 if (num1048 > maxLeft + _random.NextFloat() * 15 && num1048 < maxRight - _random.NextFloat() * 15 && (Main.tile[num1048, num1052].WallType == _grassWallType || Main.tile[num1048, num1052].WallType == _flowerGrassWallType)) {
                     Main.tile[num1048, num1052].WallType = (ushort)(_random.NextBool(5) ? 59 : _dirtWallType);
                 }
-                if (num1048 > maxLeft && num1048 < maxRight && Main.tile[num1048, num1052].WallType == WallID.JungleUnsafe) {
+                if (num1048 > maxLeft + _random.NextFloat() * 15 && num1048 < maxRight - _random.NextFloat() * 15 && Main.tile[num1048, num1052].WallType == WallID.JungleUnsafe) {
                     Main.tile[num1048, num1052].WallType = (ushort)(_random.NextBool(5) ? _grassWallType : _flowerGrassWallType);
                 }
             }
