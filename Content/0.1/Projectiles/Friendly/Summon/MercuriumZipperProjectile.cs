99ee656e0a09be748c55497b504304713ba2620c
Serork <mandrygin.2013@mail.ru>
2025-06-16 01:52:01 +0300

    Update MercuriumZipperProjectile.cs

--- Content/0.1/Projectiles/Friendly/Summon/MercuriumZipperProjectile.cs
+++ Content/0.1/Projectiles/Friendly/Summon/MercuriumZipperProjectile.cs
@@ -561,7 +561,7 @@ sealed class MercuriumZipperProjectile : ModProjectile {
             // After choosing a randomized dust and a whip segment to spawn from, dust is spawned.
             Vector2 offset = (points[pointIndex] - points[pointIndex - 1]).SafeNormalize(Vector2.Zero) * 50f;
             Dust dust = Dust.NewDustDirect(spawnArea.TopLeft() +
-                offset, spawnArea.Width, spawnArea.Height, dustType, 0f, 0f, 0, Color.White);
+                offset, spawnArea.Width, spawnArea.Height, dustType, 0f, 0f, 100, Color.White);
             dust.position = points[pointIndex] + offset;
             dust.fadeIn = 0.3f;
             Vector2 spinningPoint = points[pointIndex] - points[pointIndex - 1];
