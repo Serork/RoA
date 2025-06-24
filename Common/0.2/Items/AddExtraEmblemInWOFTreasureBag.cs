87ca8110aaf556e7c9dd885606cf1c8ee8ad1b03
Serork <mandrygin.2013@mail.ru>
2025-06-25 00:28:24 +0300

    Create AddExtraEmblemInWOFTreasureBag.cs

---
+++ Common/0.2/Items/AddExtraEmblemInWOFTreasureBag.cs
@@ -0,0 +1,25 @@
+﻿using RoA.Content.Items.Equipables.Accessories;
+
+using System.Collections.Generic;
+using System.Linq;
+
+using Terraria;
+using Terraria.GameContent.ItemDropRules;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace RoA.Common.Items;
+
+sealed class AddExtraEmblemInWOFTreasureBag : GlobalItem {
+    public override void ModifyItemLoot(Item item, ItemLoot itemLoot) {
+        if (item.type == ItemID.WallOfFleshBossBag) {
+            foreach (IItemDropRule rule in itemLoot.Get()) {
+                if (rule is OneFromOptionsNotScaledWithLuckDropRule oneFromOptionsDrop && oneFromOptionsDrop.dropIds.Contains(ItemID.RangerEmblem)) {
+                    List<int> original = oneFromOptionsDrop.dropIds.ToList();
+                    original.Add(ModContent.ItemType<DruidEmblem>());
+                    oneFromOptionsDrop.dropIds = [.. original];
+                }
+            }
+        }
+    }
+}
