c8dbb784aa8a0da78dc2e41df8f33681fe188297
Serork <mandrygin.2013@mail.ru>
2025-06-26 20:11:07 +0300

    Update FlederSlayer.cs

--- Content/Projectiles/Friendly/Melee/FlederSlayer.cs
+++ Content/Projectiles/Friendly/Melee/FlederSlayer.cs
@@ -228,8 +228,7 @@ sealed class FlederSlayer : ModProjectile, DruidPlayerShouldersFix.IProjectileFi
             Projectile.Kill();
         }
         else {
-            Projectile.Center = player.MountedCenter + Vector2.Normalize(Projectile.velocity);
-            Projectile.Center = Utils.Floor(Projectile.Center);
+            Projectile.Center = Utils.Floor(player.MountedCenter) + Vector2.Normalize(Projectile.velocity);
             float dir = (float)(Math.PI / 2.0 + (double)playerDirection * 1.0);
             float offset = MathHelper.Pi / 1.15f * playerDirection;
             SoundStyle style = new SoundStyle(ResourceManager.ItemSounds + "Whisper") { Volume = 1.15f };
