ba3181b327670054aa9eb4e1cf9b227993987ac3
Serork <mandrygin.2013@mail.ru>
2025-07-21 04:46:02 +0300

    Update MagicDropper.cs

--- Content/0.2/Items/Placeable/MagicDropper.cs
+++ Content/0.2/Items/Placeable/MagicDropper.cs
@@ -1,7 +1,11 @@
-﻿using RoA.Core.Defaults;
+﻿using Microsoft.Xna.Framework.Graphics;
+
+using RoA.Core.Defaults;
 using RoA.Core.Utility;
+using RoA.Core.Utility.Extensions;
 
 using Terraria;
+using Terraria.DataStructures;
 using Terraria.ModLoader;
 
 namespace RoA.Content.Items.Placeable;
@@ -17,6 +21,35 @@ sealed class MagicTarDropper : MagicDropper {
 }
 
 abstract class MagicDropper : ModItem {
+    public override void Load() {
+        On_Player.PlaceThing_Tiles += On_Player_PlaceThing_Tiles;
+        On_PlayerDrawSet.CreateCompositeData += On_PlayerDrawSet_CreateCompositeData;
+    }
+
+    private void On_PlayerDrawSet_CreateCompositeData(On_PlayerDrawSet.orig_CreateCompositeData orig, ref PlayerDrawSet self) {
+        if (self.heldItem.IsModded(out ModItem modItem) && modItem is MagicDropper) {
+            self.itemEffect ^= SpriteEffects.FlipHorizontally | SpriteEffects.FlipVertically;
+        }
+
+        orig(ref self);
+    }
+
+    private void On_Player_PlaceThing_Tiles(On_Player.orig_PlaceThing_Tiles orig, Player self) {
+        Item item = self.inventory[self.selectedItem];
+        int tileToCreate = item.createTile;
+        if (tileToCreate < 0 || !(self.position.X / 16f - (float)Player.tileRangeX - (float)item.tileBoost - (float)self.blockRange <= (float)Player.tileTargetX) || !((self.position.X + (float)self.width) / 16f + (float)Player.tileRangeX + (float)item.tileBoost - 1f + (float)self.blockRange >= (float)Player.tileTargetX) || !(self.position.Y / 16f - (float)Player.tileRangeY - (float)item.tileBoost - (float)self.blockRange <= (float)Player.tileTargetY) || !((self.position.Y + (float)self.height) / 16f + (float)Player.tileRangeY + (float)item.tileBoost - 2f + (float)self.blockRange >= (float)Player.tileTargetY))
+            return;
+        if (item.IsModded(out ModItem modItem) && modItem is MagicDropper) {
+            int i = Player.tileTargetX, j = Player.tileTargetY;
+            Tile tile = WorldGenHelper.GetTileSafely(i, j - 1);
+            if (!(tile.HasTile && !tile.BottomSlope)) {
+                return;
+            }
+        }
+
+        orig(self);
+    }
+
     protected abstract ushort DrippingTarTileTypeToPlace();
 
     public sealed override void SetDefaults() {
