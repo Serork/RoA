a1e6544ba8e617ff51d9d213fc8403eeb29ccffe
Serork <mandrygin.2013@mail.ru>
2025-07-23 21:19:41 +0300

    Update TarBiome.cs

--- Content/0.2/WorldGenerations/TarBiome.cs
+++ Content/0.2/WorldGenerations/TarBiome.cs
@@ -27,7 +27,7 @@ sealed class TarBiome_AddPass : ModSystem {
             return;
         }
 
-        tasks.Insert(tasks.FindIndex(task => task.Name == "Buried Chests") - 6, new PassLegacy("Tar", delegate (GenerationProgress progress, GameConfiguration passConfig) {
+        tasks.Insert(tasks.FindIndex(task => task.Name == "Settle Liquids Again") + 1, new PassLegacy("Tar", delegate (GenerationProgress progress, GameConfiguration passConfig) {
             int num916 = 5 * WorldGenHelper.WorldSize;
             double num917 = (double)(Main.maxTilesX - 200) / (double)num916;
             List<Point> list2 = new List<Point>(num916);
