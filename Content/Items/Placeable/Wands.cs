1a8bd9e00c93b36ce06ee13c0f3495b24bb7fced
Serork <mandrygin.2013@mail.ru>
2025-08-01 12:34:36 +0300

    tml update

--- Content/Items/Placeable/Wands.cs
+++ Content/Items/Placeable/Wands.cs
@@ -40,11 +40,50 @@ abstract class Wand : ModItem {
 
     private class ExtraLoader : ILoadable {
         public void Load(Mod mod) {
-            On_Player.PlaceThing_Tiles_PlaceIt_AutoPaintAndActuate += On_Player_PlaceThing_Tiles_PlaceIt_AutoPaintAndActuate;
+            //On_Player.PlaceThing_Tiles_PlaceIt_AutoPaintAndActuate += On_Player_PlaceThing_Tiles_PlaceIt_AutoPaintAndActuate;
+            On_WorldGen.ReplaceTile += On_WorldGen_ReplaceTile;
+            On_Player.PlaceThing_Tiles_PlaceIt_KillGrassForSolids += On_Player_PlaceThing_Tiles_PlaceIt_KillGrassForSolids;
         }
 
-        private void On_Player_PlaceThing_Tiles_PlaceIt_AutoPaintAndActuate(On_Player.orig_PlaceThing_Tiles_PlaceIt_AutoPaintAndActuate orig, Player self, int[,] typeCaches, int tileToCreate) {
+        private void On_Player_PlaceThing_Tiles_PlaceIt_KillGrassForSolids(On_Player.orig_PlaceThing_Tiles_PlaceIt_KillGrassForSolids orig, Player self) {
+            orig(self);
+
+            ConsumeWandItem(self);
+        }
+
+        private bool On_WorldGen_ReplaceTile(On_WorldGen.orig_ReplaceTile orig, int x, int y, ushort targetType, int targetStyle) {
+            bool result = orig(x, y, targetType, targetStyle);
+
+            if (!Main.dedServ) {
+                Player self = Main.LocalPlayer;
+                Item heldItem = self.HeldItem;
+                if (x == Player.tileTargetX && y == Player.tileTargetY) {
+                    ConsumeWandItem(self);
+                }
+            }
+            return result;
+        }
+
+        private void ConsumeWandItem(Player self) {
+            int selectedItemType = self.GetSelectedItem().type;
+            ModItem modItem = ItemLoader.GetItem(selectedItemType);
+            if (modItem != null && modItem is Wand wand) {
+                int tileWand = wand.ItemToConsume;
+                for (int num15 = 0; num15 < 58; num15++) {
+                    if (tileWand == self.inventory[num15].type && self.inventory[num15].stack > 0) {
+                        self.inventory[num15].stack--;
+                        if (self.inventory[num15].stack <= 0)
+                            self.inventory[num15] = new Item();
+
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void On_Player_PlaceThing_Tiles_PlaceIt_AutoPaintAndActuate(On_Player.orig_PlaceThing_Tiles_PlaceIt_AutoPaintAndActuate orig, Player self, int[,] typeCaches, int tileToCreate, Point topLeft) {
             orig(self, typeCaches, tileToCreate);
+
             int selectedItemType = self.GetSelectedItem().type;
             ModItem modItem = ItemLoader.GetItem(selectedItemType);
             if (modItem != null && modItem is Wand wand) {
