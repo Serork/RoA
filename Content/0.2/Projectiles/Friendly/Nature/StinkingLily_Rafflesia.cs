64078f3e3721eae837736d8fb4f93189eeafd0b7
Serork <mandrygin.2013@mail.ru>
2025-08-24 02:37:52 +0300

    Update StinkingLily_Rafflesia.cs

--- Content/0.2/Projectiles/Friendly/Nature/StinkingLily_Rafflesia.cs
+++ Content/0.2/Projectiles/Friendly/Nature/StinkingLily_Rafflesia.cs
@@ -152,7 +152,7 @@ sealed class Rafflesia : NatureProjectile_NoTextureLoad, IRequestAssets {
             }
             Vector2 areaSize = Projectile.Size / 2f;
             Vector2 areaCenter = owner.GetWorldMousePosition() - areaSize / 2f;
-            for (int i = 0; i < 3; i++) {
+            for (int i = 0; i < 2; i++) {
                 Fly.CreateFlyDust(areaCenter, areaCenter.DirectionTo(Projectile.Center) * Main.rand.NextFloat(0.5f, 1f) * 5f, (int)areaSize.X, (int)areaSize.Y);
             }
             Projectile.localAI[1] = 0f;
