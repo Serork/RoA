3d9796664c7c4b2f11d64292fbd3a9cdc9c3e140
Serork <mandrygin.2013@mail.ru>
2025-08-25 18:14:08 +0300

    Create StopHunterReceivingDamageFromEnemies.cs

---
+++ Common/0.2/NPCs/StopHunterReceivingDamageFromEnemies.cs
@@ -0,0 +1,21 @@
+﻿using RoA.Content.NPCs.Friendly;
+using RoA.Core;
+
+using Terraria;
+using Terraria.ModLoader;
+
+namespace RoA.Common.NPCs;
+
+sealed class StopHunterReceivingDamageFromEnemies : IInitializer {
+    public void Load(Mod mod) {
+        On_NPC.BeHurtByOtherNPC += On_NPC_BeHurtByOtherNPC;
+    }
+
+    private void On_NPC_BeHurtByOtherNPC(On_NPC.orig_BeHurtByOtherNPC orig, NPC self, int npcIndex, NPC thatNPC) {
+        if (self.type == ModContent.NPCType<Hunter>()) {
+            return;
+        }
+
+        orig(self, npcIndex, thatNPC);
+    }
+}
