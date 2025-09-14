4398d23fa3e59e48ef2538054d3883240cc14615
Serork <mandrygin.2013@mail.ru>
2025-09-14 03:24:29 +0300

    Update NixieTube.cs

--- Content/0.2/Tiles/Decorations/NixieTube.cs
+++ Content/0.2/Tiles/Decorations/NixieTube.cs
@@ -84,6 +84,10 @@ sealed class NixieTube : ModTile, TileHooks.IPostDraw {
         int frameX = tile.TileFrameX;
         bool flag = frameY >= 56;
         if (flag && TryGetTE(out NixieTubeTE? nixieTubeTE, i, j) && nixieTubeTE!.Active) {
+            if (!nixieTubeTE.Dye2.IsEmpty() && nixieTubeTE.Dye2.type == ItemID.ShadowDye) {
+                r = g = b = 0f;
+                return;
+            }
             Color? lightColor = nixieTubeTE.LightColor;
             if (lightColor == null) {
                 r = 224 / 255f;
