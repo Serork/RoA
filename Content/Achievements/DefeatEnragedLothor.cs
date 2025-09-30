45c6a8beab8f860bd71adbcfc06e3d9dc3d5aa98
Serork <mandrygin.2013@mail.ru>
2025-10-01 00:01:14 +0300

    Update DefeatEnragedLothor.cs

---
+++ Content/Achievements/DefeatEnragedLothor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+using Terraria.Achievements;
+using Terraria.GameContent.Achievements;
+using Terraria.ModLoader;
+
+namespace RoA.Content.Achievements;
+
+sealed class DefeatEnragedLothor : ModAchievement {
+    public CustomFlagCondition KillEnragedLothorCondition { get; private set; }
+
+    public override void SetStaticDefaults() {
+        Achievement.SetCategory(AchievementCategory.Challenger);
+
+        KillEnragedLothorCondition = AddCondition();
+    }
+
+    public override Position GetDefaultPosition() => new Before("REAL_ESTATE_AGENT");
+}
