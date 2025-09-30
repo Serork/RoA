a609fde15af2753937efce4ce1f208efc9890197
Serork <mandrygin.2013@mail.ru>
2025-09-30 23:58:46 +0300

    adjs

--- Content/Achievements/SurviveBackwoodsFog.cs
+++ Content/Achievements/SurviveBackwoodsFog.cs
@@ -6,11 +6,13 @@ namespace RoA.Content.Achievements;
 
 sealed class SurviveBackwoodsFog : ModAchievement {
     public CustomFlagCondition SurviveBackwoodsFogCondition { get; private set; }
+    //public CustomFlagCondition SurviveBackwoodsFogCondition_End { get; private set; }
 
     public override void SetStaticDefaults() {
         Achievement.SetCategory(AchievementCategory.Explorer);
 
         SurviveBackwoodsFogCondition = AddCondition();
+        //SurviveBackwoodsFogCondition_End = AddCondition("Condition_END");
     }
 
     public override Position GetDefaultPosition() => new After("BLOODBATH");
