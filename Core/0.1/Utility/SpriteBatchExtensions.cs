9e3af0af24ab22485ffb029ff4014d9e9a92f1ff
Serork <mandrygin.2013@mail.ru>
2025-10-05 22:35:59 +0300

    Update SpriteBatchExtensions.cs

--- Core/0.1/Utility/SpriteBatchExtensions.cs
+++ Core/0.1/Utility/SpriteBatchExtensions.cs
@@ -17,48 +17,14 @@ static partial class SpriteBatchExtensions {
     public static void LineAngle(this SpriteBatch spriteBatch, Vector2 start, float angle, float length, Color color, float thickness, SpriteEffects effects = 0, Texture2D? texture = null) =>
         spriteBatch.Draw(texture ?? ResourceManager.Pixel, start - Main.screenPosition, null, color, angle, texture != null ? texture.Size() / 2f : new Vector2(0.0f, 0.5f), new Vector2(length, thickness), effects, 0f);
 
-    public static void With(this SpriteBatch spriteBatch,
-                            BlendState blendState,
-                            Action drawAction,
-                            Effect effect = null,
-                            SamplerState samplerState = null) {
-        spriteBatch.With(blendState, false, drawAction, effect, samplerState);
-    }
-
-    public static void With(this SpriteBatch spriteBatch,
-                            BlendState blendState,
-                            bool isUI,
-                            Action drawAction,
-                            Effect effect = null,
-                            SamplerState samplerState = null,
-                            RasterizerState rasterizerState = null) {
-        bool activeShader = effect != null;
-        spriteBatch.End();
-        spriteBatch.Begin(activeShader ? default : SpriteSortMode.Immediate,
-                          blendState,
-                          samplerState ?? Main.DefaultSamplerState,
-                          activeShader ? default : DepthStencilState.None,
-                          rasterizerState ?? (activeShader ? RasterizerState.CullNone : RasterizerState.CullCounterClockwise),
-                          effect,
-                          isUI ? Main.UIScaleMatrix : Main.GameViewMatrix.TransformationMatrix);
-        drawAction();
-        spriteBatch.End();
-        spriteBatch.Begin(SpriteSortMode.Immediate,
-                          BlendState.AlphaBlend, isUI ? SamplerState.AnisotropicClamp : Main.DefaultSamplerState,
-                          DepthStencilState.None,
-                          RasterizerState.CullCounterClockwise,
-                          effect,
-                          isUI ? Main.UIScaleMatrix : Main.GameViewMatrix.TransformationMatrix);
-    }
-
     public static void BeginBlendState(this SpriteBatch spriteBatch, BlendState state, SamplerState samplerState = null, bool isUI = false, bool shader = false) {
         spriteBatch.End();
-        spriteBatch.Begin(shader ? SpriteSortMode.Immediate : isUI ? SpriteSortMode.Deferred : SpriteSortMode.Immediate, state, samplerState ?? Main.DefaultSamplerState, DepthStencilState.None, RasterizerState.CullCounterClockwise, null, isUI ? Main.UIScaleMatrix : Main.GameViewMatrix.ZoomMatrix);
+        spriteBatch.Begin(shader ? SpriteSortMode.Immediate : SpriteSortMode.Deferred, state, samplerState ?? Main.DefaultSamplerState, DepthStencilState.None, RasterizerState.CullCounterClockwise, null, isUI ? Main.UIScaleMatrix : Main.GameViewMatrix.ZoomMatrix);
     }
 
     public static void EndBlendState(this SpriteBatch spriteBatch, bool isUI = false) {
         spriteBatch.End();
-        spriteBatch.Begin(isUI ? SpriteSortMode.Deferred : SpriteSortMode.Immediate, BlendState.AlphaBlend, isUI ? SamplerState.AnisotropicClamp : Main.DefaultSamplerState, DepthStencilState.None, RasterizerState.CullCounterClockwise, null, isUI ? Main.UIScaleMatrix : Main.GameViewMatrix.ZoomMatrix);
+        spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, isUI ? SamplerState.AnisotropicClamp : Main.DefaultSamplerState, DepthStencilState.None, RasterizerState.CullCounterClockwise, null, isUI ? Main.UIScaleMatrix : Main.GameViewMatrix.ZoomMatrix);
     }
 
     public static void BeginWorld(this SpriteBatch spriteBatch, bool shader = false, Matrix? overrideMatrix = null, BlendState state = null) {
