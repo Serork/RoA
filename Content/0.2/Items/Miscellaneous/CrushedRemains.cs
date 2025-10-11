1eb33ea0f2613dd0daddd0befd2716e9bc1cccd0
Serork <mandrygin.2013@mail.ru>
2025-10-11 23:39:16 +0300

    adjs

--- Content/0.2/Items/Miscellaneous/CrushedRemains.cs
+++ Content/0.2/Items/Miscellaneous/CrushedRemains.cs
@@ -1,15 +1,46 @@
 ﻿using Microsoft.Xna.Framework;
 
+using RoA.Content.Items.Equipables.Miscellaneous;
+
 using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace RoA.Content.Items.Miscellaneous;
 
 sealed class CrushedRemains : ModItem {
+    public override void SetStaticDefaults() {
+        ItemID.Sets.ExtractinatorMode[Type] = Type;
+    }
+
     public override void SetDefaults() {
         Item.maxStack = Item.CommonMaxStack;
         int width = 36, height = 24;
         Item.Size = new Vector2(width, height);
-        Item.rare = 1;
+        Item.rare = ItemRarityID.Blue;
+    }
+
+    public override void ExtractinatorUse(int extractinatorBlockType, ref int resultType, ref int resultStack) { // Calls upon use of an extractinator. Below is the chance you will get ExampleOre from the extractinator.
+        float chance = Main.rand.Next(10);
+        if (chance < 3) {
+            resultType = ItemID.Bone;
+            resultStack = Main.rand.Next(1, 6);
+        }
+        else if (chance < 6) {
+            resultType = ItemID.CopperCoin;
+            resultStack = Main.rand.Next(1, 101);
+        }
+        else if (chance < 8) {
+            resultType = ItemID.SilverCoin;
+            resultStack = Main.rand.Next(1, 101);
+        }
+        else if (chance < 10) {
+            resultStack = 1;
+            if (Main.rand.NextBool()) {
+                resultType = ModContent.ItemType<CarcassChestguard>();
+                return;
+            }
+            resultType = ModContent.ItemType<CarcassSandals>();
+        }
     }
 }
