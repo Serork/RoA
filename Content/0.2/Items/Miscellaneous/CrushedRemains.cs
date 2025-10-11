d3407f98efc8493c52ebc5e20a0cef4f9d10d32a
Serork <mandrygin.2013@mail.ru>
2025-10-11 23:34:07 +0300

    crushed remains

---
+++ Content/0.2/Items/Miscellaneous/CrushedRemains.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework;
+
+using Terraria;
+using Terraria.ModLoader;
+
+namespace RoA.Content.Items.Miscellaneous;
+
+sealed class CrushedRemains : ModItem {
+    public override void SetDefaults() {
+        Item.maxStack = Item.CommonMaxStack;
+        int width = 36, height = 24;
+        Item.Size = new Vector2(width, height);
+        Item.rare = 1;
+    }
+}
