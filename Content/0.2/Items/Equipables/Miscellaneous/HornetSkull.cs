71cb4ae1a57c809ec21581ddac878f1f958fb8bb
Serork <mandrygin.2013@mail.ru>
2025-10-12 21:42:45 +0300

    hornet

---
+++ Content/0.2/Items/Equipables/Miscellaneous/HornetSkull.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+
+using RoA.Core.Utility.Vanilla;
+
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace RoA.Content.Items.Equipables.Miscellaneous;
+
+[AutoloadEquip(EquipType.Head)]
+sealed class HornetSkull : ModItem {
+    public override void SetStaticDefaults() {
+        ArmorIDs.Head.Sets.DrawHead[Item.headSlot] = false;
+    }
+
+    public override void SetDefaults() {
+        int width = 22; int height = 28;
+        Item.Size = new Vector2(width, height);
+    }
+
+    public override bool IsArmorSet(Item head, Item body, Item legs) => body.type == ModContent.ItemType<CarcassChestguard>() && legs.type == ModContent.ItemType<CarcassSandals>();
+
+    public override void UpdateArmorSet(Player player) {
+        player.GetCommon().ApplyBoneArmorVisuals = true;
+    }
+}
