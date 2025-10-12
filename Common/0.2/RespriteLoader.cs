cfdefb12ae7e74a3949cf7e45d1d2c12b54af05c
Serork <mandrygin.2013@mail.ru>
2025-10-12 03:10:41 +0300

    Update RespriteLoader.cs

--- Common/0.2/RespriteLoader.cs
+++ Common/0.2/RespriteLoader.cs
@@ -15,7 +15,7 @@ sealed partial class RespriteLoader {
     }
 
     public partial void UnloadV02Resprites() {
-        TextureAssets.Item[ItemID.Daybloom] = ModContent.Request<Texture2D>($"Terraria/Images/Item_{ItemID.Skull}");
+        TextureAssets.Item[ItemID.Skull] = ModContent.Request<Texture2D>($"Terraria/Images/Item_{ItemID.Skull}");
         TextureAssets.ArmorHead[ArmorIDs.Head.Skull] = ModContent.Request<Texture2D>($"Terraria/Images/Armor_Head_{ArmorIDs.Head.Skull}");
     }
 }
