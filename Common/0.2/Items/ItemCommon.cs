f8e91cfffbc4bd161be4f7e6969b22a891ac63f6
Serork <mandrygin.2013@mail.ru>
2025-10-12 21:54:28 +0300

    adjs

--- Common/0.2/Items/ItemCommon.cs
+++ Common/0.2/Items/ItemCommon.cs
@@ -9,34 +9,6 @@ using Terraria.ModLoader;
 
 namespace RoA.Common.Items;
 
-sealed class ItemCommon : GlobalItem {
+sealed partial class ItemCommon : GlobalItem {
     public override bool InstancePerEntity => true;
-
-    public override void SetDefaults(Item entity) {
-        ResetSkullItemDefaults(entity);
-    }
-
-    private void ResetSkullItemDefaults(Item entity) {
-        if (entity.type == ItemID.Skull) {
-            int width = 20, height = 22;
-            entity.Size = new Vector2(width, height);
-            entity.headSlot = 93;
-            entity.rare = ItemRarityID.White;
-            entity.vanity = false;
-        }
-    }
-
-    public override string IsArmorSet(Item head, Item body, Item legs) {
-        if (head.type == ItemID.Skull && body.type == ModContent.ItemType<CarcassChestguard>() && legs.type == ModContent.ItemType<CarcassSandals>()) {
-            return "SkullArmorSet";
-        }
-
-        return base.IsArmorSet(head, body, legs);   
-    }
-
-    public override void ArmorSetShadows(Player player, string set) {
-        if (set == "SkullArmorSet") {
-            player.GetCommon().ApplyBoneArmorVisuals = true;
-        }
-    }
 }
---
+++ Common/0.2/Items/VanillaSkullChanges.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework;
+
+using RoA.Content.Items.Equipables.Miscellaneous;
+using RoA.Core.Utility.Vanilla;
+
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace RoA.Common.Items;
+
+sealed partial class ItemCommon : GlobalItem {
+    public override void SetDefaults(Item entity) {
+        ResetSkullItemDefaults(entity);
+    }
+
+    private void ResetSkullItemDefaults(Item entity) {
+        if (entity.type == ItemID.Skull) {
+            int width = 20, height = 22;
+            entity.Size = new Vector2(width, height);
+            entity.headSlot = 93;
+            entity.rare = ItemRarityID.White;
+            entity.vanity = false;
+        }
+    }
+
+    public override string IsArmorSet(Item head, Item body, Item legs) {
+        if (head.type == ItemID.Skull && body.type == ModContent.ItemType<CarcassChestguard>() && legs.type == ModContent.ItemType<CarcassSandals>()) {
+            return "SkullArmorSet";
+        }
+
+        return base.IsArmorSet(head, body, legs);
+    }
+
+    public override void ArmorSetShadows(Player player, string set) {
+        if (set == "SkullArmorSet") {
+            player.GetCommon().ApplyBoneArmorVisuals = true;
+        }
+    }
+}
