ee81d819b293c5c01b02f9b5254ced534f86d50a
Serork <mandrygin.2013@mail.ru>
2025-10-23 13:35:15 +0300

    Update BackwoodsBackgroundSurface.cs

--- Content/0.1/Backgrounds/BackwoodsBackgroundSurface.cs
+++ Content/0.1/Backgrounds/BackwoodsBackgroundSurface.cs
@@ -214,7 +214,7 @@ sealed class BackwoodsBackgroundSurface : ModSurfaceBackgroundStyle {
             float value2 = Main.GraveyardVisualIntensity * 0.92f;
             bool flag = false;
             if (/*Main.cloudAlpha > 0f || value2 > 0f || */BackwoodsFogHandler.Opacity > 0f && Main.cloudAlpha <= 0f && value2 <= 0f) {
-                backgroundColor *= Math.Max(BackwoodsFogHandler.Opacity * 0.15f, Math.Max(Main.cloudAlpha, value2) * 0.1f);
+                backgroundColor *= Math.Max(BackwoodsFogHandler.Opacity * 0.25f, Math.Max(Main.cloudAlpha, value2) * 0.1f);
                 flag = true;
             }
             if (flag) {
