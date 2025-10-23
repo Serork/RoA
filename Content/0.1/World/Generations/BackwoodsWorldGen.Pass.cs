cc1261e293016e74eaf0067556fe247d4b6ca0ff
Serork <mandrygin.2013@mail.ru>
2025-10-23 07:14:40 +0300

    Update BackwoodsWorldGen.Pass.cs

--- Content/0.1/World/Generations/BackwoodsWorldGen.Pass.cs
+++ Content/0.1/World/Generations/BackwoodsWorldGen.Pass.cs
@@ -4958,7 +4958,7 @@ sealed class BackwoodsBiomePass(string name, double loadWeight) : GenPass(name, 
             while (testJ <= max) {
                 x = cliffX + (randomnessPoints[max - testJ] + 1) * dir;
                 if (testJ < startY + 3) {
-                    for (int j = testJ - 15; j < testJ; j++) {
+                    for (int j = testJ - 25; j < testJ; j++) {
                         if (Main.tile[x, j].TileType != CliffPlaceholderTileType && Main.tile[x, j].TileType != _dirtTileType) {
                             WorldGen.KillTile(x, j);
                         }
