64a883f2c555d040da3cb15191cec21d1ef122e2
Serork <mandrygin.2013@mail.ru>
2025-11-07 22:26:01 +0300

    rod of the bifrost unique use animation

--- Content/0.2/Items/Weapons/Nature/Hardmode/RodOfTheBifrost.cs
+++ Content/0.2/Items/Weapons/Nature/Hardmode/RodOfTheBifrost.cs
@@ -1,15 +1,19 @@
 ﻿using Microsoft.Xna.Framework;
 
 using RoA.Common.Druid;
+using RoA.Common.Players;
 using RoA.Content.Projectiles.Friendly.Nature;
 using RoA.Core.Defaults;
 using RoA.Core.Utility;
+using RoA.Core.Utility.Extensions;
 using RoA.Core.Utility.Vanilla;
 
 using Terraria;
 using Terraria.DataStructures;
 using Terraria.Enums;
 using Terraria.ID;
+
+using static Terraria.Player;
 
 namespace RoA.Content.Items.Weapons.Nature.Hardmode;
 
@@ -21,12 +25,30 @@ sealed class RodOfTheBifrost : NatureItem {
     protected override void SafeSetDefaults() {
         Item.SetSizeValues(28, 40);
         Item.SetWeaponValues(60, 4f);
-        Item.SetUsableValues(ItemUseStyleID.Shoot, 20, useSound: SoundID.Item7);
+        Item.SetUsableValues(ItemUseStyleID.HiddenAnimation, 20, useSound: SoundID.Item7);
         Item.SetShopValues(ItemRarityColor.Yellow8, Item.sellPrice());
         Item.SetShootableValues();
 
         NatureWeaponHandler.SetPotentialDamage(Item, 100);
         NatureWeaponHandler.SetFillingRateModifier(Item, 0.5f);
+    }
+
+    public override void UseStyle(Player player, Rectangle heldItemFrame) {
+        player.itemLocation = player.Center + new Vector2(player.direction * 6f, 4f * player.gravDir);
+        float maxRotation = -0.15f;
+        player.itemRotation = player.DirectionTo(player.Center + new Vector2(player.direction * 100f, -10f)).ToRotation() + Helper.Wave(-maxRotation, maxRotation, 10f, player.whoAmI) * player.direction
+            + MathHelper.Pi * (!player.FacedRight()).ToInt();
+
+        CompositeArmStretchAmount compositeArmStretchAmount2 = CompositeArmStretchAmount.Full;
+        float rotation = player.itemRotation * 0.375f * -player.direction;
+        rotation += MathHelper.PiOver4 * 1f;
+        rotation *= -player.direction;
+        if (player.FacedRight()) {
+            rotation += -0.1f;
+        }
+        rotation += -0.5f * player.direction;
+        player.SetCompositeArmFront(enabled: true, compositeArmStretchAmount2, rotation);
+        player.SetCompositeArmBack(enabled: true, CompositeArmStretchAmount.Full, rotation);
     }
 
     public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback) {
