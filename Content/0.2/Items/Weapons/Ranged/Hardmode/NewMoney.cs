7d44d1e12d0cb0e1702c943adc30a45db0678bc0
Serork <mandrygin.2013@mail.ru>
2025-11-13 19:06:14 +0300

    Update NewMoney.cs

--- Content/0.2/Items/Weapons/Ranged/Hardmode/NewMoney.cs
+++ Content/0.2/Items/Weapons/Ranged/Hardmode/NewMoney.cs
@@ -57,7 +57,7 @@ sealed class NewMoney : ModItem {
         if (type == ProjectileID.Bullet) {
             type = ModContent.ProjectileType<NewMoneyBullet>();
         }
-        position -= velocity.TurnLeft().SafeNormalize() * 7.5f * -player.direction;
+        position -= velocity.TurnLeft().SafeNormalize() * 6.5f * -player.direction;
         Projectile.NewProjectile(source, position.X, position.Y, num50, num51, type, damage, knockback, player.whoAmI);
 
         return false;
