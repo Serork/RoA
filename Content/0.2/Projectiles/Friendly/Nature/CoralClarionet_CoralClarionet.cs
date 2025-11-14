8a27eeb9f7961a0b38ca10891b19b89be9b63fa2
Serork <mandrygin.2013@mail.ru>
2025-11-14 16:40:35 +0300

    Update CoralClarionet_CoralClarionet.cs

--- Content/0.2/Projectiles/Friendly/Nature/CoralClarionet_CoralClarionet.cs
+++ Content/0.2/Projectiles/Friendly/Nature/CoralClarionet_CoralClarionet.cs
@@ -4,12 +4,14 @@ using Microsoft.Xna.Framework.Graphics;
 using ReLogic.Content;
 
 using RoA.Common;
+using RoA.Common.Players;
 using RoA.Common.Projectiles;
 using RoA.Core;
 using RoA.Core.Defaults;
 using RoA.Core.Graphics.Data;
 using RoA.Core.Utility;
 
+using System;
 using System.Collections.Generic;
 
 using Terraria;
@@ -34,7 +36,11 @@ sealed class CoralClarionet : NatureProjectile_NoTextureLoad, IRequestAssets {
 
     public ref float WaveValue => ref Projectile.localAI[0];
     public ref float WaveValue2 => ref Projectile.localAI[1];
-    public ref float SpawnValue => ref Projectile.localAI[2];
+    public ref float DesiredRotation => ref Projectile.localAI[2];
+
+    public ref float SpawnValue => ref Projectile.ai[0];
+
+    public float Opacity => Utils.GetLerpValue(0f, 0.25f, SpawnValue / SPAWNTIMEINTICKS, true);
 
     protected override void SafeSetDefaults() {
         Projectile.SetSizeValues(10);
@@ -53,6 +59,24 @@ sealed class CoralClarionet : NatureProjectile_NoTextureLoad, IRequestAssets {
         WaveValue = Projectile.whoAmI + (float)(Main.timeForVisualEffects / 10 % MathHelper.TwoPi);
 
         SpawnValue = Helper.Approach(SpawnValue, SPAWNTIMEINTICKS, 1f);
+
+        Player owner = Projectile.GetOwnerAsPlayer();
+        owner.SyncMousePosition();
+        Vector2 mousePosition = owner.GetViableMousePosition();
+        ref float rotation = ref Projectile.rotation;
+        float lerpValue = 0.1f;
+        float desiredRotation = Projectile.Center.AngleTo(mousePosition) + MathHelper.PiOver2;
+        float maxRotation = 0.1f;
+        float desiredRotationValue = MathF.Abs(desiredRotation);
+        if (desiredRotationValue > maxRotation) {
+            desiredRotation = Utils.AngleLerp(desiredRotation, maxRotation * desiredRotation.GetDirection(), 0.75f);
+        }
+        float lerpValue2 = lerpValue;
+        if (mousePosition.Y > Projectile.Center.Y) {
+            lerpValue2 *= 0.5f;
+        }
+        DesiredRotation = Utils.AngleLerp(DesiredRotation, desiredRotation, lerpValue2);
+        rotation = Utils.AngleLerp(rotation, DesiredRotation, lerpValue);
     }
 
     protected override void Draw(ref Color lightColor) {
@@ -69,7 +93,7 @@ sealed class CoralClarionet : NatureProjectile_NoTextureLoad, IRequestAssets {
         Vector2 origin = clip.BottomCenter();
         SpriteBatch batch = Main.spriteBatch;
         float spawnProgress = SpawnValue / SPAWNTIMEINTICKS;
-        float opacity = Utils.GetLerpValue(0f, 0.25f, spawnProgress, true);
+        float opacity = Opacity;
         Color color = Color.White * opacity;
         Vector2 scale = Vector2.One * Ease.QuartOut(spawnProgress);
         scale.X *= Utils.Remap(spawnProgress * Ease.CubeOut(spawnProgress), 0f, 1f, 2f, 1f);
@@ -81,18 +105,19 @@ sealed class CoralClarionet : NatureProjectile_NoTextureLoad, IRequestAssets {
             Scale = scale
         };
         float maxRotation = 0.05f;
+        float rotation = Projectile.rotation;
         ShaderLoader.WavyShader.WaveFactor = WaveValue;
         ShaderLoader.WavyShader.StrengthX = 0.15f;
         ShaderLoader.WavyShader.StrengthY = 1.5f;
         ShaderLoader.WavyShader.DrawColor = color;
         ShaderLoader.WavyShader.Apply(batch, () => {
-            drawInfo = drawInfo with { Rotation = Helper.Wave(WaveValue2, -maxRotation, maxRotation, 5f, 0f) };
+            drawInfo = drawInfo with { Rotation = rotation + Helper.Wave(WaveValue2, -maxRotation, maxRotation, 5f, 0f) };
             batch.Draw(part1Texture, position, drawInfo);
-            drawInfo = drawInfo with { Rotation = Helper.Wave(WaveValue2, -maxRotation, maxRotation, 5f, 1f) };
+            drawInfo = drawInfo with { Rotation = rotation + Helper.Wave(WaveValue2, -maxRotation, maxRotation, 5f, 1f) };
             batch.Draw(part2Texture, position, drawInfo);
-            drawInfo = drawInfo with { Rotation = Helper.Wave(WaveValue2, -maxRotation, maxRotation, 5f, 2f) };
+            drawInfo = drawInfo with { Rotation = rotation + Helper.Wave(WaveValue2, -maxRotation, maxRotation, 5f, 2f) };
             batch.Draw(part3Texture, position, drawInfo);
-            drawInfo = drawInfo with { Rotation = Helper.Wave(WaveValue2, -maxRotation, maxRotation, 5f, 3f) };
+            drawInfo = drawInfo with { Rotation = rotation + Helper.Wave(WaveValue2, -maxRotation, maxRotation, 5f, 3f) };
             batch.Draw(part4Texture, position, drawInfo);
         });
     }
