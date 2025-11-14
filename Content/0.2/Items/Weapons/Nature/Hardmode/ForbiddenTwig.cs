c35006364538aa5a9efca1e19364edcc66c9cca2
Serork <mandrygin.2013@mail.ru>
2025-11-14 11:08:16 +0300

    Update ForbiddenTwig.cs

--- Content/0.2/Items/Weapons/Nature/Hardmode/ForbiddenTwig.cs
+++ Content/0.2/Items/Weapons/Nature/Hardmode/ForbiddenTwig.cs
@@ -1,8 +1,13 @@
-﻿using RoA.Common.Druid;
+﻿using Microsoft.Xna.Framework;
+
+using RoA.Common.Druid;
 using RoA.Content.Projectiles.Friendly.Nature;
 using RoA.Core.Defaults;
 
+using System;
+
 using Terraria;
+using Terraria.DataStructures;
 using Terraria.Enums;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -23,5 +28,19 @@ sealed class ForbiddenTwig : NatureItem {
 
         NatureWeaponHandler.SetPotentialDamage(Item, 60);
         NatureWeaponHandler.SetFillingRateModifier(Item, 0.2f);
+    }
+
+    public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback) {
+        Vector2 pointPoisition = player.RotatedRelativePoint(player.MountedCenter);
+        float num2 = (float)Main.mouseX + Main.screenPosition.X - pointPoisition.X;
+        float num3 = (float)Main.mouseY + Main.screenPosition.Y - pointPoisition.Y;
+        Vector2 vector6 = new Vector2(num2, num3);
+        vector6.X = (float)Main.mouseX + Main.screenPosition.X - pointPoisition.X;
+        vector6.Y = (float)Main.mouseY + Main.screenPosition.Y - pointPoisition.Y - 1000f;
+        player.itemRotation = (float)Math.Atan2(vector6.Y * (float)player.direction, vector6.X * (float)player.direction);
+        NetMessage.SendData(13, -1, -1, null, player.whoAmI);
+        NetMessage.SendData(41, -1, -1, null, player.whoAmI);
+
+        return true;
     }
 }
