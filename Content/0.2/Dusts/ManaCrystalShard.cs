74d9838a49653e96625d58df442b17ed94c01cbf
Serork <mandrygin.2013@mail.ru>
2025-11-17 13:43:00 +0300

    adjs

--- Content/0.2/Dusts/ManaCrystalShard.cs
+++ Content/0.2/Dusts/ManaCrystalShard.cs
@@ -8,7 +8,7 @@ using Terraria.ModLoader;
 namespace RoA.Content.Dusts;
 
 sealed class ManaCrystalShard : ModDust {
-    public override Color? GetAlpha(Dust dust, Color lightColor) => base.GetAlpha(dust, lightColor);
+    public override Color? GetAlpha(Dust dust, Color lightColor) => base.GetAlpha(dust, lightColor) * 0.9f;
 
     public override bool Update(Dust dust) {
         dust.BasicDust();
