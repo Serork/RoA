451597c9b460b80fb28fce1dcbd985c7d462e1fa
Serork <mandrygin.2013@mail.ru>
2025-11-17 05:14:47 +0300

    Update HandOfGod_Feather.cs

--- Content/0.2/Projectiles/Friendly/Nature/HandOfGod_Feather.cs
+++ Content/0.2/Projectiles/Friendly/Nature/HandOfGod_Feather.cs
@@ -71,7 +71,7 @@ sealed class GodFeather : NatureProjectile_NoTextureLoad, IRequestAssets {
     }
 
     public override void DrawBehind(int index, List<int> behindNPCsAndTiles, List<int> behindNPCs, List<int> behindProjectiles, List<int> overPlayers, List<int> overWiresUI) {
-        behindNPCs.Add(index);
+        overPlayers.Add(index);
     }
 
     public override bool? CanDamage() => false;
