478a7452fae09f9c84131aaf346127005988946d
Serork <mandrygin.2013@mail.ru>
2025-11-23 03:50:09 +0300

    unhinged potion

---
+++ Content/0.1/Buffs/Unhinged.cs
@@ -0,0 +1,10 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace RoA.Content.Buffs;
+
+sealed class Unhinged : ModBuff {
+    public override void Update(Player player, ref int buffIndex) {
+        player.GetAttackSpeed(DamageClass.Generic) += 0.15f;
+    }
+}
