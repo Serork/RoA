7b7a8e7a0b20c60fb518f3a59eab7bee6628656f
Serork <mandrygin.2013@mail.ru>
2025-12-05 11:20:44 +0300

    woodpecker wings

---
+++ Content/Items/Equipables/Accessories/Hardmode/Woodpecker_WoodpeckerWings.cs
@@ -0,0 +1,28 @@
+﻿using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace RoA.Content.Items.Equipables.Accessories.Hardmode;
+
+[AutoloadEquip(EquipType.Wings)]
+sealed class WoodpeckerWings : ModItem {
+    public override void SetStaticDefaults() {
+        int flyTime3 = 85;
+        float flySpeedOverride5 = 6.75f;
+        ArmorIDs.Wing.Sets.Stats[Item.wingSlot] = new WingStats(flyTime3, flySpeedOverride5);
+    }
+
+    public override void VerticalWingSpeeds(Player player, ref float ascentWhenFalling, ref float ascentWhenRising,
+    ref float maxCanAscendMultiplier, ref float maxAscentMultiplier, ref float constantAscend) {
+        maxAscentMultiplier *= 2f;
+    }
+
+    public override void SetDefaults() {
+        Item.width = 26;
+        Item.height = 30;
+        Item.accessory = true;
+        Item.value = Item.buyPrice(0, 40);
+        Item.rare = ItemRarityID.Pink;
+    }
+}
---
+++ Content/Items/Materials/Woodpecker_WoodpeckerFeather.cs
@@ -0,0 +1,15 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace RoA.Content.Items.Materials;
+
+sealed class WoodpeckerFeather : ModItem {
+    public override void SetDefaults() {
+        Item.maxStack = Item.CommonMaxStack;
+        Item.width = 14;
+        Item.height = 34;
+        Item.value = Item.sellPrice(0, 2, 50);
+        Item.rare = ItemRarityID.Pink;
+    }
+}
