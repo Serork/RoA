fbe81e15e68937730b93ed527d36dbbc6b577fb8
Serork <mandrygin.2013@mail.ru>
2025-12-07 16:02:50 +0300

    backwoods special rubble

---
+++ Content/Tiles/Ambient/BackwoodsSpecial2_Rubble.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+using RoA.Content.Dusts.Backwoods;
+using RoA.Content.Items.Placeable.Solid;
+
+using Terraria;
+using Terraria.Enums;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace RoA.Content.Tiles.Ambient;
+
+sealed class BackwoodsSpecial2_Rubble : BackwoodsRocks0 {
+    public override string Texture => TileLoader.GetTile(ModContent.TileType<BackwoodsSpecial2>()).Texture;
+
+    public override void SetStaticDefaults() {
+        Main.tileFrameImportant[Type] = true;
+        Main.tileNoAttach[Type] = true;
+        Main.tileObsidianKill[Type] = true;
+
+        TileID.Sets.ReplaceTileBreakUp[Type] = true;
+        TileID.Sets.BreakableWhenPlacing[Type] = true;
+
+        TileObjectData.newTile.CopyFrom(TileObjectData.Style2x1);
+        TileObjectData.newTile.DrawYOffset = 2;
+        TileObjectData.newTile.Direction = TileObjectDirection.PlaceLeft;
+        TileObjectData.newTile.StyleHorizontal = true;
+        TileObjectData.newTile.LavaDeath = false;
+        TileObjectData.newAlternate.CopyFrom(TileObjectData.newTile);
+        TileObjectData.newAlternate.Direction = TileObjectDirection.PlaceRight;
+        TileObjectData.addAlternate(1);
+        TileObjectData.addTile(Type);
+
+        DustType = ModContent.DustType<Stone>();
+
+        AddMapEntry(new Color(34, 37, 46));
+        AddMapEntry(new Color(110, 91, 74));
+
+        MineResist = 0.01f;
+
+        FlexibleTileWand.RubblePlacementMedium.AddVariations(ModContent.ItemType<Elderwood>(), Type, 0, 1, 2);
+        RegisterItemDrop(ModContent.ItemType<Elderwood>());
+    }
+
+    public override void SetDrawPositions(int i, int j, ref int width, ref int offsetY, ref int height, ref short tileFrameX, ref short tileFrameY) {
+        height = 20;
+        offsetY -= 4;
+    }
+
+    public override ushort GetMapOption(int i, int j) {
+        var tileFrameX = Main.tile[i, j].TileFrameX;
+        if (tileFrameX < 36)
+            return 0;
+        else
+            return 1;
+    }
+
+    public override void SetSpriteEffects(int i, int j, ref SpriteEffects spriteEffects) { }
+
+    public override bool CreateDust(int i, int j, ref int type) {
+        Tile tile = Main.tile[i, j];
+        short tileFrameX = tile.TileFrameX;
+        if (tileFrameX < 36) {
+            type = ModContent.DustType<Stone>();
+        }
+        else if (tileFrameX < 72) {
+            type = Main.rand.NextBool() ? ModContent.DustType<WoodTrash>() : ModContent.DustType<Stone>();
+        }
+        else {
+            type = ModContent.DustType<WoodTrash>();
+        }
+
+        return true;
+    }
+}
---
+++ Content/Tiles/Ambient/BackwoodsSpecial3_Rubble.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+using ReLogic.Content;
+
+using RoA.Common.Tiles;
+using RoA.Content.Items.Placeable.Solid;
+using RoA.Content.Tiles.Solid.Backwoods;
+
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.Enums;
+using Terraria.GameContent;
+using Terraria.GameContent.Drawing;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace RoA.Content.Tiles.Ambient;
+
+sealed class BackwoodsSpecial3_Rubble : ModTile, TileHooks.IGetTileDrawData {
+    public override string Texture => TileLoader.GetTile(ModContent.TileType<BackwoodsSpecial3>()).Texture;
+
+    public void GetTileDrawData(TileDrawing self, int x, int y, Tile tileCache, ushort typeCache, ref short tileFrameX, ref short tileFrameY, ref int tileWidth, ref int tileHeight, ref int tileTop, ref int halfBrickHeight, ref int addFrX, ref int addFrY, ref SpriteEffects tileSpriteEffect, ref Texture2D glowTexture, ref Rectangle glowSourceRect, ref Color glowColor) {
+        glowTexture = BackwoodsSpecial3.GlowTexture.Value;
+        glowColor = TileDrawingExtra.BackwoodsMossGlowColor;
+        glowSourceRect = new Rectangle(tileFrameX, tileFrameY, tileWidth, tileHeight);
+        addFrY = -2;
+    }
+
+    public override void SetStaticDefaults() {
+        Main.tileFrameImportant[Type] = true;
+        Main.tileNoAttach[Type] = true;
+        Main.tileObsidianKill[Type] = true;
+
+        TileID.Sets.ReplaceTileBreakUp[Type] = true;
+        TileID.Sets.BreakableWhenPlacing[Type] = true;
+
+        TileObjectData.newTile.DrawYOffset = 2;
+        TileObjectData.newTile.Width = 3;
+        TileObjectData.newTile.Height = 2;
+        TileObjectData.newTile.Origin = new Point16(0, 1);
+        TileObjectData.newTile.UsesCustomCanPlace = true;
+        TileObjectData.newTile.CoordinateHeights = [16, 16];
+        TileObjectData.newTile.CoordinateWidth = 16;
+        TileObjectData.newTile.CoordinatePadding = 2;
+        TileObjectData.newTile.Direction = TileObjectDirection.PlaceLeft;
+        TileObjectData.newTile.StyleHorizontal = true;
+        TileObjectData.newTile.LavaDeath = false;
+        TileObjectData.newAlternate.CopyFrom(TileObjectData.newTile);
+        TileObjectData.newAlternate.Direction = TileObjectDirection.PlaceRight;
+        TileObjectData.addAlternate(1);
+        TileObjectData.addTile(Type);
+
+        DustType = ModContent.DustType<Dusts.Backwoods.Stone>();
+
+        AddMapEntry(new Color(178, 178, 137));
+        AddMapEntry(new Color(95, 98, 113));
+        AddMapEntry(new Color(110, 91, 74));
+
+        MineResist = 0.01f;
+
+        FlexibleTileWand.RubblePlacementLarge.AddVariations(ModContent.ItemType<Elderwood>(), Type, 0, 1, 2, 3);
+        RegisterItemDrop(ModContent.ItemType<Elderwood>());
+    }
+
+    public override ushort GetMapOption(int i, int j) {
+        var tileFrameX = Main.tile[i, j].TileFrameX;
+        if (tileFrameX < 54)
+            return 0;
+        else if (tileFrameX < 108) {
+            return 1;
+        }
+        else {
+            return 2;
+        }
+    }
+
+    public override bool CreateDust(int i, int j, ref int type) {
+        short tileFrame = Main.tile[i, j].TileFrameX;
+        if (tileFrame < 54) {
+            type = DustID.Bone;
+        }
+        else if (tileFrame < 108) {
+            type = ModContent.DustType<Dusts.Backwoods.Stone>();
+        }
+        else {
+            type = ModContent.DustType<Dusts.Backwoods.WoodTrash>();
+        }
+
+        return base.CreateDust(i, j, ref type);
+    }
+
+    public override void ModifyLight(int i, int j, ref float r, ref float g, ref float b) {
+        var tileFrameX = Main.tile[i, j].TileFrameX;
+        if (tileFrameX < 54) {
+        }
+        else if (tileFrameX < 108) {
+            BackwoodsGreenMoss.SetupLight(ref r, ref g, ref b);
+        }
+        else {
+
+        }
+    }
+}
