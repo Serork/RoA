798e8537a008f7a7bd93ceebbce9544ac9e0fa7a
Serork <mandrygin.2013@mail.ru>
2025-12-09 14:59:46 +0300

    money cap

---
+++ Content/Items/Equipables/Vanity/MoneyCap.cs
@@ -0,0 +1,23 @@
+﻿using RoA.Core.Defaults;
+
+using Terraria;
+using Terraria.Enums;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace RoA.Content.Items.Equipables.Vanity;
+
+[AutoloadEquip(EquipType.Head)]
+sealed class MoneyCap : ModItem {
+    public override void SetStaticDefaults() {
+        ArmorIDs.Head.Sets.DrawHatHair[Item.headSlot] = false;
+    }
+
+    public override void SetDefaults() {
+        Item.SetSizeValues(22, 16);
+
+        Item.SetShopValues(ItemRarityColor.Blue1, Item.sellPrice(0, 1));
+
+        Item.vanity = true;
+    }
+}
