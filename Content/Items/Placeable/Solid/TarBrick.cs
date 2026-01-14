d382f8ee7163663adce67fe0d3ae16bb47b4f4b5
Serork <mandrygin.2013@mail.ru>
2026-01-14 05:52:02 +0400

    tar brick

---
+++ Content/Items/Placeable/Solid/TarBrick.cs
@@ -0,0 +1,9 @@
+﻿using Terraria.ModLoader;
+
+namespace RoA.Content.Items.Placeable.Solid;
+
+sealed class TarBrick : ModItem {
+    public override void SetDefaults() {
+        Item.DefaultToPlaceableTile((ushort)ModContent.TileType<Tiles.Solid.TarBrick>(), 0);
+    }
+}
---
+++ Content/Tiles/Solid/TarBrick.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+
+using RoA.Core.Utility;
+
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace RoA.Content.Tiles.Solid;
+
+sealed class TarBrick : ModTile {
+    public override void SetStaticDefaults() {
+        TileHelper.Solid(Type, blendAll: false);
+
+        TileID.Sets.BlockMergesWithMergeAllBlock[Type] = true;
+        TileID.Sets.GeneralPlacementTiles[Type] = false;
+
+        DustType = (ushort)ModContent.DustType<Dusts.SolidifiedTar>();
+        HitSound = SoundID.Tink;
+        AddMapEntry(new Color(68, 57, 77));
+    }
+}
