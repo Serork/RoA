0efe847d5a28c074db7ded8e48dd0a4f98aa05e2
Serork <mandrygin.2013@mail.ru>
2026-01-14 05:53:17 +0400

    Update TarBrick.cs

--- Content/Items/Placeable/Solid/TarBrick.cs
+++ Content/Items/Placeable/Solid/TarBrick.cs
@@ -1,9 +1,12 @@
-﻿using Terraria.ModLoader;
+﻿using RoA.Core.Defaults;
+
+using Terraria.ModLoader;
 
 namespace RoA.Content.Items.Placeable.Solid;
 
 sealed class TarBrick : ModItem {
     public override void SetDefaults() {
         Item.DefaultToPlaceableTile((ushort)ModContent.TileType<Tiles.Solid.TarBrick>(), 0);
+        Item.SetSizeValues(16, 16);
     }
 }
