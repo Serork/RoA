fc63726d2f50aaebde7a8c5a92846180f0fa19bd
Serork <mandrygin.2013@mail.ru>
2026-01-23 11:36:33 +0400

    mystery cake

---
+++ Content/Items/Food/MysteryCake.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.Enums;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace RoA.Content.Items.Food;
+
+sealed class MysteryCake : ModItem {
+    public override void SetStaticDefaults() {
+        Main.RegisterItemAnimation(Type, new DrawAnimationVertical(int.MaxValue, 3));
+        ItemID.Sets.FoodParticleColors[Type] = [
+            new Color(230, 218, 211),
+            new Color(200, 189, 182)
+        ];
+        ItemID.Sets.IsFood[Type] = true;
+    }
+
+    public override void SetDefaults() {
+        Item.DefaultToFood(28, 28, BuffID.WellFed3, 43200);
+        Item.SetShopValues(ItemRarityColor.Orange3, Item.buyPrice(0, 1));
+    }
+}
