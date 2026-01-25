d63314ccfa70236b4b1d9b640449ca37bf55f33e
Serork <mandrygin.2013@mail.ru>
2026-01-25 22:09:47 +0400

    Update SmallMoon.cs

--- Content/Buffs/SmallMoon.cs
+++ Content/Buffs/SmallMoon.cs
@@ -162,7 +162,7 @@ sealed class SmallMoonPlayer : ModPlayer {
         if (_lerpColorProgress2 > 1f) {
             _lerpColorProgress2 = 0f;
         }
-        smallMoonColor2 = Color.Lerp(smallMoonColor2, _lerpColor2, 0.25f);
+        smallMoonColor2 = Color.Lerp(smallMoonColor2, _lerpColor2, 0.2f);
         return _lerpColor;
     }
 }
