bd31b7f48b873248f98ae83bdfdfc0bd114f95a3
Serork <mandrygin.2013@mail.ru>
2026-01-25 10:29:58 +0400

    Update SnakeIdol.cs

--- Content/Items/Equipables/Accessories/Hardmode/SnakeIdol.cs
+++ Content/Items/Equipables/Accessories/Hardmode/SnakeIdol.cs
@@ -3,6 +3,7 @@
 using Terraria;
 using Terraria.Enums;
 using Terraria.ModLoader;
+using Terraria.WorldBuilding;
 
 namespace RoA.Content.Items.Equipables.Accessories.Hardmode;
 
@@ -15,5 +16,16 @@ sealed class SnakeIdol : ModItem {
 
     public override void UpdateAccessory(Player player, bool hideVisual) {
         player.GetCommon().IsSnakeIdolEffectActive = true;
+
+        int debuffCount = 0;
+        for (int i = 0; i < Player.MaxBuffs; i++) {
+            if (player.buffTime[i] >= 1 && player.buffType[i] > 0) {
+                if (Main.debuff[player.buffType[i]] || Main.pvpBuff[player.buffType[i]]) {
+                    debuffCount++;
+                }
+            }
+        }
+        int defenseDecrease = 5 * debuffCount;
+        player.statDefense -= defenseDecrease;
     }
 }
