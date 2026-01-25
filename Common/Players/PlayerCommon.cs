ef4d63ff9377ce007fc1b470acec12b50af25691
Serork <mandrygin.2013@mail.ru>
2026-01-25 14:30:14 +0400

    Update PlayerCommon.cs

--- Common/Players/PlayerCommon.cs
+++ Common/Players/PlayerCommon.cs
@@ -136,7 +136,7 @@ sealed partial class PlayerCommon : ModPlayer {
 
     public bool IsObsidianStopwatchTeleportAvailable => IsObsidianStopwatchEffectActive && _obsidianStopwatchTeleportCooldown <= 0;
     public bool IsObsidianStopwatchTeleportAvailable2 => IsObsidianStopwatchEffectActive && (_obsidianStopwatchTeleportCooldown <= OBSIDIANSTOPWATCHBETWEENINTICKS || _obsidianStopwatchTeleportCooldown >= OBSIDIANSTOPWATCHCOOLDOWNINTICKS - OBSIDIANSTOPWATCHBETWEENINTICKS * 2);
-    public float ObsidianStopwatchEffectOpacity => 1f - Utils.GetLerpValue(0, OBSIDIANSTOPWATCHBETWEENINTICKS, _obsidianStopwatchTeleportCooldown, true) * Utils.GetLerpValue(OBSIDIANSTOPWATCHCOOLDOWNINTICKS, OBSIDIANSTOPWATCHCOOLDOWNINTICKS - OBSIDIANSTOPWATCHBETWEENINTICKS * 2, _obsidianStopwatchTeleportCooldown, true);
+    public float ObsidianStopwatchEffectOpacity => 1f - Utils.GetLerpValue(0, OBSIDIANSTOPWATCHBETWEENINTICKS, _obsidianStopwatchTeleportCooldown, true) * Utils.GetLerpValue(OBSIDIANSTOPWATCHCOOLDOWNINTICKS, OBSIDIANSTOPWATCHCOOLDOWNINTICKS - OBSIDIANSTOPWATCHBETWEENINTICKS * 1f, _obsidianStopwatchTeleportCooldown, true);
 
     public void DoBackflip(float time = 0f) {
         if (DoingBackflip) {
