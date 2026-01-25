4b48183fe2b110c894ba9d44663d3f6f85f3f0b0
Serork <mandrygin.2013@mail.ru>
2026-01-26 01:08:53 +0400

    Update GobletOfPain_WorldGen.cs

--- Content/WorldGenerations/GobletOfPain_WorldGen.cs
+++ Content/WorldGenerations/GobletOfPain_WorldGen.cs
@@ -19,38 +19,36 @@ sealed class GobletOfPain_WorldGen : ModSystem {
     public override void ModifyWorldGenTasks(List<GenPass> tasks, ref double totalWeight) {
         tasks.Insert(tasks.FindIndex(task => task.Name == "Micro Biomes"), new PassLegacy("Goblet of Pain", delegate (GenerationProgress progress, GameConfiguration passConfig) {
             var genRand = WorldGen.genRand;
-            for (int i = 0; i < 2; i++) {
-                int count = WorldGenHelper.WorldSize;
-                for (int num436 = 0; num436 < count; num436++) {
-                    double value2 = (double)num436 / (double)(count);
-                    progress.Set(value2);
-                    bool flag24 = false;
-                    int num437 = 0;
-                    while (!flag24) {
-                        int num438 = genRand.Next((int)(Main.maxTilesX * 0.17f), (int)(Main.maxTilesX * 0.83f));
-                        int num439 = genRand.Next(Main.maxTilesY - 250, Main.maxTilesY - 30);
-                        Tile tile = WorldGenHelper.GetTileSafely(num438, num439);
-                        if (tile.HasTile && ((tile.TileType == TileID.Tables && tile.TileFrameX >= 702 && tile.TileFrameX <= 356)
-                        || (tile.TileType == TileID.WorkBenches && tile.TileFrameX >= 504 && tile.TileFrameX <= 540))) {
-                            if (!WorldGenHelper.GetTileSafely(num438, num439 - 1).HasTile) {
-                                bool flag4 = true;
-                                if (!(num436 == 1 ? genRand.NextBool(2) : num436 != 2 || genRand.NextBool(4))) {
-                                    flag4 = false;
-                                }
-                                if (flag4) {
-                                    WorldGen.PlaceTile(num438, num439 - 1, ModContent.TileType<GobletOfPain>());
-                                }
-                                else {
+            int count = WorldGenHelper.WorldSize;
+            for (int num436 = 0; num436 < count; num436++) {
+                double value2 = (double)num436 / (double)(count);
+                progress.Set(value2);
+                bool flag24 = false;
+                int num437 = 0;
+                while (!flag24) {
+                    int num438 = genRand.Next((int)(Main.maxTilesX * 0.17f), (int)(Main.maxTilesX * 0.83f));
+                    int num439 = genRand.Next(Main.maxTilesY - 250, Main.maxTilesY - 30);
+                    Tile tile = WorldGenHelper.GetTileSafely(num438, num439);
+                    if (tile.HasTile && ((tile.TileType == TileID.Tables && tile.TileFrameX >= 702 && tile.TileFrameX <= 356)
+                    || (tile.TileType == TileID.WorkBenches && tile.TileFrameX >= 504 && tile.TileFrameX <= 540))) {
+                        if (!WorldGenHelper.GetTileSafely(num438, num439 - 1).HasTile) {
+                            bool flag4 = true;
+                            if (!(num436 == 1 ? genRand.NextBool(2) : num436 != 2 || genRand.NextBool(4))) {
+                                flag4 = false;
+                            }
+                            if (flag4) {
+                                WorldGen.PlaceTile(num438, num439 - 1, ModContent.TileType<GobletOfPain>());
+                            }
+                            else {
+                                flag24 = true;
+                            }
+                            if (WorldGenHelper.GetTileSafely(num438, num439 - 1).TileType == ModContent.TileType<GobletOfPain>()) {
+                                flag24 = true;
+                            }
+                            else {
+                                num437++;
+                                if (num437 >= 10000)
                                     flag24 = true;
-                                }
-                                if (WorldGenHelper.GetTileSafely(num438, num439 - 1).TileType == ModContent.TileType<GobletOfPain>()) {
-                                    flag24 = true;
-                                }
-                                else {
-                                    num437++;
-                                    if (num437 >= 10000)
-                                        flag24 = true;
-                                }
                             }
                         }
                     }
