f94a88c239f444f493122b61f06d5a1ae64fd81b
Serork <mandrygin.2013@mail.ru>
2026-01-26 17:24:34 +0400

    hornet flowers

---
+++ Content/Tiles/Miscellaneous/HornetFlowers.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Xna.Framework;
+
+using RoA.Content.Items.Equipables.Miscellaneous;
+
+using System.Collections.Generic;
+
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.Enums;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace RoA.Content.Tiles.Miscellaneous;
+
+sealed class HornetFlowers : ModTile {
+    public override void SetStaticDefaults() {
+        Main.tileFrameImportant[Type] = true;
+        Main.tileNoAttach[Type] = true;
+        Main.tileLavaDeath[Type] = true;
+
+        Main.tileSpelunker[Type] = true;
+        Main.tileShine2[Type] = true;
+        Main.tileShine[Type] = 2000;
+
+        TileID.Sets.GeneralPlacementTiles[Type] = false;
+        TileID.Sets.CanBeClearedDuringGeneration[Type] = false;
+        TileID.Sets.FriendlyFairyCanLureTo[Type] = true;
+        Main.tileOreFinderPriority[Type] = 550;
+
+        TileObjectData.newTile.CopyFrom(TileObjectData.Style3x2);
+        TileObjectData.newTile.DrawYOffset = 0;
+        TileObjectData.newTile.Width = 3;
+        TileObjectData.newTile.Height = 2;
+        TileObjectData.newTile.Origin = new Point16(1, 1);
+        TileObjectData.newTile.UsesCustomCanPlace = true;
+        TileObjectData.newTile.CoordinateHeights = [16, 16];
+        TileObjectData.newTile.CoordinateWidth = 16;
+        TileObjectData.newTile.CoordinatePadding = 2;
+        TileObjectData.newTile.Direction = TileObjectDirection.PlaceLeft;
+        TileObjectData.newTile.StyleHorizontal = true;
+        TileObjectData.newTile.LavaDeath = false;
+        TileObjectData.newAlternate.CopyFrom(TileObjectData.newTile);
+        TileObjectData.newAlternate.Direction = TileObjectDirection.PlaceRight;
+        TileObjectData.addAlternate(1);
+        TileObjectData.addTile(Type);
+
+        AddMapEntry(new Color(200, 196, 192), CreateMapEntryName());
+    }
+
+    public override IEnumerable<Item> GetItemDrops(int i, int j) {
+        yield return new Item(ModContent.ItemType<HornetSkull>());
+    }
+
+    public override bool CreateDust(int i, int j, ref int type) {
+        type = Main.rand.NextBool(3) ? DustID.JunglePlants : DustID.JungleGrass;
+
+        return base.CreateDust(i, j, ref type);
+    }
+
+    public override void NumDust(int i, int j, bool fail, ref int num) {
+
+    }
+}
---
+++ Content/WorldGenerations/HornetSkull_WorldGen.cs
@@ -0,0 +1,60 @@
+﻿using RoA.Content.Tiles.Miscellaneous;
+using RoA.Core.Utility;
+
+using System;
+using System.Collections.Generic;
+
+using Terraria;
+using Terraria.GameContent.Generation;
+using Terraria.ID;
+using Terraria.IO;
+using Terraria.ModLoader;
+using Terraria.WorldBuilding;
+
+namespace RoA.Content.WorldGenerations;
+
+sealed class HornetSkull_WorldGen : ModSystem {
+    public override void ModifyWorldGenTasks(List<GenPass> tasks, ref double totalWeight) {
+        tasks.Insert(tasks.FindIndex(task => task.Name == "Jungle Chests") + 1, new PassLegacy("Hornet Flowers", delegate (GenerationProgress progress, GameConfiguration passConfig) {
+            var genRand = WorldGen.genRand;
+            for (int k = 0; k < 2; k++) {
+                int count = WorldGenHelper.WorldSize;
+                for (int num436 = 0; num436 < count; num436++) {
+                    double value2 = (double)num436 / (double)(count);
+                    progress.Set(value2);
+                    bool flag24 = false;
+                    int num437 = 0;
+                    while (!flag24) {
+                        int num438 = genRand.Next((int)(Main.maxTilesX * 0.05f), (int)(Main.maxTilesX * 0.95f));
+                        int num439 = genRand.Next(250, Main.maxTilesY - 30);
+                        Tile tile = WorldGenHelper.GetTileSafely(num438, num439);
+                        if (tile.HasTile && tile.TileType == TileID.JungleGrass) {
+                            if (!WorldGenHelper.GetTileSafely(num438, num439 - 1).HasTile && WorldGenHelper.GetTileSafely(num438, num439 - 1).LiquidAmount <= 0) {
+                                bool flag4 = true;
+                                if (!(num436 == 1 ? genRand.NextBool(2) : num436 != 2 || genRand.NextBool(4))) {
+                                    flag4 = false;
+                                }
+                                if (flag4) {
+                                    WorldGenHelper.Place3x2(num438, num439 - 1, (ushort)ModContent.TileType<HornetFlowers>(), Main.rand.NextBool().ToInt());
+                                    Console.WriteLine($"{num438}, {num439}");
+                                }
+                                else {
+                                    flag24 = true;
+                                }
+                                if (WorldGenHelper.GetTileSafely(num438, num439 - 1).TileType == ModContent.TileType<HornetFlowers>()) {
+                                    flag24 = true;
+                                    Console.WriteLine($"{num438}, {num439}");
+                                }
+                                else {
+                                    num437++;
+                                    if (num437 >= 10000)
+                                        flag24 = true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }));
+    }
+}
