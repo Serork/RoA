9e68f8e610479d5ececb41d74142f2c6cad801ba
Serork <mandrygin.2013@mail.ru>
2026-01-27 14:08:05 +0400

    hazelnut mask

---
+++ Content/Items/Equipables/Vanity/HazelnutMask.cs
@@ -0,0 +1,23 @@
+﻿using RoA.Core.Defaults;
+
+using Terraria;
+using Terraria.Enums;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace RoA.Content.Items.Equipables.Vanity;
+
+[AutoloadEquip(EquipType.Head)]
+sealed class HazelnutMask : ModItem {
+    public override void SetStaticDefaults() {
+        ArmorIDs.Head.Sets.DrawHead[Item.headSlot] = false;
+    }
+
+    public override void SetDefaults() {
+        Item.SetSizeValues(24, 24);
+
+        Item.SetShopValues(ItemRarityColor.Blue1, Item.sellPrice(0, 1));
+
+        Item.vanity = true;
+    }
+}
