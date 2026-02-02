63b94d97ceb96223adc3cd39d7d75f525d7c7caa
Serork <mandrygin.2013@mail.ru>
2026-02-03 01:19:55 +0400

    lava huge damage

--- Content/Items/Consumables/DarkNeoplasm.cs
+++ Content/Items/Consumables/DarkNeoplasm.cs
@@ -1,6 +1,14 @@
-﻿using RoA.Common.Items;
+﻿using Microsoft.Xna.Framework;
+
+using RoA.Common.Items;
+using RoA.Content.Projectiles.LiquidsSpecific;
+using RoA.Core.Utility;
+
+using System;
 
 using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace RoA.Content.Items.Consumables;
@@ -12,6 +20,30 @@ class DarkNeoplasm : ModItem {
         Item.height = 30;
         Item.maxStack = Item.CommonMaxStack;
         Item.rare = 2;
+    }
+
+    public override void PostUpdate() {
+        if (Item.playerIndexTheItemIsReservedFor == Main.myPlayer) {
+            // TODO: net test
+            if (Helper.SinglePlayerOrServer) {
+                int x = (int)Item.Bottom.X / 16, y = (int)Item.Bottom.Y / 16;
+                Tile tile = Main.tile[x - 1, y];
+                Tile tile2 = Main.tile[x + 1, y];
+                Tile tile3 = Main.tile[x, y - 1];
+                Tile tile4 = Main.tile[x, y + 1];
+                Tile tile5 = Main.tile[x, y];
+                tile.LiquidAmount = tile2.LiquidAmount = tile3.LiquidAmount = 0;
+                Projectile.NewProjectile(null, new Point16(x, y).ToWorldCoordinates(), Vector2.Zero, ModContent.ProjectileType<TarExplosion>(), 250, 0f, Main.myPlayer, ai2: 1f);
+                WorldGen.SquareTileFrame(x, y);
+            }
+
+            Item.active = false;
+            Item.type = 0;
+            Item.stack = 0;
+            if (Main.netMode != NetmodeID.SinglePlayer) {
+                NetMessage.SendData(MessageID.SyncItem, -1, -1, null, Item.whoAmI);
+            }
+        }
     }
 
     public virtual ItemCommon.TarEnchantmentStat GetAppliedEnchantment() {
--- Content/Items/Consumables/PerfectClot.cs
+++ Content/Items/Consumables/PerfectClot.cs
@@ -1,8 +1,13 @@
-﻿using RoA.Core.Utility.Vanilla;
+﻿using Microsoft.Xna.Framework;
+
+using RoA.Content.Projectiles.LiquidsSpecific;
+using RoA.Core.Utility;
+using RoA.Core.Utility.Vanilla;
 
 using System;
 
 using Terraria;
+using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -29,6 +34,28 @@ sealed class PerfectClot : ModItem {
     public override void Update(ref float gravity, ref float maxFallSpeed) {
         Item.velocity.Y = MathF.Abs(Item.velocity.Y) * -1f;
         Item.velocity.X *= 0.925f;
+
+        if (Item.playerIndexTheItemIsReservedFor == Main.myPlayer) {
+            // TODO: net test
+            if (Helper.SinglePlayerOrServer) {
+                int x = (int)Item.Bottom.X / 16, y = (int)Item.Bottom.Y / 16;
+                Tile tile = Main.tile[x - 1, y];
+                Tile tile2 = Main.tile[x + 1, y];
+                Tile tile3 = Main.tile[x, y - 1];
+                Tile tile4 = Main.tile[x, y + 1];
+                Tile tile5 = Main.tile[x, y];
+                tile.LiquidAmount = tile2.LiquidAmount = tile3.LiquidAmount = 0;
+                Projectile.NewProjectile(null, new Point16(x, y).ToWorldCoordinates(), Vector2.Zero, ModContent.ProjectileType<TarExplosion>(), 500, 0f, Main.myPlayer, ai2: 2f);
+                WorldGen.SquareTileFrame(x, y);
+            }
+
+            Item.active = false;
+            Item.type = 0;
+            Item.stack = 0;
+            if (Main.netMode != NetmodeID.SinglePlayer) {
+                NetMessage.SendData(MessageID.SyncItem, -1, -1, null, Item.whoAmI);
+            }
+        }
     }
 
     public override bool? UseItem(Player player) {
