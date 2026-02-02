1f785056ae9a07722a3ccfff6006cf7169cdcda1
Serork <mandrygin.2013@mail.ru>
2026-02-02 18:50:11 +0400

    band of purity nerf

--- Content/Items/Equipables/Accessories/BandOfPurity.cs
+++ Content/Items/Equipables/Accessories/BandOfPurity.cs
@@ -30,9 +30,9 @@ sealed class BandOfPurity : NatureItem {
     }
 
     public override void UpdateAccessory(Player player, bool hideVisual) {
-        player.GetModPlayer<DruidStats>().WreathChargeRateMultiplier += 0.2f;
+        player.GetModPlayer<DruidStats>().WreathChargeRateMultiplier += 0.15f;
         if (player.GetWreathHandler().IsFull1) {
-            player.lifeRegen += 10;
+            player.lifeRegen += 3;
         }
     }
 }
