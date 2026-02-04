4c6249925078a327c1345a22d92af3d3ff387bed
Serork <mandrygin.2013@mail.ru>
2026-02-05 01:59:24 +0400

    nature priest cape

---
+++ Content/Items/Equipables/Accessories/Hardmode/NaturePriestCape.cs
@@ -0,0 +1,18 @@
+﻿using Terraria;
+using Terraria.Enums;
+using Terraria.ModLoader;
+
+namespace RoA.Content.Items.Equipables.Accessories.Hardmode;
+
+[AutoloadEquip(EquipType.Back, EquipType.Front)]
+sealed class NaturePriestCape : ModItem {
+    public override void SetDefaults() {
+        Item.DefaultToAccessory(30, 30);
+
+        Item.SetShopValues(ItemRarityColor.LightRed4, Item.sellPrice(0, 1));
+    }
+
+    public override void UpdateAccessory(Player player, bool hideVisual) {
+
+    }
+}
