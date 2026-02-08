600c65b4de4aba4374fd6ca3107d14f3db0f273b
Serork <mandrygin.2013@mail.ru>
2026-02-09 02:41:55 +0400

    Update Aries.cs

--- Content/Projectiles/Friendly/Pets/Aries.cs
+++ Content/Projectiles/Friendly/Pets/Aries.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 
+using RoA.Core.Utility;
 using RoA.Core.Utility.Extensions;
 using RoA.Core.Utility.Vanilla;
 
@@ -12,13 +13,25 @@ using Terraria.ModLoader;
 namespace RoA.Content.Projectiles.Friendly.Pets;
 
 sealed class Aries : ModProjectile {
+    public int Frame;
+    public int FrameCounter;
+
     public override void SetStaticDefaults() {
         Main.projPet[Type] = true;
 
         Projectile.SetFrameCount(4);
+
+        ProjectileID.Sets.CharacterPreviewAnimations[Projectile.type] = ProjectileID.Sets.SimpleLoop(2, Main.projFrames[Projectile.type], 6)
+            .WithOffset(0, 0)
+            .WithSpriteDirection(-1)
+            .WithCode(DelegateMethods.CharacterPreview.Float);
     }
 
     public override void SetDefaults() {
+        Projectile.CloneDefaults(ProjectileID.JunimoPet); // Copy the stats of the Zephyr Fish
+
+        AIType = ProjectileID.JunimoPet; // Mimic as the Zephyr Fish during AI.
+
         Projectile.netImportant = true;
         Projectile.width = 46;
         Projectile.height = 44;
@@ -32,6 +45,8 @@ sealed class Aries : ModProjectile {
 
     public override bool PreAI() {
         int owner = Projectile.owner;
+        Main.player[owner].petFlagJunimoPet = false; // Relic from AIType
+
         if (Main.player[owner].dead)
             Main.player[owner].GetCommon().IsAriesActive = false;
 
@@ -42,45 +57,50 @@ sealed class Aries : ModProjectile {
     }
 
     public override bool PreDraw(ref Color lightColor) {
-        Projectile.QuickDrawAnimated(lightColor * Projectile.Opacity);
+        Projectile.QuickDrawAnimated(lightColor * Projectile.Opacity, spriteEffects: (-Projectile.spriteDirection).ToSpriteEffects());
 
         return false;
     }
 
+    public override void PostAI() {
+    }
+
     public override void AI() {
-        if (Projectile.velocity.Y == 0f) {
+        int walkFrameCounter = 20;
+        int flyFrameCoutner = 8;
+
+        Projectile.frameCounter = 0;
+        if (Projectile.velocity.Y == 0.4f) {
             if ((double)Projectile.velocity.X < -0.1 || (double)Projectile.velocity.X > 0.1) {
-                Projectile.frameCounter += (int)Math.Abs(Projectile.velocity.X);
-                Projectile.frameCounter++;
-                if (Projectile.frameCounter > 6) {
-                    Projectile.frame++;
-                    Projectile.frameCounter = 0;
+                FrameCounter += (int)Math.Abs(Projectile.velocity.X);
+                FrameCounter++;
+                if (FrameCounter > walkFrameCounter) {
+                    Frame++;
+                    FrameCounter = 0;
                 }
 
-                if (Projectile.frame >= 2)
-                    Projectile.frame = 0;
+                if (Frame >= 2)
+                    Frame = 0;
             }
             else {
-                Projectile.frame = 0;
-                Projectile.frameCounter = 0;
+                Frame = 0;
+                FrameCounter = 0;
             }
         }
         else {
-            Projectile.frameCounter++;
-            if (Projectile.frameCounter > 6) {
-                Projectile.frame++;
-                Projectile.frameCounter = 0;
+            FrameCounter++;
+            if (FrameCounter > flyFrameCoutner) {
+                Frame++;
+                FrameCounter = 0;
             }
 
-            if (Projectile.frame > 3)
-                Projectile.frame = 2;
+            if (Frame > 3)
+                Frame = 2;
 
-            if (Projectile.frame < 2)
-                Projectile.frame = 2;
+            if (Frame < 2)
+                Frame = 2;
         }
 
-        Projectile.velocity.Y += 0.4f;
-        if (Projectile.velocity.Y > 14f)
-            Projectile.velocity.Y = 14f;
+        Projectile.frame = Frame;
     }
 }
