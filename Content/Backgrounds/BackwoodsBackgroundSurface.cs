1933804ec33077183d257713e114f29fc0199653
Serork <mandrygin.2013@mail.ru>
2026-02-13 14:32:56 +0400

    Update BackwoodsBackgroundSurface.cs

--- Content/Backgrounds/BackwoodsBackgroundSurface.cs
+++ Content/Backgrounds/BackwoodsBackgroundSurface.cs
@@ -91,21 +91,6 @@ sealed class BackwoodsBackgroundSurface : ModSurfaceBackgroundStyle {
             }
         }
         UpdateThem();
-
-        for (int i = 0; i < fades.Length; i++) {
-            if (i == Slot) {
-                fades[i] += transitionSpeed;
-                if (fades[i] > 1f) {
-                    fades[i] = 1f;
-                }
-            }
-            else {
-                fades[i] -= transitionSpeed;
-                if (fades[i] < 0f) {
-                    fades[i] = 0f;
-                }
-            }
-        }
     }
 
     public override int ChooseCloseTexture(ref float scale, ref double parallax, ref float a, ref float b) => -1;
