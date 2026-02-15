44a122ef4136fb7a15782097ac5894571e4a809d
Serork <mandrygin.2013@mail.ru>
2026-02-16 02:48:30 +0400

    Update FilamentSky.cs

--- Common/World/FilamentSky.cs
+++ Common/World/FilamentSky.cs
@@ -46,8 +46,8 @@ sealed class FilamentSky : CustomSky {
 
     public override Color OnTileColor(Color inColor) => new Color(Vector4.Lerp(inColor.ToVector4(), Vector4.One, _fadeOpacity * 0.5f));
 
-    public static Color FilterColor => new(212, 213, 34);
-    public static float FilterOpacity => 0.5f;
+    public static Color FilterColor => new(160, 148, 57);
+    public static float FilterOpacity => 0.6f;
 
     public static Color BeforePlanetGradientColor => new Color(251, 232, 193);
     public static Color PlanetColor => Color.White;
