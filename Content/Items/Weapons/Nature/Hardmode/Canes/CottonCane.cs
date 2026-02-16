ba4241526448609b28411aba5eff891b0f72cf7a
Serork <mandrygin.2013@mail.ru>
2026-02-16 07:26:06 +0400

    cotton cane (1)

---
+++ Content/Items/Weapons/Nature/Hardmode/Canes/CottonCane.cs
@@ -0,0 +1,30 @@
+﻿using RoA.Common.Druid;
+using RoA.Content.Projectiles.Friendly.Nature;
+using RoA.Core.Defaults;
+
+using Terraria;
+using Terraria.Enums;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace RoA.Content.Items.Weapons.Nature.Hardmode.Canes;
+
+sealed class CottonCane : CaneBaseItem<CottonCane.CottonCaneBase> {
+    protected override void SafeSetDefaults() {
+        Item.SetSizeValues(38, 38);
+        Item.SetWeaponValues(60, 4f);
+        Item.SetUsableValues(ItemUseStyleID.None, 30, useSound: SoundID.Item7);
+        Item.SetShopValues(ItemRarityColor.LightRed4, Item.sellPrice());
+
+        NatureWeaponHandler.SetPotentialDamage(Item, 100);
+        NatureWeaponHandler.SetFillingRateModifier(Item, 0.5f);
+
+        Item.autoReuse = true;
+    }
+
+    protected override ushort ProjectileTypeToCreate() => (ushort)ModContent.ProjectileType<CottonBoll>();
+
+    public sealed class CottonCaneBase : CaneBaseProjectile {
+        protected override bool ShouldWaitUntilProjDespawn() => false;
+    }
+}
---
+++ Content/Projectiles/Friendly/Nature/CottonCane_Boll.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework;
+
+using RoA.Core.Defaults;
+using RoA.Core.Utility;
+using RoA.Core.Utility.Vanilla;
+
+namespace RoA.Content.Projectiles.Friendly.Nature;
+
+sealed class CottonBoll : NatureProjectile {
+    private static ushort TIMELEFT => MathUtils.SecondsToFrames(15);
+
+    protected override void SafeSetDefaults() {
+        Projectile.SetSizeValues(10);
+        Projectile.aiStyle = -1;
+        Projectile.friendly = true;
+        Projectile.timeLeft = TIMELEFT;
+        Projectile.penetrate = -1;
+        Projectile.hide = true;
+
+        Projectile.tileCollide = false;
+
+        Projectile.netImportant = true;
+
+        Projectile.manualDirectionChange = true;
+    }
+
+    public override bool? CanDamage() => false;
+    public override bool? CanCutTiles() => false;
+
+    public override bool PreDraw(ref Color lightColor) {
+        Projectile.QuickDraw(lightColor);
+
+        return false;
+    }
+}
