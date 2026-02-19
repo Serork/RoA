f268c53f5da2d3d27750b8deac6fed1519be62e6
Serork <mandrygin.2013@mail.ru>
2026-02-19 09:51:14 +0400

    Update BrambleMaze.cs

--- Content/Items/Weapons/Nature/PreHardmode/BrambleMaze.cs
+++ Content/Items/Weapons/Nature/PreHardmode/BrambleMaze.cs
@@ -11,13 +11,17 @@ using Terraria.ModLoader;
 namespace RoA.Content.Items.Weapons.Nature.PreHardmode;
 
 sealed class BrambleMaze : NatureItem {
+    public override void SetStaticDefaults() {
+        Item.staff[Type] = true;
+    }
+
     protected override void SafeSetDefaults() {
         int width = 42; int height = 48;
         Item.Size = new Vector2(width, height);
 
         Item.useStyle = ItemUseStyleID.Shoot;
         Item.useTime = Item.useAnimation = 20;
-        Item.autoReuse = true;
+        Item.autoReuse = false;
 
         Item.noMelee = true;
         Item.knockBack = 2f;
@@ -36,7 +40,6 @@ sealed class BrambleMaze : NatureItem {
     }
 
     public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback) {
-        velocity = Vector2.Zero;
         position = player.GetPlayerCorePoint();
     }
 }
