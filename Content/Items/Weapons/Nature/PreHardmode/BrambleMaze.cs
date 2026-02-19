db12afbac235046d78795f9433d2a0152ef36a8f
Serork <mandrygin.2013@mail.ru>
2026-02-19 09:37:21 +0400

    bramble maze (1)

--- Content/Items/Weapons/Nature/PreHardmode/BrambleMaze.cs
+++ Content/Items/Weapons/Nature/PreHardmode/BrambleMaze.cs
@@ -1,4 +1,42 @@
-﻿namespace RoA.Content.Items.Weapons.Nature.PreHardmode;
+﻿using Microsoft.Xna.Framework;
+
+using RoA.Common.Druid;
+using RoA.Content.Projectiles.Friendly.Nature;
+using RoA.Core.Utility.Extensions;
+
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace RoA.Content.Items.Weapons.Nature.PreHardmode;
 
 sealed class BrambleMaze : NatureItem {
+    protected override void SafeSetDefaults() {
+        int width = 42; int height = 48;
+        Item.Size = new Vector2(width, height);
+
+        Item.useStyle = ItemUseStyleID.Shoot;
+        Item.useTime = Item.useAnimation = 20;
+        Item.autoReuse = true;
+
+        Item.noMelee = true;
+        Item.knockBack = 2f;
+
+        Item.damage = 6;
+        NatureWeaponHandler.SetPotentialDamage(Item, 16);
+        NatureWeaponHandler.SetFillingRateModifier(Item, 0.25f);
+
+        Item.rare = ItemRarityID.Blue;
+        Item.UseSound = SoundID.Item20;
+
+        Item.shootSpeed = 1f;
+        Item.shoot = ModContent.ProjectileType<BrambleMazeRoot>();
+
+        Item.value = Item.sellPrice(silver: 20);
+    }
+
+    public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback) {
+        velocity = Vector2.Zero;
+        position = player.GetPlayerCorePoint();
+    }
 }
---
+++ Content/Projectiles/Friendly/Nature/BrambleMaze_Root.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Xna.Framework;
+
+using RoA.Core.Defaults;
+using RoA.Core.Utility;
+using RoA.Core.Utility.Extensions;
+using RoA.Core.Utility.Vanilla;
+
+using Terraria;
+
+namespace RoA.Content.Projectiles.Friendly.Nature;
+
+sealed class BrambleMazeRoot : NatureProjectile {
+    public override void SetStaticDefaults() {
+        Projectile.SetFrameCount(4);
+    }
+
+    protected override void SafeSetDefaults() {
+        Projectile.SetSizeValues(24, 16);
+
+        Projectile.friendly = true;
+        Projectile.penetrate = 1;
+        Projectile.tileCollide = false;
+    }
+
+    public override bool ShouldUpdatePosition() => false;
+
+    public override void AI() {
+        if (Projectile.localAI[1] == 0f) {
+            Projectile.localAI[1] = 1f;
+
+            Projectile.frame = Main.rand.Next(4);
+        }
+
+        if (Projectile.IsOwnerLocal() && Projectile.localAI[0] == 0f && Projectile.ai[0] < 10f) {
+            Projectile.localAI[0] = 1f;
+
+            Projectile.ai[0]++;
+
+            Vector2 position = Projectile.Center + Vector2.UnitX * Projectile.width;
+            int damage = Projectile.damage;
+            float knockBack = Projectile.knockBack;
+            ProjectileUtils.SpawnPlayerOwnedProjectile<BrambleMazeRoot>(new ProjectileUtils.SpawnProjectileArgs(Projectile.GetOwnerAsPlayer(), Projectile.GetSource_FromThis()) {
+                Position = position,
+                Damage = damage,
+                KnockBack = knockBack,
+                AI0 = Projectile.ai[0]
+            });
+        }
+    }
+
+    public override bool PreDraw(ref Color lightColor) {
+        Projectile.QuickDrawAnimated(lightColor);
+
+        return false;
+    }
+
+    public override void OnKill(int timeLeft) {
+        
+    }
+}
