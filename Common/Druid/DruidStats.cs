17d8a64f851fb0e8aa2fadc06eb0ec3d7acd0bec
Serork <mandrygin.2013@mail.ru>
2026-02-21 20:25:08 +0400

    Update DruidStats.cs

--- Common/Druid/DruidStats.cs
+++ Common/Druid/DruidStats.cs
@@ -163,7 +163,7 @@ sealed partial class DruidStats : ModPlayer {
 
         if (Player.whoAmI == Main.myPlayer) {
             int cooldownCounter = info.CooldownCounter;
-            if (Player.starCloakItem == null && IsStarfruitCharmEffectActive /*&& (cooldownCounter == -1 || cooldownCounter == 1)*/) {
+            if (Player.starCloakItem == null && IsStarfruitCharmEffectActive && (cooldownCounter == -1 || cooldownCounter == 1)) {
                 for (int num15 = 0; num15 < 3; num15++) {
                     float x = Player.position.X + (float)Main.rand.Next(-400, 400);
                     float y = Player.position.Y - (float)Main.rand.Next(500, 800);
