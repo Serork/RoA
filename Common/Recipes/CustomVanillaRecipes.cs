40879ece6adabd2c4bdcd2d9c91a0fae6e52e752
Serork <mandrygin.2013@mail.ru>
2026-02-23 14:53:30 +0400

    Update CustomVanillaRecipes.cs

--- Common/Recipes/CustomVanillaRecipes.cs
+++ Common/Recipes/CustomVanillaRecipes.cs
@@ -17,6 +17,9 @@ sealed class CustomVanillaRecipes : ModSystem {
         if (ModContent.GetInstance<RoAServerConfig>().ChangeLunarPillarLogic) {
             NewLunarRecipes();
         }
+        else {
+            RemadeLunarRecipes();
+        }
 
         if (!ModContent.GetInstance<RoAServerConfig>().ChangeVanillaRecipes) {
             return;
@@ -46,6 +49,248 @@ sealed class CustomVanillaRecipes : ModSystem {
         // other herb related items
         RecipeHelper.NewRecipe(ItemID.GarlandHat, [ModContent.ItemType<MiracleMint>()], [1], false);
         RecipeHelper.NewRecipe(ItemID.GenderChangePotion, [ModContent.ItemType<MiracleMint>()], [1], false);
+    }
+
+    private void RemadeLunarRecipes() {
+        // celestial sigil
+        {
+            var celestialSigil = Main.recipe.First(x => x.HasResult(ItemID.CelestialSigil));
+            celestialSigil.DisableRecipe();
+
+            Recipe celestialSigilNewRecipe = Recipe.Create(ItemID.CelestialSigil, 1);
+            celestialSigilNewRecipe
+                .AddIngredient(ItemID.FragmentSolar, 10)
+                .AddIngredient(ItemID.FragmentVortex, 10)
+                .AddIngredient(ItemID.FragmentNebula, 10)
+                .AddIngredient(ItemID.FragmentStardust, 10)
+                .AddTile(TileID.LunarCraftingStation)
+                .Register();
+
+            celestialSigilNewRecipe = Recipe.Create(ItemID.CelestialSigil, 1);
+            celestialSigilNewRecipe
+                .AddIngredient(ItemID.FragmentVortex, 10)
+                .AddIngredient(ItemID.FragmentNebula, 10)
+                .AddIngredient(ItemID.FragmentStardust, 10)
+                .AddIngredient(ModContent.ItemType<FilamentFragment>(), 10)
+                .AddTile(TileID.LunarCraftingStation)
+                .Register();
+
+            celestialSigilNewRecipe = Recipe.Create(ItemID.CelestialSigil, 1);
+            celestialSigilNewRecipe
+                .AddIngredient(ItemID.FragmentSolar, 10)
+                .AddIngredient(ItemID.FragmentNebula, 10)
+                .AddIngredient(ItemID.FragmentStardust, 10)
+                .AddIngredient(ModContent.ItemType<FilamentFragment>(), 10)
+                .AddTile(TileID.LunarCraftingStation)
+                .Register();
+
+            celestialSigilNewRecipe = Recipe.Create(ItemID.CelestialSigil, 1);
+            celestialSigilNewRecipe
+                .AddIngredient(ItemID.FragmentSolar, 10)
+                .AddIngredient(ItemID.FragmentVortex, 10)
+                .AddIngredient(ItemID.FragmentStardust, 10)
+                .AddIngredient(ModContent.ItemType<FilamentFragment>(), 10)
+                .AddTile(TileID.LunarCraftingStation)
+                .Register();
+
+            celestialSigilNewRecipe = Recipe.Create(ItemID.CelestialSigil, 1);
+            celestialSigilNewRecipe
+                .AddIngredient(ItemID.FragmentSolar, 10)
+                .AddIngredient(ItemID.FragmentVortex, 10)
+                .AddIngredient(ItemID.FragmentNebula, 10)
+                .AddIngredient(ModContent.ItemType<FilamentFragment>(), 10)
+                .AddTile(TileID.LunarCraftingStation)
+                .Register();
+        }
+        // lunar hook
+        {
+            var lunakHook = Main.recipe.First(x => x.HasResult(ItemID.LunarHook));
+            lunakHook.DisableRecipe();
+            Recipe lunarHookNewRecipe = Recipe.Create(ItemID.LunarHook, 1);
+            lunarHookNewRecipe
+                .AddIngredient(ItemID.FragmentSolar, 5)
+                .AddIngredient(ItemID.FragmentVortex, 5)
+                .AddIngredient(ItemID.FragmentNebula, 5)
+                .AddIngredient(ItemID.FragmentStardust, 5)
+                .AddTile(TileID.LunarCraftingStation)
+                .Register();
+
+            lunarHookNewRecipe = Recipe.Create(ItemID.LunarHook, 1);
+            lunarHookNewRecipe
+                .AddIngredient(ItemID.FragmentVortex, 5)
+                .AddIngredient(ItemID.FragmentNebula, 5)
+                .AddIngredient(ItemID.FragmentStardust, 5)
+                .AddIngredient(ModContent.ItemType<FilamentFragment>(), 5)
+                .AddTile(TileID.LunarCraftingStation)
+                .Register();
+
+            lunarHookNewRecipe = Recipe.Create(ItemID.LunarHook, 1);
+            lunarHookNewRecipe
+                .AddIngredient(ItemID.FragmentSolar, 5)
+                .AddIngredient(ItemID.FragmentNebula, 5)
+                .AddIngredient(ItemID.FragmentStardust, 5)
+                .AddIngredient(ModContent.ItemType<FilamentFragment>(), 5)
+                .AddTile(TileID.LunarCraftingStation)
+                .Register();
+
+            lunarHookNewRecipe = Recipe.Create(ItemID.LunarHook, 1);
+            lunarHookNewRecipe
+                .AddIngredient(ItemID.FragmentSolar, 5)
+                .AddIngredient(ItemID.FragmentVortex, 5)
+                .AddIngredient(ItemID.FragmentStardust, 5)
+                .AddIngredient(ModContent.ItemType<FilamentFragment>(), 5)
+                .AddTile(TileID.LunarCraftingStation)
+                .Register();
+
+            lunarHookNewRecipe = Recipe.Create(ItemID.LunarHook, 1);
+            lunarHookNewRecipe
+                .AddIngredient(ItemID.FragmentSolar, 5)
+                .AddIngredient(ItemID.FragmentVortex, 5)
+                .AddIngredient(ItemID.FragmentNebula, 5)
+                .AddIngredient(ModContent.ItemType<FilamentFragment>(), 5)
+                .AddTile(TileID.LunarCraftingStation)
+                .Register();
+        }
+        // fragments
+        {
+            var nebulaFragment = Main.recipe.First(x => x.HasResult(ItemID.FragmentNebula));
+            nebulaFragment.DisableRecipe();
+            Recipe nebulaFragmentNewRecipe = Recipe.Create(ItemID.FragmentNebula, 1);
+            nebulaFragmentNewRecipe
+                .AddIngredient(ItemID.FragmentSolar, 1)
+                .AddIngredient(ItemID.FragmentVortex, 1)
+                .AddIngredient(ItemID.FragmentStardust, 1)
+                .AddTile(TileID.LunarCraftingStation)
+                .Register();
+
+            nebulaFragmentNewRecipe = Recipe.Create(ItemID.FragmentNebula, 1);
+            nebulaFragmentNewRecipe
+                .AddIngredient(ItemID.FragmentVortex, 1)
+                .AddIngredient(ItemID.FragmentStardust, 1)
+                .AddIngredient(ModContent.ItemType<FilamentFragment>(), 1)
+                .AddTile(TileID.LunarCraftingStation)
+                .Register();
+
+            nebulaFragmentNewRecipe = Recipe.Create(ItemID.FragmentNebula, 1);
+            nebulaFragmentNewRecipe
+                .AddIngredient(ItemID.FragmentSolar, 1)
+                .AddIngredient(ItemID.FragmentStardust, 1)
+                .AddIngredient(ModContent.ItemType<FilamentFragment>(), 1)
+                .AddTile(TileID.LunarCraftingStation)
+                .Register();
+
+            nebulaFragmentNewRecipe = Recipe.Create(ItemID.FragmentNebula, 1);
+            nebulaFragmentNewRecipe
+                .AddIngredient(ItemID.FragmentSolar, 1)
+                .AddIngredient(ItemID.FragmentVortex, 1)
+                .AddIngredient(ModContent.ItemType<FilamentFragment>(), 1)
+                .AddTile(TileID.LunarCraftingStation)
+                .Register();
+
+
+            var solarFragment = Main.recipe.First(x => x.HasResult(ItemID.FragmentSolar));
+            solarFragment.DisableRecipe();
+            Recipe solarFragmentNewRecipe = Recipe.Create(ItemID.FragmentSolar, 1);
+            solarFragmentNewRecipe
+                .AddIngredient(ItemID.FragmentVortex, 1)
+                .AddIngredient(ItemID.FragmentNebula, 1)
+                .AddIngredient(ItemID.FragmentStardust, 1)
+                .AddTile(TileID.LunarCraftingStation)
+                .Register();
+
+            solarFragmentNewRecipe = Recipe.Create(ItemID.FragmentSolar, 1);
+            solarFragmentNewRecipe
+                .AddIngredient(ItemID.FragmentNebula, 1)
+                .AddIngredient(ItemID.FragmentStardust, 1)
+                .AddIngredient(ModContent.ItemType<FilamentFragment>(), 1)
+                .AddTile(TileID.LunarCraftingStation)
+                .Register();
+
+            solarFragmentNewRecipe = Recipe.Create(ItemID.FragmentSolar, 1);
+            solarFragmentNewRecipe
+                .AddIngredient(ItemID.FragmentVortex, 1)
+                .AddIngredient(ItemID.FragmentStardust, 1)
+                .AddIngredient(ModContent.ItemType<FilamentFragment>(), 1)
+                .AddTile(TileID.LunarCraftingStation)
+                .Register();
+
+            solarFragmentNewRecipe = Recipe.Create(ItemID.FragmentSolar, 1);
+            solarFragmentNewRecipe
+                .AddIngredient(ItemID.FragmentVortex, 1)
+                .AddIngredient(ItemID.FragmentNebula, 1)
+                .AddIngredient(ModContent.ItemType<FilamentFragment>(), 1)
+                .AddTile(TileID.LunarCraftingStation)
+                .Register();
+
+
+            var vortexFragment = Main.recipe.First(x => x.HasResult(ItemID.FragmentVortex));
+            vortexFragment.DisableRecipe();
+            Recipe vortexFragmentNewRecipe = Recipe.Create(ItemID.FragmentVortex, 1);
+            vortexFragmentNewRecipe
+                .AddIngredient(ItemID.FragmentSolar, 1)
+                .AddIngredient(ItemID.FragmentNebula, 1)
+                .AddIngredient(ItemID.FragmentStardust, 1)
+                .AddTile(TileID.LunarCraftingStation)
+                .Register();
+
+            vortexFragmentNewRecipe = Recipe.Create(ItemID.FragmentVortex, 1);
+            vortexFragmentNewRecipe
+                .AddIngredient(ItemID.FragmentNebula, 1)
+                .AddIngredient(ItemID.FragmentStardust, 1)
+                .AddIngredient(ModContent.ItemType<FilamentFragment>(), 1)
+                .AddTile(TileID.LunarCraftingStation)
+                .Register();
+
+            vortexFragmentNewRecipe = Recipe.Create(ItemID.FragmentVortex, 1);
+            vortexFragmentNewRecipe
+                .AddIngredient(ItemID.FragmentSolar, 1)
+                .AddIngredient(ItemID.FragmentStardust, 1)
+                .AddIngredient(ModContent.ItemType<FilamentFragment>(), 1)
+                .AddTile(TileID.LunarCraftingStation)
+                .Register();
+
+            vortexFragmentNewRecipe = Recipe.Create(ItemID.FragmentVortex, 1);
+            vortexFragmentNewRecipe
+                .AddIngredient(ItemID.FragmentSolar, 1)
+                .AddIngredient(ItemID.FragmentNebula, 1)
+                .AddIngredient(ModContent.ItemType<FilamentFragment>(), 1)
+                .AddTile(TileID.LunarCraftingStation)
+                .Register();
+
+            var stardustFragment = Main.recipe.First(x => x.HasResult(ItemID.FragmentStardust));
+            stardustFragment.DisableRecipe();
+            Recipe stardustFragmentNewRecipe = Recipe.Create(ItemID.FragmentStardust, 1);
+            stardustFragmentNewRecipe
+                .AddIngredient(ItemID.FragmentSolar, 1)
+                .AddIngredient(ItemID.FragmentVortex, 1)
+                .AddIngredient(ItemID.FragmentNebula, 1)
+                .AddTile(TileID.LunarCraftingStation)
+                .Register();
+
+            stardustFragmentNewRecipe = Recipe.Create(ItemID.FragmentStardust, 1);
+            stardustFragmentNewRecipe
+                .AddIngredient(ItemID.FragmentVortex, 1)
+                .AddIngredient(ItemID.FragmentNebula, 1)
+                .AddIngredient(ModContent.ItemType<FilamentFragment>(), 1)
+                .AddTile(TileID.LunarCraftingStation)
+                .Register();
+
+            stardustFragmentNewRecipe = Recipe.Create(ItemID.FragmentStardust, 1);
+            stardustFragmentNewRecipe
+                .AddIngredient(ItemID.FragmentSolar, 1)
+                .AddIngredient(ItemID.FragmentNebula, 1)
+                .AddIngredient(ModContent.ItemType<FilamentFragment>(), 1)
+                .AddTile(TileID.LunarCraftingStation)
+                .Register();
+
+            stardustFragmentNewRecipe = Recipe.Create(ItemID.FragmentStardust, 1);
+            stardustFragmentNewRecipe
+                .AddIngredient(ItemID.FragmentSolar, 1)
+                .AddIngredient(ItemID.FragmentVortex, 1)
+                .AddIngredient(ModContent.ItemType<FilamentFragment>(), 1)
+                .AddTile(TileID.LunarCraftingStation)
+                .Register();
+        }
     }
 
     private void NewLunarRecipes() {
